0ec0815c254c0e1c29f6fc49ebbea07bb3be353e
FlorianPalm <f.palm@plt.rwth-aachen.de>
2014-01-22 15:12:17 +0000

    mapping temperature value to node works

--- examples/src/raspberrypi_io.c
+++ examples/src/raspberrypi_io.c
@@ -24,9 +24,9 @@ int readTemp(float *temp){
 
 	fgets(buf, 1000, ptr_file);
 
-	ptr_temp = strtok(buf, (int) delimiter);
+	ptr_temp = strtok(buf, (char*) delimiter);
 
-	ptr_temp = strtok((void*)0, (int) delimiter);
+	ptr_temp = strtok((void*)0, (char*) delimiter);
 
 	temperaturInmC = atol(ptr_temp);
 
