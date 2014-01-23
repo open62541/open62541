ca5bad2541f036573207c06f88035e9ec81ab7cb
FlorianPalm <palm@plt.rwth-aachen.de>
2014-01-23 20:25:16 +0100

    commented functions in opcua_encodingLayer.h

--- OPCUAServer/src/opcua_encodingLayer.h
+++ OPCUAServer/src/opcua_encodingLayer.h
@@ -8,32 +8,50 @@
 #ifndef OPCUA_ENCODINGLAYER_H_
 #define OPCUA_ENCODINGLAYER_H_
 
+/**
+ *  \brief function to decode a request header message
+ *
+ * \param srcRaw             pointer to raw data which holds the encoded data
+ * \param dstRequestHeader   pointer to a structure which hold the encoded header
+ * \return                   0 = success
+ */
+Int32 decodeRequestHeader(const AD_RawMessage *srcRaw, T_RequestHeader *dstRequestHeader);
 
-T_RequestHeader decodeRequestHeader(char *buf);
-
-Int32 encodeRequestHeader(T_RequestHeader *header,char *dstBuf, Int32 *outBufLen);
 
 
+/**
+ *  \brief function to encode a request header message
+ *
+ * \param srcHeader        pointer to header which should be encoded
+ * \param dstRaw           pointer to the destination buffer which receives the encoded data
+ * \return				   0 = success
+ */
+Int32 encodeRequestHeader(const T_RequestHeader *srcHeader,AD_RawMessage *dstRaw);
 
-T_ResponseHeader EL_decodeResponseHeader(AD_RawMessage *dstBuf);
+
+
+/**
+ *  \brief function to decode a response header message
+ *
+ * \param srcRaw             pointer to raw data which holds the encoded data
+ * \param dstResponseHeader  pointer to ResponseHeader structure in which the encoded structure is copied
+ * \return                   0 = success
+ */
+Int32 decodeResponseHeader(const AD_RawMessage *srcRaw, T_ResponseHeader *dstResponseHeader);
+
+
+
 /**
  *  \brief function to encode a secureChannelRequestHeader
  *
  * \param header   a open secure channel header structure which should be encoded to binary format
- * \param dstBuf    pointer to a structure which hold the encoded header
+ * \param dstBuf   pointer to a structure which hold the encoded header
  * \return
  */
+Int32 encodeResponseHeader(T_ResponseHeader *header,AD_RawMessage* *dstBuf);
 
-Int32 EL_encodeResponseHeader(T_ResponseHeader *header,AD_RawMessage* *dstBuf);
 
-/**
- *  \brief function to encode a secureChannelRequest
- *
- * \param request   a open secure channel request structure which should be encoded to binary format
- * \param dstBuf    pointer to a structure which hold the encoded request
- * \return
- */
-//Int32 EL_encodeOpenSecureChannelRequest(T_openSecureChannelRequest* request, AD_RawMessage* dstBuf;
+
 
 
 #endif /* OPCUA_ENCODINGLAYER_H_ */
