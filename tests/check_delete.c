80e81fb3780c5c572e8f4577b9bf6c05d6617017
MaximilianBauer <bauer.maximilian@web.de>
2014-03-24 13:02:44 +0100

    check_calcSize has been added.

---
+++ tests/check_delete.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : check_delete.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "opcua.h"
+#include "opcua_transportLayer.h"
+#include "check.h"
+
+int main (void)
+{
+	int number_failed = 0;
+
+	return 0;
+}
