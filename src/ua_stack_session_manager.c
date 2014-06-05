a5f17f5f903bf67d3a7ffe3a58ffea63322781fb
FlorianPalm <f.palm@plt.rwth-aachen.de>
2014-06-05 10:30:24 +0200

    created new structure to handle sessions

---
+++ src/ua_stack_session_manager.c
@@ -0,0 +1,10 @@
+/*
+ * ua_stack_session_manager.c
+ *
+ *  Created on: 05.06.2014
+ *      Author: root
+ */
+
+
+
+
---
+++ src/ua_stack_session_manager.h
@@ -0,0 +1,15 @@
+/*
+ * ua_stack_session_manager.h
+ *
+ *  Created on: 05.06.2014
+ *      Author: root
+ */
+
+#ifndef UA_STACK_SESSION_MANAGER_H_
+#define UA_STACK_SESSION_MANAGER_H_
+
+
+
+
+
+#endif /* UA_STACK_SESSION_MANAGER_H_ */
