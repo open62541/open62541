9ecab2fcf61e478ccd57cb2a35ad37bc99c36ac5
Stasik0 <github@stasik.com>
2014-06-22 09:57:51 +0200

    allocatig the dimensions on the heap

--- src/ua_application.c
+++ src/ua_application.c
@@ -472,8 +472,10 @@ void appMockup_init() {
 	UA_String_copycstring("http://opcfoundation.org/UA/",&((UA_String *)((namespaceArray->value).data))[0]);
 	UA_String_copycstring("http://localhost:16664/open62541/",&((UA_String *)(((namespaceArray)->value).data))[1]);
 	namespaceArray->arrayDimensionsSize = 1;
-	UA_UInt32 dimensions = 2;
-	namespaceArray->arrayDimensions = &dimensions;
+	UA_UInt32* dimensions = UA_NULL;
+	UA_alloc((void**)&dimensions, sizeof(UA_UInt32));
+	*dimensions = 2;
+	namespaceArray->arrayDimensions = dimensions;
 	namespaceArray->dataType = NS0NODEID(UA_STRING_NS0);
 	namespaceArray->valueRank = 1;
 	namespaceArray->minimumSamplingInterval = 1.0;
