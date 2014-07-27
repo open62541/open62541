07409187252ad218074dfe8988589c7b0107406c
Stasik0 <github@stasik.com>
2014-07-27 18:53:45 -0300

    fixing the fix by @FlorianPalm

--- src/util/ua_list.c
+++ src/util/ua_list.c
@@ -90,7 +90,9 @@ UA_Int32 UA_list_removeFirst(UA_list_List* list, UA_list_PayloadVisitor visitor)
 		}
 		UA_free(list->first);
 		list->first = temp;
-		temp->prev = UA_NULL;
+		if(temp){
+			temp->prev = UA_NULL;
+		}
 		list->size--;
 		if(list->size == 1){
 			list->last = temp;
@@ -111,7 +113,9 @@ UA_Int32 UA_list_removeLast(UA_list_List* list, UA_list_PayloadVisitor visitor){
 		}
 		UA_free(list->last);
 		list->last = temp;
-		temp->next = UA_NULL;
+		if(temp){
+			temp->next = UA_NULL;
+		}
 		list->size--;
 		if(list->size == 1){
 			list->first = temp;
