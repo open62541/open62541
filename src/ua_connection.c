313efe05895f3428dfcde464e8b6cbaa98d47121
Florian Palm <f.palm@plt.rwth-aachen.de>
2014-10-13 09:35:19 +0200

    little changes in secureChannel

--- src/ua_connection.c
+++ src/ua_connection.c
@@ -5,6 +5,7 @@ UA_ConnectionConfig UA_ConnectionConfig_standard = { .protocolVersion = 0,    .s
                                                      .recvBufferSize  = 8192, .maxMessageSize = 8192,
                                                      .maxChunkCount   = 1 };
 
+
 UA_Int32 UA_Connection_init(UA_Connection *connection, UA_ConnectionConfig localConf, void *callbackHandle,
                             UA_Connection_closeCallback close, UA_Connection_writeCallback write) {
     connection->state          = UA_CONNECTION_OPENING;
