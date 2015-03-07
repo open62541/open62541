d1d9743f19a7b2b8028a6bdee54b6f4cbf9ff6fb
Stasik0 <github@stasik.com>
2015-03-07 23:38:39 +0100

    one more try with m32

--- cmake/Toolchain-gcc-m32.cmake
+++ cmake/Toolchain-gcc-m32.cmake
@@ -4,13 +4,3 @@ set(CMAKE_SYSTEM_NAME Linux)
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER gcc -m32)
 set(CMAKE_CXX_COMPILER g++ -m32)
-
-# here is the target environment located
-set(CMAKE_FIND_ROOT_PATH   /usr/lib32 )
-
-# adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search
-# programs in the host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
