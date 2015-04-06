60c05488fa93c2fcbbbef173ffda207b689fb445
Sergey Alirzaev <zl29ah@gmail.com>
2015-04-07 01:34:49 +0300

    client.c: more consistent indentation style

--- examples/client.c
+++ examples/client.c
@@ -1,8 +1,8 @@
 #ifdef NOT_AMALGATED
-    #include "ua_types.h"
-    #include "ua_client.h"
+	#include "ua_types.h"
+	#include "ua_client.h"
 #else
-    #include "open62541.h"
+	#include "open62541.h"
 #endif
 
 #include <stdio.h>
@@ -11,31 +11,31 @@
 int main(int argc, char *argv[]) {
 	UA_Client *client = UA_Client_new();
 	UA_ClientNetworkLayer nl = ClientNetworkLayerTCP_new(UA_ConnectionConfig_standard);
-    UA_StatusCode retval = UA_Client_connect(client, UA_ConnectionConfig_standard, nl,
-                                             "opc.tcp://localhost:16664");
+	UA_StatusCode retval = UA_Client_connect(client, UA_ConnectionConfig_standard, nl,
+			"opc.tcp://localhost:16664");
 	if(retval != UA_STATUSCODE_GOOD) {
-        UA_Client_delete(client);
-    	return retval;
-    }
+		UA_Client_delete(client);
+		return retval;
+	}
 
-    UA_ReadRequest req;
-    UA_ReadRequest_init(&req);
-    req.nodesToRead = UA_ReadValueId_new();
-    req.nodesToReadSize = 1;
-    req.nodesToRead[0].nodeId = UA_NODEID_STRING_ALLOC(1, "the.answer"); /* assume this node exists */
-    req.nodesToRead[0].attributeId = UA_ATTRIBUTEID_VALUE;
+	UA_ReadRequest req;
+	UA_ReadRequest_init(&req);
+	req.nodesToRead = UA_ReadValueId_new();
+	req.nodesToReadSize = 1;
+	req.nodesToRead[0].nodeId = UA_NODEID_STRING_ALLOC(1, "the.answer"); /* assume this node exists */
+	req.nodesToRead[0].attributeId = UA_ATTRIBUTEID_VALUE;
 
-    UA_ReadResponse resp = UA_Client_read(client, &req);
-    if(resp.responseHeader.serviceResult == UA_STATUSCODE_GOOD &&
-       resp.resultsSize > 0 && resp.results[0].hasValue &&
-       resp.results[0].value.data /* an empty array returns a null-ptr */ &&
-       resp.results[0].value.type == &UA_TYPES[UA_TYPES_INT32])
-        printf("the answer is: %i\n", *(UA_Int32*)resp.results[0].value.data);
+	UA_ReadResponse resp = UA_Client_read(client, &req);
+	if(resp.responseHeader.serviceResult == UA_STATUSCODE_GOOD &&
+	   resp.resultsSize > 0 && resp.results[0].hasValue &&
+	   resp.results[0].value.data /* an empty array returns a null-ptr */ &&
+	   resp.results[0].value.type == &UA_TYPES[UA_TYPES_INT32])
+		printf("the answer is: %i\n", *(UA_Int32*)resp.results[0].value.data);
 
-    UA_ReadRequest_deleteMembers(&req);
-    UA_ReadResponse_deleteMembers(&resp);
-    UA_Client_disconnect(client);
-    UA_Client_delete(client);
-    return UA_STATUSCODE_GOOD;
+	UA_ReadRequest_deleteMembers(&req);
+	UA_ReadResponse_deleteMembers(&resp);
+	UA_Client_disconnect(client);
+	UA_Client_delete(client);
+	return UA_STATUSCODE_GOOD;
 }
 
