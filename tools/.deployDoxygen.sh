a4a659f95bbc62690591f462754abd73c5c5f231
Stasik0 <github@stasik.com>
2015-05-04 22:20:29 +0200

    adding doxygen doc to releases

--- tools/.deployDoxygen.sh
+++ tools/.deployDoxygen.sh
@@ -9,7 +9,7 @@ git fetch origin coverity_scan
 git clone --depth=5 -b gh-pages https://$GITAUTH@github.com/acplt/open62541-www
 cd open62541-www
 git rm -rf doc
-cp -r ../build/doc ./
+cp -r ../doc ./
 git add -A doc/*
 git config --global user.email "open62541-travis-ci@users.noreply.github.com"
 git config --global user.name "Open62541 travis-ci"
