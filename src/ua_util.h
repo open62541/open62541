ab8af8cffccbe9cf8d009fedf045c0e0426a0aed
Julius Pfrommer <jpfr@users.noreply.github.com>
2015-06-03 16:43:14 +0200

    use __restrict also for all visual studio versions

--- src/ua_util.h
+++ src/ua_util.h
@@ -34,7 +34,7 @@
 #endif
 
 /* Visual Studio needs __restrict */
-#if defined(_MSC_VER) && _MSC_VER >= 1400
+#ifdef _MSC_VER
     #define UA_RESTRICT __restrict
 #else
     #define UA_RESTRICT restrict
