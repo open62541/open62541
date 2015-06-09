0f5370f17241ed647f78cde8e63e8d2d5b66848d
ichrispa <Chris_Paul.Iatrou@tu-dresden.de>
2015-06-09 16:31:06 +0200

    Swapped addOneWayReferenceWithSession() for userspace accessible UA_Server_AddMonodirectionalReference(), making the code generator usable for userspace namespace model generation.

--- tools/pyUANamespace/open62541_MacroHelper.py
+++ tools/pyUANamespace/open62541_MacroHelper.py
@@ -58,17 +58,23 @@ class open62541_MacroHelper():
   #    UA_Server_addReference(UA_Server *server, const UA_NodeId sourceId, const UA_NodeId refTypeId,
   #                                   const UA_ExpandedNodeId targetId)
     code = []
-    refid = "ref_" + reference.getCodePrintableID()
-    code.append("UA_AddReferencesItem " + refid + ";")
-    code.append("UA_AddReferencesItem_init(&" + refid + ");")
-    code.append(refid + ".sourceNodeId = " + self.getCreateNodeIDMacro(sourcenode) + ";")
-    code.append(refid + ".referenceTypeId = " + self.getCreateNodeIDMacro(reference.referenceType()) + ";")
+    #refid = "ref_" + reference.getCodePrintableID()
+    #code.append("UA_AddReferencesItem " + refid + ";")
+    #code.append("UA_AddReferencesItem_init(&" + refid + ");")
+    #code.append(refid + ".sourceNodeId = " + self.getCreateNodeIDMacro(sourcenode) + ";")
+    #code.append(refid + ".referenceTypeId = " + self.getCreateNodeIDMacro(reference.referenceType()) + ";")
+    #if reference.isForward():
+      #code.append(refid + ".isForward = UA_TRUE;")
+    #else:
+      #code.append(refid + ".isForward = UA_FALSE;")
+    #code.append(refid + ".targetNodeId = " + self.getCreateExpandedNodeIDMacro(reference.target()) + ";")
+    #code.append("addOneWayReferenceWithSession(server, (UA_Session *) UA_NULL, &" + refid + ");")
+    
     if reference.isForward():
-      code.append(refid + ".isForward = UA_TRUE;")
+      code.append("UA_Server_AddMonodirectionalReference(server, " + self.getCreateNodeIDMacro(sourcenode) + ", " + self.getCreateExpandedNodeIDMacro(reference.target()) + ", " + self.getCreateNodeIDMacro(reference.referenceType()) + ", UA_TRUE);")
     else:
-      code.append(refid + ".isForward = UA_FALSE;")
-    code.append(refid + ".targetNodeId = " + self.getCreateExpandedNodeIDMacro(reference.target()) + ";")
-    code.append("addOneWayReferenceWithSession(server, (UA_Session *) UA_NULL, &" + refid + ");")
+      code.append("UA_Server_AddMonodirectionalReference(server, " + self.getCreateNodeIDMacro(sourcenode) + ", " + self.getCreateExpandedNodeIDMacro(reference.target()) + ", " + self.getCreateNodeIDMacro(reference.referenceType()) + ", UA_FALSE);")
+    
     return code
 
   def getCreateNode(self, node):
