d0e09b24ffed3a9cbff9d34042dbdc4cd194fb9d
Stasik0 <github@stasik.com>
2015-06-27 15:32:48 +0200

    removing enforcing NameSpaceId in BrowseName, relates to #284

--- src/server/ua_nodestore.c
+++ src/server/ua_nodestore.c
@@ -259,9 +259,6 @@ UA_StatusCode UA_NodeStore_insert(UA_NodeStore *ns, UA_Node *node, const UA_Node
         if(node->nodeId.namespaceIndex==0) //original request for ns=0 should yield ns=1
             node->nodeId.namespaceIndex=1;
 
-        //set namespaceIndex in browseName in case id is generated
-        node->browseName.namespaceIndex=node->nodeId.namespaceIndex;
-
         UA_Int32 identifier = ns->count+1; // start value
         UA_Int32 size = ns->size;
         hash_t increase = mod2(identifier, size);
