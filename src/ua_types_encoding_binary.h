2b3d33d41ed9bc07b3a784a5f23ecdf6ae571d8d
Julius Pfrommer <julius.pfrommer@web.de>
2016-01-21 04:36:04 +0100

    simplify encoding

--- src/ua_types_encoding_binary.h
+++ src/ua_types_encoding_binary.h
@@ -4,9 +4,9 @@
 #include "ua_types.h"
 
 UA_StatusCode UA_encodeBinary(const void *src, const UA_DataType *type, UA_ByteString *dst,
-                              size_t *UA_RESTRICT offset) UA_FUNC_ATTR_WARN_UNUSED_RESULT;
+                              size_t *offset) UA_FUNC_ATTR_WARN_UNUSED_RESULT;
 
-UA_StatusCode UA_decodeBinary(const UA_ByteString *src, size_t * UA_RESTRICT offset, void *dst,
+UA_StatusCode UA_decodeBinary(const UA_ByteString *src, size_t *offset, void *dst,
                               const UA_DataType *type) UA_FUNC_ATTR_WARN_UNUSED_RESULT;
 
 #endif /* UA_TYPES_ENCODING_BINARY_H_ */
