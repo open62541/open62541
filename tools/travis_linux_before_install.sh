e659d928bbcdb3fba07ba7b5c3b07fbe83665c4d
Stefan Profanter <git@s.profanter.me>
2016-03-01 15:10:09 +0100

    Using new travis build environment and cppcheck (fix #391 and fix #371)

--- tools/travis_linux_before_install.sh
+++ tools/travis_linux_before_install.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 set -ev
-sudo apt-get update
-sudo apt-get install binutils-mingw-w64-i686 gcc-mingw-w64-i686 mingw-w64
-sudo add-apt-repository ppa:kalakris/cmake -y
-sudo apt-get update -qq
-sudo apt-get install -qq --no-install-recommends build-essential cmake python-lxml gcc-multilib wget zip
-sudo apt-get install libsubunit-dev #for check_0.10.0
-wget http://ftp.de.debian.org/debian/pool/main/c/check/check_0.10.0-3_amd64.deb
-sudo dpkg -i check_0.10.0-3_amd64.deb
-wget https://launchpad.net/ubuntu/+source/liburcu/0.8.5-1ubuntu1/+build/6513813/+files/liburcu2_0.8.5-1ubuntu1_amd64.deb
-wget https://launchpad.net/ubuntu/+source/liburcu/0.8.5-1ubuntu1/+build/6513813/+files/liburcu-dev_0.8.5-1ubuntu1_amd64.deb
-sudo dpkg -i liburcu2_0.8.5-1ubuntu1_amd64.deb
-sudo dpkg -i liburcu-dev_0.8.5-1ubuntu1_amd64.deb
-sudo pip install cpp-coveralls
-sudo pip install sphinx
-sudo pip install sphinx_rtd_theme
+
+if [ $ANALYZE = "true" ]; then
+  	g++ --version
+  	cppcheck --version
+else
+	cd $LOCAL_PKG
+
+	# Install specific check version which is not yet in the apt package
+ 	wget http://ftp.de.debian.org/debian/pool/main/c/check/check_0.10.0-3_amd64.deb
+ 	dpkg -x check_0.10.0-3_amd64.deb $LOCAL_PKG/package
+ 	# change pkg-config file path
+	sed -i "s|prefix=/usr|prefix=${LOCAL_PKG}|g" $LOCAL_PKG/package/usr/lib/x86_64-linux-gnu/pkgconfig/check.pc
+	sed -i 's|libdir=.*|libdir=${prefix}/lib|g' $LOCAL_PKG/package/usr/lib/x86_64-linux-gnu/pkgconfig/check.pc
+	# move files to globally included dirs
+	cp -R $LOCAL_PKG/package/usr/lib/x86_64-linux-gnu/* $LOCAL_PKG/lib/
+	cp -R $LOCAL_PKG/package/usr/include/* $LOCAL_PKG/include/
+	cp -R $LOCAL_PKG/package/usr/bin/* $LOCAL_PKG/
+
+	# Install specific liburcu version which is not yet in the apt package
+	wget https://launchpad.net/ubuntu/+source/liburcu/0.8.5-1ubuntu1/+build/6513813/+files/liburcu2_0.8.5-1ubuntu1_amd64.deb
+	wget https://launchpad.net/ubuntu/+source/liburcu/0.8.5-1ubuntu1/+build/6513813/+files/liburcu-dev_0.8.5-1ubuntu1_amd64.deb
+	dpkg -x liburcu2_0.8.5-1ubuntu1_amd64.deb $LOCAL_PKG/package
+	dpkg -x liburcu-dev_0.8.5-1ubuntu1_amd64.deb $LOCAL_PKG/package
+	# move files to globally included dirs
+	cp -R $LOCAL_PKG/package/usr/lib/x86_64-linux-gnu/* $LOCAL_PKG/lib/
+	cp -R $LOCAL_PKG/package/usr/include/* $LOCAL_PKG/include/
+
+
+
+	pip install --user cpp-coveralls
+	pip install --user sphinx
+	pip install --user sphinx_rtd_theme
+fi
