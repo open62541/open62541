27d7320e2f0255cc70fe3046bf5d65389ae9b2bf
Julius Pfrommer <julius.pfrommer@web.de>
2016-03-24 11:33:23 +0100

    use old build/doc directory for html output

--- tools/travis_push_doc.sh
+++ tools/travis_push_doc.sh
@@ -4,7 +4,7 @@ git clone --depth=5 -b gh-pages https://$GITAUTH@github.com/open62541/open62541-
 cd open62541-www
 
 git rm -r -f ./doc/current/*
-cp -r ../../doc_html/* ./doc/current/
+cp -r ../../doc/* ./doc/current/
 git add -A ./doc/current
 git config --global user.email "open62541-travis-ci@users.noreply.github.com"
 git config --global user.name "Open62541 travis-ci"
