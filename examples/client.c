d51eb62a39acaab3cb1952d56e21442443bb0d7f
Julius Pfrommer <julius.pfrommer@web.de>
2016-07-06 23:27:07 +0200

    use highlevel API for the readme example

--- examples/client.c
+++ examples/client.c
@@ -121,24 +121,16 @@ int main(int argc, char *argv[]) {
 #endif
 
     /* Read attribute */
-    printf("\nReading the value of node (1, \"the.answer\"):\n");
     UA_Int32 value = 0;
-    UA_ReadRequest rReq;
-    UA_ReadRequest_init(&rReq);
-    rReq.nodesToRead =  UA_Array_new(1, &UA_TYPES[UA_TYPES_READVALUEID]);
-    rReq.nodesToReadSize = 1;
-    rReq.nodesToRead[0].nodeId = UA_NODEID_STRING_ALLOC(1, "the.answer"); /* assume this node exists */
-    rReq.nodesToRead[0].attributeId = UA_ATTRIBUTEID_VALUE; /* return the value attribute */
-    UA_ReadResponse rResp = UA_Client_Service_read(client, rReq);
-    if(rResp.responseHeader.serviceResult == UA_STATUSCODE_GOOD &&
-       rResp.resultsSize > 0 && rResp.results[0].hasValue &&
-       UA_Variant_isScalar(&rResp.results[0].value) &&
-       rResp.results[0].value.type == &UA_TYPES[UA_TYPES_INT32]) {
-        value = *(UA_Int32*)rResp.results[0].value.data;
-        printf("the value is: %i\n", value);
+    printf("\nReading the value of node (1, \"the.answer\"):\n");
+    UA_Variant *val = UA_Variant_new();
+    retval = UA_Client_readValueAttribute(client, UA_NODEID_STRING(1, "the.answer"), val);
+    if(retval == UA_STATUSCODE_GOOD && UA_Variant_isScalar(val) &&
+       val->type == &UA_TYPES[UA_TYPES_INT32]) {
+            value = *(UA_Int32*)val->data;
+            printf("the value is: %i\n", value);
     }
-    UA_ReadRequest_deleteMembers(&rReq);
-    UA_ReadResponse_deleteMembers(&rResp);
+    UA_Variant_delete(val);
 
     /* Write node attribute */
     value++;
