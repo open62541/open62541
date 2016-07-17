63d65b88a287d4f2e9ba9d264be6319aceecb2c4
Julius Pfrommer <julius.pfrommer@web.de>
2016-07-17 14:55:21 +0200

    fix caching packages

--- tools/travis/travis_linux_before_install.sh
+++ tools/travis/travis_linux_before_install.sh
@@ -1,56 +1,65 @@
 #!/bin/bash
 set -ev
 
-# Print the compiler for debugging of travis behaviour
-if [ "$CC" = "clang" ]; then
-    clang --version
-else
-    g++ --version
-    cppcheck --version
+# Increase the environment version to force a rebuild of the packages
+# The version is writen to the cache file after every build of the dependencies
+ENV_VERSION = "1"
+ENV_INSTALLED = "none"
+if [ -e "$LOCAL_PKG/.cached" ]
+   read -r ENV_INSTALLED < $LOCAL_PKG/.cached
 fi
 
 # travis caches the $LOCAL_PKG dir. If it is loaded, we don't need to reinstall the packages
-if [ -f $LOCAL_PKG/.cached ]; then
+if [ "$ENV_VERSION" = "$ENV_INSTALLED" ]; then
+    # Print version numbers
+    clang --version
+    g++ --version
+    cppcheck --version
+    valgrind --version
     exit 0
 fi
 
+# Clean up
+rm -rf $LOCAL_PKG/*
+rm -rf $LOCAL_PKG/.*
+
 # Install newer valgrind
-cd $LOCAL_PKG
-mkdir -p $LOCAL_PKG/package && cd $LOCAL_PKG/package
 wget http://valgrind.org/downloads/valgrind-3.11.0.tar.bz2
 tar xf valgrind-3.11.0.tar.bz2
 cd valgrind-3.11.0
 ./configure --prefix=$LOCAL_PKG
 make -s -j8 install
-echo "\n### Installed valgrind version: $(valgrind --version)"
+cd ..
 
 # Install specific check version which is not yet in the apt package
-cd $LOCAL_PKG
+mkdir tmp_check
 wget http://ftp.de.debian.org/debian/pool/main/c/check/check_0.10.0-3_amd64.deb
-dpkg -x check_0.10.0-3_amd64.deb $LOCAL_PKG/package
+dpkg -x check_0.10.0-3_amd64.deb ./tmp_check
 # change pkg-config file path
-sed -i "s|prefix=/usr|prefix=${LOCAL_PKG}|g" $LOCAL_PKG/package/usr/lib/x86_64-linux-gnu/pkgconfig/check.pc
-sed -i 's|libdir=.*|libdir=${prefix}/lib|g' $LOCAL_PKG/package/usr/lib/x86_64-linux-gnu/pkgconfig/check.pc
+sed -i "s|prefix=/usr|prefix=${LOCAL_PKG}|g" .tmp_check/usr/lib/x86_64-linux-gnu/pkgconfig/check.pc
+sed -i 's|libdir=.*|libdir=${prefix}/lib|g' ./tmp_check/usr/lib/x86_64-linux-gnu/pkgconfig/check.pc
 # move files to globally included dirs
-cp -R $LOCAL_PKG/package/usr/lib/x86_64-linux-gnu/* $LOCAL_PKG/lib/
-cp -R $LOCAL_PKG/package/usr/include/* $LOCAL_PKG/include/
-cp -R $LOCAL_PKG/package/usr/bin/* $LOCAL_PKG/
+cp -R ./tmp_check/usr/lib/x86_64-linux-gnu/* $LOCAL_PKG/lib/
+cp -R ./tmp_check/package/usr/include/* $LOCAL_PKG/include/
+cp -R ./tmp_check/package/usr/bin/* $LOCAL_PKG/
 
 # Install specific liburcu version which is not yet in the apt package
+mkdir tmp_liburcu
 wget https://launchpad.net/ubuntu/+source/liburcu/0.8.5-1ubuntu1/+build/6513813/+files/liburcu2_0.8.5-1ubuntu1_amd64.deb
 wget https://launchpad.net/ubuntu/+source/liburcu/0.8.5-1ubuntu1/+build/6513813/+files/liburcu-dev_0.8.5-1ubuntu1_amd64.deb
-dpkg -x liburcu2_0.8.5-1ubuntu1_amd64.deb $LOCAL_PKG/package
-dpkg -x liburcu-dev_0.8.5-1ubuntu1_amd64.deb $LOCAL_PKG/package
+dpkg -x liburcu2_0.8.5-1ubuntu1_amd64.deb ./tmp_liburcu
+dpkg -x liburcu-dev_0.8.5-1ubuntu1_amd64.deb ./tmp_liburcu
 # move files to globally included dirs
-cp -R $LOCAL_PKG/package/usr/lib/x86_64-linux-gnu/* $LOCAL_PKG/lib/
-cp -R $LOCAL_PKG/package/usr/include/* $LOCAL_PKG/include/
+cp -R ./tmp_liburcu/usr/lib/x86_64-linux-gnu/* $LOCAL_PKG/lib/
+cp -R ./tmp_liburcu/usr/include/* $LOCAL_PKG/include/
 
 # Install newer cppcheck
 wget https://github.com/danmar/cppcheck/archive/1.73.tar.gz -O cppcheck-1.73.tar.gz
 tar xf cppcheck-1.73.tar.gz
-cd $LOCAL_PKG/cppcheck-1.73
-make SRCDIR=build CFGDIR="$LOCAL_PKG/cppcheck-1.73/cfg" HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" -j8
-ln -s $LOCAL_PKG/cppcheck-1.73/cppcheck $LOCAL_PKG/cppcheck
+cd cppcheck-1.73
+make PREFIX="$LOCAL_PKG" SRCDIR=build CFGDIR="$LOCAL_PKG/cppcheck-cfg" HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" -j8
+make PREFIX="$LOCAL_PKG" SRCDIR=build CFGDIR="$LOCAL_PKG/cppcheck-cfg" HAVE_RULES=yes install
+cd ..
 
 # Install python packages
 pip install --user cpp-coveralls
@@ -58,4 +67,10 @@ pip install --user sphinx
 pip install --user sphinx_rtd_theme
 
 # create cached flag
-touch $LOCAL_PKG/.cached
+echo $ENV_VERSION > $LOCAL_PKG/.cached
+
+# Print version numbers
+clang --version
+g++ --version
+cppcheck --version
+valgrind --version
