e1a8a2c26eaf16718ca53c00ef78f62538b9ce20
Julius Pfrommer <julius.pfrommer@web.de>
2016-07-18 01:05:01 +0200

    fix notification setting

--- tools/travis/travis_notifications.sh
+++ tools/travis/travis_notifications.sh
@@ -4,8 +4,8 @@ set -ev
 IRC_NOTIFY_SUCCESS="never"
 IRC_NOTIFY_FAILURE="never"
 
-if [[ ( ${TRAVIS_OS_NAME} == "linux" && ${CC} == "gcc" ) ]]; then
-    if [[ ( ${TRAVIS_BRANCH} == "0.2" || ${TRAVIS_BRANCH} == "master" ) ]]; then
+if [ ${TRAVIS_OS_NAME} == "linux" ] && [ ${CC} == "gcc" ]; then
+    if [ ${TRAVIS_BRANCH} == "0.2" ] || [ ${TRAVIS_BRANCH} == "master" ]; then
         IRC_NOTIFY_SUCCESS="always"
         IRC_NOTIFY_FAILURE="always"
     fi
