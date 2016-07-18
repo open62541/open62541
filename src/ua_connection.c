53d842754430824c0247e57afcd5ea0a1df0745b
Julius Pfrommer <julius.pfrommer@web.de>
2016-07-18 06:40:07 +0200

    work on #689

--- src/ua_connection.c
+++ src/ua_connection.c
@@ -32,101 +32,117 @@ void UA_Connection_deleteMembers(UA_Connection *connection) {
 UA_StatusCode
 UA_Connection_completeMessages(UA_Connection *connection, UA_ByteString * UA_RESTRICT message,
                               UA_Boolean * UA_RESTRICT realloced) {
-    UA_ByteString *current = message;
-    *realloced = false;
+    UA_StatusCode retval = UA_STATUSCODE_GOOD;
+    UA_ByteString *current = message; /* points to either the network message or the copied message */
+
+    /* We have a stored half-chunk. Concat the received message to the end */
     if(connection->incompleteMessage.length > 0) {
-        /* concat the existing incomplete message with the new message */
         UA_Byte *data = UA_realloc(connection->incompleteMessage.data,
                                    connection->incompleteMessage.length + message->length);
         if(!data) {
-            /* not enough memory */
-            UA_ByteString_deleteMembers(&connection->incompleteMessage);
-            connection->releaseRecvBuffer(connection, message);
-            return UA_STATUSCODE_BADOUTOFMEMORY;
+            retval = UA_STATUSCODE_BADOUTOFMEMORY;
+            goto cleanup;
         }
         memcpy(&data[connection->incompleteMessage.length], message->data, message->length);
         connection->incompleteMessage.data = data;
         connection->incompleteMessage.length += message->length;
         connection->releaseRecvBuffer(connection, message);
         current = &connection->incompleteMessage;
-        *realloced = true;
     }
 
-    /* the while loop sets offset to the first element after the last complete message. if a message
-       contains garbage, the buffer length is set to contain only the "good" messages before. */
-    size_t offset = 0;
-    size_t delete_at = current->length-1; // garbled message after this point
-    while(current->length - offset >= 16) {
-        UA_UInt32 msgtype = (UA_UInt32)current->data[offset] +
-            ((UA_UInt32)current->data[offset+1] << 8) +
-            ((UA_UInt32)current->data[offset+2] << 16);
+    /* Loop over the chunks in the received buffer */
+    size_t complete_until = 0; /* the received complete chunks end at this point */
+    UA_Boolean garbage_end = false; /* garbage after the last complete message */
+    while(current->length - complete_until >= 8) {
+        /* Check the message type */
+        UA_UInt32 msgtype = (UA_UInt32)current->data[complete_until] +
+            ((UA_UInt32)current->data[complete_until+1] << 8) +
+            ((UA_UInt32)current->data[complete_until+2] << 16);
         if(msgtype != ('M' + ('S' << 8) + ('G' << 16)) &&
            msgtype != ('O' + ('P' << 8) + ('N' << 16)) &&
            msgtype != ('H' + ('E' << 8) + ('L' << 16)) &&
            msgtype != ('A' + ('C' << 8) + ('K' << 16)) &&
            msgtype != ('C' + ('L' << 8) + ('O' << 16))) {
-            /* the message type is not recognized */
-            delete_at = offset; // throw the remaining message away
+            garbage_end = true; /* the message type is not recognized */
             break;
         }
-        UA_UInt32 length = 0;
-        size_t length_pos = offset + 4;
-        UA_StatusCode retval = UA_UInt32_decodeBinary(current, &length_pos, &length);
-        if(retval != UA_STATUSCODE_GOOD || length < 16 || length > connection->localConf.recvBufferSize) {
-            /* the message size is not allowed. throw the remaining bytestring away */
-            delete_at = offset;
+
+        /* Decode the length of the chunk */
+        UA_UInt32 chunk_length = 0;
+        size_t length_pos = complete_until + 4;
+        UA_StatusCode decode_retval = UA_UInt32_decodeBinary(current, &length_pos, &chunk_length);
+
+        /* The message size is not allowed. Throw the remaining bytestring away */
+        if(decode_retval != UA_STATUSCODE_GOOD || chunk_length < 16 || chunk_length > connection->localConf.recvBufferSize) {
+            garbage_end = true;
             break;
         }
-        if(length + offset > current->length)
-            break; /* the message is incomplete. keep the beginning */
-        offset += length;
+
+        /* The chunk is okay but incomplete. Store the end. */
+        if(chunk_length + complete_until > current->length)
+            break;
+
+        /* Go to the next chunk */
+        complete_until += chunk_length;
     }
 
-    /* throw the message away */
-    if(delete_at == 0) {
-        if(!*realloced) {
-            connection->releaseRecvBuffer(connection, message);
-            *realloced = true;
-        } else
-            UA_ByteString_deleteMembers(current);
-        return UA_STATUSCODE_GOOD;
-    }
+    /* Separate complete from incomplete chunks */
+    if(complete_until != current->length) {
+        size_t incomplete_length = current->length - complete_until;
 
-    /* no complete message at all */
-    if(offset == 0) {
-        if(!*realloced) {
-            /* store the buffer in the connection */
-            UA_ByteString_copy(current, &connection->incompleteMessage);
-            connection->releaseRecvBuffer(connection, message);
-            *realloced = true;
-        }
-        return UA_STATUSCODE_GOOD;
-    }
-
-    /* there remains an incomplete message at the end */
-    if(current->length != offset) {
-        UA_Byte *data = UA_malloc(current->length - offset);
-        if(!data) {
-            UA_ByteString_deleteMembers(&connection->incompleteMessage);
-            if(!*realloced) {
-                connection->releaseRecvBuffer(connection, message);
-                *realloced = true;
+        /* No complete chunk to return */
+        if(complete_until == 0) {
+            if(garbage_end) { /* All garbage, reset all */
+                retval = UA_STATUSCODE_BADDECODINGERROR;
+                goto cleanup;
             }
-            return UA_STATUSCODE_BADOUTOFMEMORY;
+
+            /* Store the incomplete message and return UA_BYTESTRING_NULL */
+            if(current == message) { /* Incomplete message lives in network layer */
+                retval = UA_ByteString_allocBuffer(&connection->incompleteMessage, incomplete_length);
+                if(retval != UA_STATUSCODE_GOOD)
+                    goto cleanup;
+                memcpy(connection->incompleteMessage.data, message->data, incomplete_length);
+                connection->releaseRecvBuffer(connection, message);
+            } else { /* Incomplete message already copied to the buffer */
+                current = message; // message is already set to UA_BYTESTRING_NULL
+            }
+            goto cleanup;
         }
-        size_t newlength = current->length - offset;
-        memcpy(data, &current->data[offset], newlength);
-        current->length = offset;
-        if(*realloced)
-            *message = *current;
-        connection->incompleteMessage.data = data;
-        connection->incompleteMessage.length = newlength;
-        return UA_STATUSCODE_GOOD;
+
+        /* Just ignore garbage after a complete message */
+        if(garbage_end) {
+            current->length = complete_until;
+            goto cleanup;
+        }
+
+        /* We have an incomplete chunk at the end that needs to be stored */
+        current->length = complete_until;
+        UA_ByteString oldIncompleteMessage = connection->incompleteMessage;
+        retval = UA_ByteString_allocBuffer(&connection->incompleteMessage, incomplete_length);
+        if(retval != UA_STATUSCODE_GOOD)
+            goto cleanup;
+        memcpy(&connection->incompleteMessage.data, &current->data[complete_until], incomplete_length);
+        if(current != message) { /* Message was already on the incompleteMessage buffer */
+            *message = oldIncompleteMessage;
+            current = message;
+        }
     }
 
-    if(current == &connection->incompleteMessage) {
-        *message = *current;
+ cleanup:
+    /* Error, reset the connection */
+    if(retval != UA_STATUSCODE_GOOD) {
+        connection->releaseRecvBuffer(connection, message);
+        UA_ByteString_deleteMembers(&connection->incompleteMessage);
         connection->incompleteMessage = UA_BYTESTRING_NULL;
+        return retval;
+    }
+
+    /* If the original message was moved, delete on the network layer */
+    *realloced = (current != message);
+    if(*realloced) {
+        connection->releaseRecvBuffer(connection, message);
+        *message = *current;
     }
     return UA_STATUSCODE_GOOD;
 }
