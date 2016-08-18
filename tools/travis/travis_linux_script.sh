ff0d273b490fc767c5939f93738514037c7e5349
Stefan Profanter <git@s.profanter.me>
2016-08-18 16:10:13 +0200

    Update mdnsd

--- tools/travis/travis_linux_script.sh
+++ tools/travis/travis_linux_script.sh
@@ -35,83 +35,83 @@ if [ $ANALYZE = "true" ]; then
     fi
 else
     echo "=== Building ==="
-
-    echo "Documentation and certificate build"
-    mkdir -p build
-    cd build
-    cmake -DCMAKE_BUILD_TYPE=Release -DUA_BUILD_EXAMPLES=ON -DUA_BUILD_DOCUMENTATION=ON -DUA_BUILD_SELFSIGNED_CERTIFICATE=ON ..
-    make doc
-    make selfsigned
-    cp -r doc ../../
-    cp ./examples/server_cert.der ../../
-    cd .. && rm build -rf
-
-    # cross compilation only with gcc
-    if [ "$CC" = "gcc" ]; then
-        echo "Cross compile release build for MinGW 32 bit"
-        mkdir -p build && cd build
-        cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cmake/Toolchain-mingw32.cmake -DUA_ENABLE_AMALGAMATION=ON -DCMAKE_BUILD_TYPE=Release -DUA_BUILD_EXAMPLES=ON ..
-        make -j
-        zip -r open62541-win32.zip ../../doc ../../server_cert.der ../LICENSE ../AUTHORS ../README.md examples/server.exe examples/client.exe libopen62541.dll.a open62541.h open62541.c
-        cp open62541-win32.zip ..
-        cd .. && rm build -rf
-
-        echo "Cross compile release build for MinGW 64 bit"
-        mkdir -p build && cd build
-        cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cmake/Toolchain-mingw64.cmake -DUA_ENABLE_AMALGAMATION=ON -DCMAKE_BUILD_TYPE=Release -DUA_BUILD_EXAMPLES=ON ..
-        make -j
-        zip -r open62541-win64.zip ../../doc ../../server_cert.der ../LICENSE ../AUTHORS ../README.md examples/server.exe examples/client.exe libopen62541.dll.a open62541.h open62541.c
-        cp open62541-win64.zip ..
-        cd .. && rm build -rf
-
-        echo "Cross compile release build for 32-bit linux"
-        mkdir -p build && cd build
-        cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cmake/Toolchain-gcc-m32.cmake -DUA_ENABLE_AMALGAMATION=ON -DCMAKE_BUILD_TYPE=Release -DUA_BUILD_EXAMPLES=ON ..
-        make -j
-        tar -pczf open62541-linux32.tar.gz ../../doc ../../server_cert.der ../LICENSE ../AUTHORS ../README.md examples/server examples/client libopen62541.a open62541.h open62541.c
-        cp open62541-linux32.tar.gz ..
-        cd .. && rm build -rf
-    fi
-
-    echo "Compile release build for 64-bit linux"
-    mkdir -p build && cd build
-    cmake -DCMAKE_BUILD_TYPE=Release -DUA_ENABLE_AMALGAMATION=ON -DUA_BUILD_EXAMPLES=ON ..
-    make -j
-    tar -pczf open62541-linux64.tar.gz ../../doc ../../server_cert.der ../LICENSE ../AUTHORS ../README.md examples/server examples/client libopen62541.a open62541.h open62541.c
-    cp open62541-linux64.tar.gz ..
-    cp open62541.h .. # copy single file-release
-    cp open62541.c .. # copy single file-release
-    cd .. && rm build -rf
-
-    if [ "$CC" = "gcc" ]; then
-        echo "Upgrade to gcc 4.8"
-        export CXX="g++-4.8" CC="gcc-4.8"
-    fi
-
-    echo "Building the C++ example"
-    mkdir -p build && cd build
-    cp ../open62541.* .
-    gcc-4.8 -std=c99 -c open62541.c
-    g++-4.8 ../examples/server.cpp -I./ open62541.o -lrt -o cpp-server
-    cd .. && rm build -rf
-
-    echo "Compile multithreaded version"
-    mkdir -p build && cd build
-    cmake -DUA_ENABLE_MULTITHREADING=ON -DUA_BUILD_EXAMPLES=ON ..
-    make -j
-    cd .. && rm build -rf
-
-    echo "Compile without discovery version"
-    mkdir -p build && cd build
-    cmake -DUA_ENABLE_DISCOVERY=OFF -DUA_ENABLE_DISCOVERY_MULTICAST=OFF -DUA_BUILD_EXAMPLES=ON ..
-    make -j
-    cd .. && rm build -rf
-
-    echo "Compile discovery without multicast version"
-    mkdir -p build && cd build
-    cmake -DUA_ENABLE_DISCOVERY=ON -DUA_ENABLE_DISCOVERY_MULTICAST=OFF -DUA_BUILD_EXAMPLES=ON ..
-    make -j
-    cd .. && rm build -rf
+#
+#    echo "Documentation and certificate build"
+#    mkdir -p build
+#    cd build
+#    cmake -DCMAKE_BUILD_TYPE=Release -DUA_BUILD_EXAMPLES=ON -DUA_BUILD_DOCUMENTATION=ON -DUA_BUILD_SELFSIGNED_CERTIFICATE=ON ..
+#    make doc
+#    make selfsigned
+#    cp -r doc ../../
+#    cp ./examples/server_cert.der ../../
+#    cd .. && rm build -rf
+#
+#    # cross compilation only with gcc
+#    if [ "$CC" = "gcc" ]; then
+#        echo "Cross compile release build for MinGW 32 bit"
+#        mkdir -p build && cd build
+#        cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cmake/Toolchain-mingw32.cmake -DUA_ENABLE_AMALGAMATION=ON -DCMAKE_BUILD_TYPE=Release -DUA_BUILD_EXAMPLES=ON ..
+#        make -j
+#        zip -r open62541-win32.zip ../../doc ../../server_cert.der ../LICENSE ../AUTHORS ../README.md examples/server.exe examples/client.exe libopen62541.dll.a open62541.h open62541.c
+#        cp open62541-win32.zip ..
+#        cd .. && rm build -rf
+#
+#        echo "Cross compile release build for MinGW 64 bit"
+#        mkdir -p build && cd build
+#        cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cmake/Toolchain-mingw64.cmake -DUA_ENABLE_AMALGAMATION=ON -DCMAKE_BUILD_TYPE=Release -DUA_BUILD_EXAMPLES=ON ..
+#        make -j
+#        zip -r open62541-win64.zip ../../doc ../../server_cert.der ../LICENSE ../AUTHORS ../README.md examples/server.exe examples/client.exe libopen62541.dll.a open62541.h open62541.c
+#        cp open62541-win64.zip ..
+#        cd .. && rm build -rf
+#
+#        echo "Cross compile release build for 32-bit linux"
+#        mkdir -p build && cd build
+#        cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cmake/Toolchain-gcc-m32.cmake -DUA_ENABLE_AMALGAMATION=ON -DCMAKE_BUILD_TYPE=Release -DUA_BUILD_EXAMPLES=ON ..
+#        make -j
+#        tar -pczf open62541-linux32.tar.gz ../../doc ../../server_cert.der ../LICENSE ../AUTHORS ../README.md examples/server examples/client libopen62541.a open62541.h open62541.c
+#        cp open62541-linux32.tar.gz ..
+#        cd .. && rm build -rf
+#    fi
+#
+#    echo "Compile release build for 64-bit linux"
+#    mkdir -p build && cd build
+#    cmake -DCMAKE_BUILD_TYPE=Release -DUA_ENABLE_AMALGAMATION=ON -DUA_BUILD_EXAMPLES=ON ..
+#    make -j
+#    tar -pczf open62541-linux64.tar.gz ../../doc ../../server_cert.der ../LICENSE ../AUTHORS ../README.md examples/server examples/client libopen62541.a open62541.h open62541.c
+#    cp open62541-linux64.tar.gz ..
+#    cp open62541.h .. # copy single file-release
+#    cp open62541.c .. # copy single file-release
+#    cd .. && rm build -rf
+#
+#    if [ "$CC" = "gcc" ]; then
+#        echo "Upgrade to gcc 4.8"
+#        export CXX="g++-4.8" CC="gcc-4.8"
+#    fi
+#
+#    echo "Building the C++ example"
+#    mkdir -p build && cd build
+#    cp ../open62541.* .
+#    gcc-4.8 -std=c99 -c open62541.c
+#    g++-4.8 ../examples/server.cpp -I./ open62541.o -lrt -o cpp-server
+#    cd .. && rm build -rf
+#
+#    echo "Compile multithreaded version"
+#    mkdir -p build && cd build
+#    cmake -DUA_ENABLE_MULTITHREADING=ON -DUA_BUILD_EXAMPLES=ON ..
+#    make -j
+#    cd .. && rm build -rf
+#
+#    echo "Compile without discovery version"
+#    mkdir -p build && cd build
+#    cmake -DUA_ENABLE_DISCOVERY=OFF -DUA_ENABLE_DISCOVERY_MULTICAST=OFF -DUA_BUILD_EXAMPLES=ON ..
+#    make -j
+#    cd .. && rm build -rf
+#
+#    echo "Compile discovery without multicast version"
+#    mkdir -p build && cd build
+#    cmake -DUA_ENABLE_DISCOVERY=ON -DUA_ENABLE_DISCOVERY_MULTICAST=OFF -DUA_BUILD_EXAMPLES=ON ..
+#    make -j
+#    cd .. && rm build -rf
 
     #this run inclides full examples and methodcalls
     echo "Debug build and unit tests (64 bit)"
