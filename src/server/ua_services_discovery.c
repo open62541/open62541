28546a29eebfa730baf3733917050b37c9782960
Stefan Profanter <git@s.profanter.me>
2016-08-23 11:58:03 +0200

    fixup

--- src/server/ua_services_discovery.c
+++ src/server/ua_services_discovery.c
@@ -1204,7 +1204,7 @@ UA_Discovery_addRecord(UA_Server* server, const char* servername, const char* ho
 
     // [servername]-[hostname]._opcua-tcp._tcp.local.
     char *fullServiceDomain;
-    if (!(fullServiceDomain = create_fullServiceDomain("asdfas saf sa fsdafwefwergt wg weg wegw3 we 34 w4 t34 eswgweg  0,65 s123456789", hostname, 63))) {
+    if (!(fullServiceDomain = create_fullServiceDomain(servername, hostname, 63))) {
         return UA_STATUSCODE_BADOUTOFMEMORY;
     }
 
