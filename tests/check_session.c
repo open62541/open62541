2946a30334fcdc94cfec83f34fee543a67eabe82
Stefan Profanter <git@s.profanter.me>
2016-08-24 14:38:13 +0200

    untabify
    Command used:
    `find . \( -name '*.c' -o -name '*.h' \) -not \( -path "./deps/*" -prune \) ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;`

--- tests/check_session.c
+++ tests/check_session.c
@@ -7,8 +7,8 @@
 
 START_TEST(Session_init_ShallWork)
 {
-	UA_Session session;
-	UA_Session_init(&session);
+    UA_Session session;
+    UA_Session_init(&session);
 
     UA_NodeId tmpNodeId;
     UA_NodeId_init(&tmpNodeId);
@@ -16,9 +16,9 @@ START_TEST(Session_init_ShallWork)
     UA_ApplicationDescription_init(&tmpAppDescription);
     UA_DateTime tmpDateTime;
     UA_DateTime_init(&tmpDateTime);
-	ck_assert_int_eq(session.activated,false);
-	ck_assert_int_eq(session.authenticationToken.identifier.numeric,tmpNodeId.identifier.numeric);
-	ck_assert_int_eq(session.availableContinuationPoints,MAXCONTINUATIONPOINTS);
+    ck_assert_int_eq(session.activated,false);
+    ck_assert_int_eq(session.authenticationToken.identifier.numeric,tmpNodeId.identifier.numeric);
+    ck_assert_int_eq(session.availableContinuationPoints,MAXCONTINUATIONPOINTS);
     ck_assert_ptr_eq(session.channel,NULL);
     ck_assert_ptr_eq(session.clientDescription.applicationName.locale.data,NULL);
     ck_assert_ptr_eq(session.continuationPoints.lh_first, NULL);
@@ -33,40 +33,40 @@ END_TEST
 
 START_TEST(Session_updateLifetime_ShallWork)
 {
-	UA_Session session;
-	UA_Session_init(&session);
+    UA_Session session;
+    UA_Session_init(&session);
     UA_DateTime tmpDateTime;
     tmpDateTime = session.validTill;
-	UA_Session_updateLifetime(&session);
+    UA_Session_updateLifetime(&session);
 
-	UA_Int32 result = (session.validTill > tmpDateTime);
-	ck_assert_int_gt(result,0);
+    UA_Int32 result = (session.validTill > tmpDateTime);
+    ck_assert_int_gt(result,0);
 }
 END_TEST
 
 static Suite* testSuite_Session(void) {
-	Suite *s = suite_create("Session");
-	TCase *tc_core = tcase_create("Core");
-	tcase_add_test(tc_core, Session_init_ShallWork);
-	tcase_add_test(tc_core, Session_updateLifetime_ShallWork);
+    Suite *s = suite_create("Session");
+    TCase *tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, Session_init_ShallWork);
+    tcase_add_test(tc_core, Session_updateLifetime_ShallWork);
 
-	suite_add_tcase(s,tc_core);
-	return s;
+    suite_add_tcase(s,tc_core);
+    return s;
 }
 
 int main(void) {
-	int number_failed = 0;
+    int number_failed = 0;
 
-	Suite *s;
-	SRunner *sr;
+    Suite *s;
+    SRunner *sr;
 
-	s = testSuite_Session();
-	sr = srunner_create(s);
-	srunner_run_all(sr,CK_NORMAL);
-	number_failed += srunner_ntests_failed(sr);
-	srunner_free(sr);
+    s = testSuite_Session();
+    sr = srunner_create(s);
+    srunner_run_all(sr,CK_NORMAL);
+    number_failed += srunner_ntests_failed(sr);
+    srunner_free(sr);
 
-	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
 
