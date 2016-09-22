260f838bf63f0197e985721b8917fabaeefd4c21
Stefan Profanter <git@s.profanter.me>
2016-09-22 15:29:30 +0200

    Fixup

--- tools/travis/travis_linux_after_success.sh
+++ tools/travis/travis_linux_after_success.sh
@@ -7,14 +7,16 @@ if [ $ANALYZE = "true" ]; then
 else
     echo "=== Executing after_success scripts ==="
 
-    if [ "$CC" = "gcc" && "${TRAVIS_REPO_SLUG}" = "open62541/open62541" ]; then
-        if [ ${TRAVIS_PULL_REQUEST} == "false" ]; then
-            if [ ${TRAVIS_BRANCH} == "0.2" ]; then
+    if [ "$CC" = "gcc" ] && [ "${TRAVIS_REPO_SLUG}" = "open62541/open62541" ]; then
+        if [ ${TRAVIS_PULL_REQUEST} = "false" ]; then
+            if [ ${TRAVIS_BRANCH} = "0.2" ]; then
                 sh ./tools/travis/travis_push_doc.sh
             fi
-            if [ ${TRAVIS_BRANCH} == "master" ]; then
+            if [ ${TRAVIS_BRANCH} = "master" ]; then
                 sh ./tools/travis/travis_push_coverity.sh
             fi
+        else
+           echo "  Skipping push doc/coverity on pull requests"
         fi
 
         sh ./tools/travis/travis_push_release.sh;
