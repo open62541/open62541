b602ca5a061226243300a688064a6f1493ef05cf
Julian Grothoff <j.grothoff@plt.rwth-aachen.de>
2016-10-03 16:01:45 +0200

    Bugfix warning malloc.

--- src/server/ua_nodestore_switch.c
+++ src/server/ua_nodestore_switch.c
@@ -13,7 +13,7 @@ UA_NodestoreSwitch *
 UA_NodestoreSwitch_new(){
     nodestoreSwitch = UA_malloc(sizeof(UA_NodestoreSwitch));
     nodestoreSwitch->nodestoreInterfacesSize = 0;
-    nodestoreSwitch->nodestoreInterfaces = UA_malloc(2*sizeof(UA_NodestoreInterface));
+    nodestoreSwitch->nodestoreInterfaces = UA_malloc(2*sizeof(UA_NodestoreInterface*));
     return nodestoreSwitch;
 }
 
