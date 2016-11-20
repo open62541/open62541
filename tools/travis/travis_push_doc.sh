fbfd7e14d6af267bd5d7adc4b29d1d6ad3686d74
Julius Pfrommer <julius.pfrommer@web.de>
2016-11-20 18:43:09 +0100

    fix: mkdir command failed during push_docs

--- tools/travis/travis_push_doc.sh
+++ tools/travis/travis_push_doc.sh
@@ -5,7 +5,6 @@ git clone --depth=5 -b gh-pages https://$GITAUTH@github.com/open62541/open62541-
 cd open62541-www
 
 rm -r -f ./doc/current/* || true # ignore result
-mkdir ./doc/current
 cp -r ../../doc/* ./doc/current/
 cp -r ../../doc_latex/open62541.pdf ./doc/open62541-current.pdf
 git add -A ./doc
