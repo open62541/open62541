49964e1c0356d1da0252103a91ee7b01558be239
olivier R-D <olivier@loke>
2016-12-26 13:49:33 +0100

    starting server works

--- python_wrapper/openua.pyx
+++ python_wrapper/openua.pyx
@@ -7,13 +7,17 @@ cdef class Server:
     cdef copenua.UA_Server* _c_server
     cdef copenua.UA_Boolean _c_running
 
-    def __cinit__(self):
+    def start(self):
         c_con_conf = copenua.UA_ConnectionConfig_standard
         self._c_nl = copenua.UA_ServerNetworkLayerTCP(c_con_conf, 4840) 
         self._c_conf = copenua.UA_ServerConfig_standard
         self._c_conf.networkLayers = &self._c_nl
         self._c_conf.networkLayersSize = 1
         self._c_server = copenua.UA_Server_new(self._c_conf)
+        self._c_running = True
         copenua.UA_Server_run(self._c_server, &self._c_running)
+
+    def stop(self):
+        self._c_running = False
 
 
