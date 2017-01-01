b3cf4941a455efdf2e372cd07c781734442bf10d
Julius Pfrommer <julius.pfrommer@web.de>
2017-01-01 13:00:06 +0100

    mark clang tools as advanced cmake options

--- tools/cmake/FindClangTools.cmake
+++ tools/cmake/FindClangTools.cmake
@@ -7,9 +7,11 @@
 #  CLANG_FORMAT_FOUND, Whether clang format was found
 
 find_program(CLANG_TIDY_PROGRAM
-  NAMES clang-tidy-3.9 clang-tidy-3.8 clang-tidy-3.7 clang-tidy-3.6  clang-tidy
+  NAMES clang-tidy-3.9 clang-tidy-3.8 clang-tidy-3.7 clang-tidy-3.6 clang-tidy
   PATHS $ENV{CLANG_TOOLS_PATH} /usr/local/bin /usr/bin
   NO_DEFAULT_PATH)
+
+mark_as_advanced(CLANG_TIDY_PROGRAM)
 
 if("${CLANG_TIDY_PROGRAM}" STREQUAL "CLANG_TIDY_PROGRAM-NOTFOUND")
   set(CLANG_TIDY_FOUND 0)
@@ -18,9 +20,11 @@ else()
 endif()
 
 find_program(CLANG_FORMAT_PROGRAM
-  NAMES clang-format 3.9 clang-format-3.8 clang-format-3.7 clang-format-3.6  clang-format
+  NAMES clang-format 3.9 clang-format-3.8 clang-format-3.7 clang-format-3.6 clang-format
   PATHS $ENV{CLANG_TOOLS_PATH} /usr/local/bin /usr/bin
   NO_DEFAULT_PATH)
+
+mark_as_advanced(CLANG_FORMAT_PROGRAM)
 
 if("${CLANG_FORMAT_PROGRAM}" STREQUAL "CLANG_FORMAT_PROGRAM-NOTFOUND")
   set(CLANG_FORMAT_FOUND 0)
