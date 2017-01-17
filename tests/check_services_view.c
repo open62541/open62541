2146c186deceaa22a62bb2dd852b1a5d7d8a8ebd
Julius Pfrommer <julius.pfrommer@web.de>
2017-01-17 12:24:58 +0100

    add test whether browse returns a browsename

--- tests/check_services_view.c
+++ tests/check_services_view.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "ua_types.h"
-#include "server/ua_services.h"
 #include "check.h"
+#include "ua_server.h"
+#include "ua_config_standard.h"
 
-/* START_TEST(Service_TranslateBrowsePathsToNodeIds_SmokeTest)
+START_TEST(Service_Browse_WithBrowseName)
 {
-    UA_TranslateBrowsePathsToNodeIdsRequest request;
-    UA_TranslateBrowsePathsToNodeIdsRequest_init(&request);
+    UA_Server * server = UA_Server_new(UA_ServerConfig_standard);
 
-    UA_TranslateBrowsePathsToNodeIdsResponse response;
-    UA_TranslateBrowsePathsToNodeIdsResponse_init(&response);
+    UA_BrowseDescription bd;
+    UA_BrowseDescription_init(&bd);
+    bd.resultMask = UA_BROWSERESULTMASK_BROWSENAME;
+    bd.nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    bd.referenceTypeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+    bd.browseDirection = UA_BROWSEDIRECTION_FORWARD;
 
-    request.browsePathsSize = 1;
-    UA_Array_new((void**)&request.browsePaths,request.browsePathsSize, &UA_.types[UA_BROWSEPATH]);
+    UA_BrowseResult br = UA_Server_browse(server, 0, &bd);
 
-    Service_TranslateBrowsePathsToNodeIds(NULL,&request,&response);
+    ck_assert_int_eq(br.statusCode, UA_STATUSCODE_GOOD);
+    ck_assert(br.referencesSize > 0);
+    ck_assert(!UA_String_equal(&br.references[0].browseName.name, &UA_STRING_NULL));
 
-    ck_assert_int_eq(response.resultsSize,request.browsePathsSize);
-    ck_assert_int_eq(response.results[0].statusCode,UA_STATUSCODE_BADNOMATCH);
-
-    //finally
-    UA_TranslateBrowsePathsToNodeIdsRequest_deleteMembers(&request);
-    UA_TranslateBrowsePathsToNodeIdsResponse_deleteMembers(&response);
+    UA_BrowseResult_deleteMembers(&br);
+    UA_Server_delete(server);
 }
-END_TEST */
+END_TEST
 
 static Suite* testSuite_Service_TranslateBrowsePathsToNodeIds(void) {
     Suite *s = suite_create("Service_TranslateBrowsePathsToNodeIds");
-    TCase *tc_core = tcase_create("Core");
-    //tcase_add_test(tc_core, Service_TranslateBrowsePathsToNodeIds_SmokeTest);
-    suite_add_tcase(s,tc_core);
+    TCase *tc_browse = tcase_create("Browse Service");
+    tcase_add_test(tc_browse, Service_Browse_WithBrowseName);
+
+    suite_add_tcase(s,tc_browse);
     return s;
 }
 
@@ -43,7 +44,8 @@ int main(void) {
 
     s = testSuite_Service_TranslateBrowsePathsToNodeIds();
     sr = srunner_create(s);
-    srunner_run_all(sr,CK_NORMAL);
+    srunner_set_fork_status(sr, CK_NOFORK);
+    srunner_run_all(sr, CK_NORMAL);
     number_failed += srunner_ntests_failed(sr);
     srunner_free(sr);
 
