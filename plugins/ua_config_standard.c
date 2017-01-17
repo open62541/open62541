9ffcb71abc1afb189c3efdd13e508677060a2328
Stefan Profanter <git@s.profanter.me>
2017-01-17 17:14:26 +0100

    Fixup after merge

--- plugins/ua_config_standard.c
+++ plugins/ua_config_standard.c
@@ -66,12 +66,12 @@ const UA_EXPORT UA_ServerConfig UA_ServerConfig_standard = {
       UA_STRING_STATIC_NULL,
       UA_STRING_STATIC_NULL,
       0, NULL }, /* .applicationDescription */
+	UA_STRING_STATIC_NULL, /* .serverCertificate */
 #ifdef UA_ENABLE_DISCOVERY
-	  NULL, /* serverCapabilities */
-	  0, /* serverCapabilitiesSize */
-	  UA_STRING_STATIC_NULL, /* mdnsServerName */
+	UA_STRING_STATIC_NULL, /* mdnsServerName */
+	NULL, /* serverCapabilities */
+	0, /* serverCapabilitiesSize */
 #endif
-    UA_STRING_STATIC_NULL, /* .serverCertificate */
 
 	/* Custom DataTypes */
 	0, /* .customDataTypesSize */
