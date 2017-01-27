44d29026dc481d036e76444cc830d3bad3ac939f
FlorianPalm <palm@plt.rwth-aachen.de>
2017-01-27 18:58:50 +0100

    removed license note from files of the plugins folder

--- plugins/ua_config_standard.c
+++ plugins/ua_config_standard.c
@@ -1,6 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public 
-* License, v. 2.0. If a copy of the MPL was not distributed with this 
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */ 
 /* This work is licensed under a Creative Commons CCZero 1.0 Universal License.
  * See http://creativecommons.org/publicdomain/zero/1.0/ for more information. */
 
