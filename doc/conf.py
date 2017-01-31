4f3bd820650a821e63db90fdd7b52a04d1f22da0
FlorianPalm <palm@plt.rwth-aachen.de>
2017-01-31 17:46:24 +0100

    Revert "added license note in python files"
    This reverts commit 39cb289927ec36724de9373e099aa21b544b89ba.

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public 
- * License, v. 2.0. If a copy of the MPL was not distributed with this 
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 # -*- coding: utf-8 -*-
 #
 # open62541 documentation build configuration file, created by
