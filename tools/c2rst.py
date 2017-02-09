94c30b74ca949c1577763cc82e0c5a0ecb9319f1
Julius Pfrommer <julius.pfrommer@web.de>
2017-02-09 22:18:45 +0100

    generate tutorial docs from compilable c-files

--- tools/c2rst.py
+++ tools/c2rst.py
@@ -57,7 +57,7 @@ def last_line(c):
     for i in range(last-1,1,-1):
         if len(c[i].strip()) > 0:
             return i
-    return 1
+    return len(c)-1
 
 if len(sys.argv) < 2:
     print("Usage: python c2rst.py input.c/h output.rst")
