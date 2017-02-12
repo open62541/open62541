810db94bcaac2a4d8b4e0577046d5440b2157095
FlorianPalm <palm@plt.rwth-aachen.de>
2017-02-12 08:05:53 +0100

    added MPL v2 license to c/h/py files

--- src/server/ua_securechannel_manager.h
+++ src/server/ua_securechannel_manager.h
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+*  License, v. 2.0. If a copy of the MPL was not distributed with this 
+*  file, You can obtain one at http://mozilla.org/MPL/2.0/.*/
+
 #ifndef UA_CHANNEL_MANAGER_H_
 #define UA_CHANNEL_MANAGER_H_
 
