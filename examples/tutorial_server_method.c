c91cedb29a8651b1fef59bae7c3914e44add14f6
Stefan Profanter <git@s.profanter.me>
2017-03-07 15:12:01 +0100

    Fix build on VS 2008

--- examples/tutorial_server_method.c
+++ examples/tutorial_server_method.c
@@ -41,7 +41,7 @@ helloWorldMethodCallback(void *handle, const UA_NodeId *objectId,
     UA_String *inputStr = (UA_String*)input->data;
     UA_String tmp = UA_STRING_ALLOC("Hello ");
     if(inputStr->length > 0) {
-        tmp.data = realloc(tmp.data, tmp.length + inputStr->length);
+        tmp.data = (UA_Byte *)realloc(tmp.data, tmp.length + inputStr->length);
         memcpy(&tmp.data[tmp.length], inputStr->data, inputStr->length);
         tmp.length += inputStr->length;
     }
