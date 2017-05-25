862d009420ef16b617c2178be76acc60ec7e4108
Bostjan Vesnicer <bostjan.vesnicer@gmail.com>
2017-05-25 08:54:48 +0100

    Improve binary encoding/chunking tests

--- tests/check_chunking.c
+++ tests/check_chunking.c
@@ -108,12 +108,58 @@ START_TEST(encodeStringIntoFiveChunksShallWork) {
 }
 END_TEST
 
+START_TEST(encodeTwoStringsIntoTenChunksShallWork) {
+
+    size_t stringLength = 143; //number of elements within the array which should be encoded
+    size_t offset = 0; // encoding offset
+    size_t chunkCount = 10; // maximum chunk count
+    size_t chunkSize = 30; //size in bytes of each chunk
+
+    UA_String string;
+    UA_ChunkInfo ci;
+    bufIndex = 0;
+    counter = 0;
+    dataCount = 0;
+    UA_String_init(&string);
+    string.data = malloc(stringLength);
+    string.length = stringLength;
+    char tmpString[9] = {'o','p','e','n','6','2','5','4','1'};
+    //char tmpString[9] = {'1','4','5','2','6','n','e','p','o'};
+    buffers = UA_Array_new(chunkCount, &UA_TYPES[UA_TYPES_BYTESTRING]);
+    for(size_t i=0;i<chunkCount;i++){
+        UA_ByteString_allocBuffer(&buffers[i],chunkSize);
+    }
+
+    UA_ByteString workingBuffer=buffers[0];
+
+    for(size_t i=0;i<stringLength;i++){
+        size_t tmp = i % 9;
+        string.data[i] =  tmpString[tmp];
+    }
+
+    UA_StatusCode retval = UA_encodeBinary(&string,&UA_TYPES[UA_TYPES_STRING],(UA_exchangeEncodeBuffer)sendChunkMockUp,&ci,&workingBuffer,&offset);
+    ck_assert_uint_eq(retval,UA_STATUSCODE_GOOD);
+    ck_assert_int_eq(counter,4); //5 chunks allocated - callback called 4 times
+    ck_assert_int_eq(UA_calcSizeBinary(&string,&UA_TYPES[UA_TYPES_STRING]), dataCount + offset);
+
+    retval = UA_encodeBinary(&string,&UA_TYPES[UA_TYPES_STRING],(UA_exchangeEncodeBuffer)sendChunkMockUp,&ci,&workingBuffer,&offset);
+    dataCount += offset; //last piece of data - no callback was called
+    ck_assert_uint_eq(retval,UA_STATUSCODE_GOOD);
+    ck_assert_int_eq(counter,9); //10 chunks allocated - callback called 4 times
+    ck_assert_int_eq(2 * UA_calcSizeBinary(&string,&UA_TYPES[UA_TYPES_STRING]), dataCount);
+
+    UA_Array_delete(buffers, chunkCount, &UA_TYPES[UA_TYPES_BYTESTRING]);
+    UA_String_deleteMembers(&string);
+}
+END_TEST
+
 
 static Suite *testSuite_builtin(void) {
     Suite *s = suite_create("Chunked encoding");
     TCase *tc_message = tcase_create("encode chunking");
     tcase_add_test(tc_message,encodeArrayIntoFiveChunksShallWork);
     tcase_add_test(tc_message,encodeStringIntoFiveChunksShallWork);
+    tcase_add_test(tc_message,encodeTwoStringsIntoTenChunksShallWork);
     suite_add_tcase(s, tc_message);
     return s;
 }
