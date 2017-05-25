f74a41b619ffa20eec8aac0a6d2cbfc979ff2177
Bostjan Vesnicer <bostjan.vesnicer@gmail.com>
2017-05-25 08:54:48 +0100

    Improve binary encoding/chunking tests

--- tests/check_client.c
+++ tests/check_client.c
@@ -18,6 +18,32 @@ UA_Boolean *running;
 UA_ServerNetworkLayer nl;
 pthread_t server_thread;
 
+static void
+addVariable(size_t size) {
+    /* Define the attribute of the myInteger variable node */
+    UA_VariableAttributes attr;
+    UA_VariableAttributes_init(&attr);
+    UA_Int32* array = malloc(size * sizeof(UA_Int32));
+    memset(array, 0, size * sizeof(UA_Int32));
+    UA_Variant_setArray(&attr.value, array, size, &UA_TYPES[UA_TYPES_INT32]);
+
+    char name[] = "my.variable";
+    attr.description = UA_LOCALIZEDTEXT("en_US", name);
+    attr.displayName = UA_LOCALIZEDTEXT("en_US", name);
+    attr.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
+
+    /* Add the variable node to the information model */
+    UA_NodeId myIntegerNodeId = UA_NODEID_STRING(1, name);
+    UA_QualifiedName myIntegerName = UA_QUALIFIEDNAME(1, name);
+    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+    UA_Server_addVariableNode(server, myIntegerNodeId, parentNodeId,
+                              parentReferenceNodeId, myIntegerName,
+                              UA_NODEID_NULL, attr, NULL, NULL);
+
+    free(array);
+}
+
 static void * serverloop(void *_) {
     while(*running)
         UA_Server_run_iterate(server, true);
@@ -33,6 +59,7 @@ static void setup(void) {
     config.networkLayersSize = 1;
     server = UA_Server_new(config);
     UA_Server_run_startup(server);
+    addVariable(16366);
     pthread_create(&server_thread, NULL, serverloop, NULL);
 }
 
@@ -56,11 +83,29 @@ START_TEST(Client_connect) {
 }
 END_TEST
 
+START_TEST(Client_read) {
+    UA_Client *client = UA_Client_new(UA_ClientConfig_standard);
+    UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://localhost:16664");
+    ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
+
+    UA_Variant val;
+    UA_NodeId nodeId = UA_NODEID_STRING(1, "my.variable");
+    retval = UA_Client_readValueAttribute(client, nodeId, &val);
+    ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
+
+    UA_Variant_deleteMembers(&val);
+
+    UA_Client_disconnect(client);
+    UA_Client_delete(client);
+}
+END_TEST
+
 static Suite* testSuite_Client(void) {
     Suite *s = suite_create("Client");
     TCase *tc_client = tcase_create("Client Basic");
     tcase_add_checked_fixture(tc_client, setup, teardown);
     tcase_add_test(tc_client, Client_connect);
+    tcase_add_test(tc_client, Client_read);
     suite_add_tcase(s,tc_client);
     return s;
 }
