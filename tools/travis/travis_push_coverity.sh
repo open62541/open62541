f43492305622d1a231c47e0b324414cd55f495b8
Stefan Profanter <git@s.profanter.me>
2017-07-20 13:23:44 +0200

    Push master to coverity_scan

--- tools/travis/travis_push_coverity.sh
+++ tools/travis/travis_push_coverity.sh
@@ -4,9 +4,11 @@ set -ev
 # This script is run by travis-ci and pushes the first commit
 # of the day to the coverity scan service
 
+echo "=== Pushing master to coverity_scan branch ==="
+
 COMMITS=`git log --oneline --since=today.midnight | wc -l`
 if [ "$COMMITS" -le 1 ]; then
-    git push https://$GITAUTH@github.com/open62541/open62541 0.2:coverity_scan
+    git push https://$GITAUTH@github.com/open62541/open62541 master:coverity_scan
 else
     echo "Not the first commit of the day - no push to coverity required"
 fi
