0e6d5a9a566b74c8f99c4a8c0053af9bca1cd1e2
Stefan Profanter <git@s.profanter.me>
2017-08-02 10:06:38 +0200

    Add DUMP_HEX_PKG function to allow easier fuzz debugging

--- src/ua_util.h
+++ src/ua_util.h
@@ -128,6 +128,10 @@ size_t UA_readNumber(u8 *buf, size_t buflen, u32 *number);
 # define UA_TYPENAME(name)
 #endif
 
+#ifdef UA_DEBUG_DUMP_PKGS
+void UA_EXPORT UA_dump_hex_pkg(UA_Byte* buffer, size_t bufferLen);
+#endif
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
