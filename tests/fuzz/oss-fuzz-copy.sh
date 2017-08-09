c2ffc4cae98b90244fcaecbcdb69079be88eda71
Stefan Profanter <git@s.profanter.me>
2017-08-09 10:17:05 +0200

    Improve fuzzing corpus by adding more binaries

--- tests/fuzz/oss-fuzz-copy.sh
+++ tests/fuzz/oss-fuzz-copy.sh
@@ -12,6 +12,19 @@ for F in $fuzzerFiles; do
 	fuzzerName=$(basename $F .cc)
 
 	if [ -d "$SRC/open62541/tests/fuzz/${fuzzerName}_corpus" ]; then
+
+		# first combine any files in subfolders to one single binary file
+		subDirs=$(find $SRC/open62541/tests/fuzz/${fuzzerName}_corpus -maxdepth 1 -mindepth 1 -type d)
+		for dirPath in $subDirs; do
+			dir=$(basename $dirPath)
+			outPath=$SRC/open62541/tests/fuzz/${fuzzerName}_corpus/$dir.bin
+			if [ -f $outPath ]; then
+				rm $outPath;
+			fi
+			echo "Combining content of $dir into $outPath"
+			cat $dirPath/*.bin > $outPath
+		done
+
 		zip -j $OUT/${fuzzerName}_seed_corpus.zip $SRC/open62541/tests/fuzz/${fuzzerName}_corpus/*
 	fi
 done
