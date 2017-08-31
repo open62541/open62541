baa2e925da2245c0422fb027005f7c39c3295003
Jens Reimann <jreimann@redhat.com>
2017-08-31 17:08:16 +0200

    Make changes for a Fedora RPM
    This change adds an RPM file and makes some
    minor modifications to the build in order to create
    an RPM file which passed the Fedora review process.
    
    For this spec file to work there has to be a tag named
    `open62541-<version>`, e.g. `open62541-0.2`. However this
    can be locally emulated by providing a sources file of the
    same name, e.g. `open62541-0.2.tar.gz`.
    
    An easier option is to use `tito` for a local build and
    execute `tito --test --rpm` in order to build a local version
    of the latest git commit automatically.
    
    Also see: https://bugzilla.redhat.com/show_bug.cgi?id=1487578
    
    Conflicts:
    	CMakeLists.txt

--- doc/conf.py
+++ doc/conf.py
@@ -19,6 +19,7 @@
 import sys
 import os
 import shlex
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -123,7 +124,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
