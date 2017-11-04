2b8177831942ce298fab2933763c055d3d8e2c77
Julius Pfrommer <julius.pfrommer@web.de>
2017-11-04 12:28:45 +0100

    improve networking docs

--- include/ua_plugin_network.h
+++ include/ua_plugin_network.h
@@ -183,15 +183,15 @@ struct UA_ServerNetworkLayer {
 /**
  * Client Network Layer
  * --------------------
- * The client has only a single connection. The connection is used for both
- * sending and receiving binary messages.
- * @param localConf the connection config for this client
- * @param endpointUrl to where to connect
- * @param timeout in ms until the connection try times out if remote not reachable
- * */
+ * The client has only a single connection used for sending and receiving binary
+ * messages. */
 
+/* @param localConf the connection config for this client
+ * @param endpointUrl to where to connect
+ * @param timeout in ms until the connection try times out if remote not reachable */
 typedef UA_Connection
-(*UA_ConnectClientConnection)(UA_ConnectionConfig localConf, const char *endpointUrl, const UA_UInt32 timeout);
+(*UA_ConnectClientConnection)(UA_ConnectionConfig localConf, const char *endpointUrl,
+                              const UA_UInt32 timeout);
 
 /**
  * Endpoint URL Parser
