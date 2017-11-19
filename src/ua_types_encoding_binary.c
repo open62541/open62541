9165b056a95f3ff5db23dbf9628acb1efee6f86b
Stefan Profanter <git@s.profanter.me>
2017-11-19 15:51:51 +0100

    Fix double free of node id
    See https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=4296
    
    Credit to oss-fuzz

--- src/ua_types_encoding_binary.c
+++ src/ua_types_encoding_binary.c
@@ -943,7 +943,7 @@ ExtensionObject_decodeBinaryContent(UA_ExtensionObject *dst, const UA_NodeId *ty
     /* Unknown type, just take the binary content */
     if(!type) {
         dst->encoding = UA_EXTENSIONOBJECT_ENCODED_BYTESTRING;
-        dst->content.encoded.typeId = *typeId;
+        UA_NodeId_copy(typeId, &dst->content.encoded.typeId);
         return ByteString_decodeBinary(&dst->content.encoded.body);
     }
 
