7c0ce2f3934b5569182e50ac0b73a85fbb59307b
Christian von Arnim <christian.von-arnim@isw.uni-stuttgart.de>
2017-11-29 13:17:31 +0100

    Fix: declare useBlockingSocket in UA_ConnectionConfig_default

--- plugins/ua_config_default.c
+++ plugins/ua_config_default.c
@@ -39,7 +39,8 @@ const UA_ConnectionConfig UA_ConnectionConfig_default = {
     65535, /* .sendBufferSize, 64k per chunk */
     65535, /* .recvBufferSize, 64k per chunk */
     0, /* .maxMessageSize, 0 -> unlimited */
-    0 /* .maxChunkCount, 0 -> unlimited */
+    0, /* .maxChunkCount, 0 -> unlimited */
+    UA_TRUE /* .useBlockingSocket */
 };
 
 /***************************/
