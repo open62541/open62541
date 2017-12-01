fbc321948c009bcc66ea79f9997e6dbbe1f43344
StalderT <github@netsolux.ch>
2017-12-01 15:56:06 +0100

    close connection when close secure channel

--- src/server/ua_services_securechannel.c
+++ src/server/ua_services_securechannel.c
@@ -54,5 +54,4 @@ Service_CloseSecureChannel(UA_Server *server, UA_SecureChannel *channel) {
     UA_LOG_INFO_CHANNEL(server->config.logger, channel, "CloseSecureChannel");
     UA_SecureChannelManager_close(&server->secureChannelManager,
                                   channel->securityToken.channelId);
-    channel->connection->close(channel->connection);
 }
