5ea828db660a73a9fd1faa044a1f24bbfea4b3f8
Mark <mark.giraud@t-online.de>
2018-03-12 11:18:45 +0100

    Add a config creation function that includes all policies

--- plugins/ua_config_default.h
+++ plugins/ua_config_default.h
@@ -40,6 +40,7 @@ UA_EXPORT UA_ServerConfig *
 UA_ServerConfig_new_minimal(UA_UInt16 portNumber, const UA_ByteString *certificate);
 
 #ifdef UA_ENABLE_ENCRYPTION
+
 UA_EXPORT UA_ServerConfig *
 UA_ServerConfig_new_basic128rsa15(UA_UInt16 portNumber,
                                   const UA_ByteString *certificate,
@@ -57,6 +58,16 @@ UA_ServerConfig_new_basic256sha256(UA_UInt16 portNumber,
                                    size_t trustListSize,
                                    const UA_ByteString *revocationList,
                                    size_t revocationListSize);
+
+UA_EXPORT UA_ServerConfig *
+UA_ServerConfig_new_allSecurityPolicies(UA_UInt16 portNumber,
+                                        const UA_ByteString *certificate,
+                                        const UA_ByteString *privateKey,
+                                        const UA_ByteString *trustList,
+                                        size_t trustListSize,
+                                        const UA_ByteString *revocationList,
+                                        size_t revocationListSize);
+
 #endif
 
 /* Creates a server config on the default port 4840 with no server
