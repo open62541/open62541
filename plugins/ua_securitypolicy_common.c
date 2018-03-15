648182c15e5f36c00f73abbd9477a388e059cb0b
StalderT <github@netsolux.ch>
2018-03-15 13:12:56 +0100

    fix deprecated mbedtls functions

--- plugins/ua_securitypolicy_common.c
+++ plugins/ua_securitypolicy_common.c
@@ -4,6 +4,7 @@
  */
 
 #include <mbedtls/sha1.h>
+#include <mbedtls/version.h>
 
 #include "ua_securitypolicy_common.h"
 
@@ -12,8 +13,17 @@ void
 sha1(const unsigned char *input, size_t ilen, unsigned char output[20]) {
     mbedtls_sha1_context sha1Context;
     mbedtls_sha1_init(&sha1Context);
+#if MBEDTLS_VERSION_NUMBER >= 0x02070000
+    // TODO check return status / actually only/always return 0
+    mbedtls_sha1_starts_ret(&sha1Context);
+    // TODO check return status / actually only/always return 0
+    mbedtls_sha1_update_ret(&sha1Context, input, ilen);
+    // TODO check return status / actually only/always return 0
+    mbedtls_sha1_finish_ret(&sha1Context, output);
+#else
     mbedtls_sha1_starts(&sha1Context);
     mbedtls_sha1_update(&sha1Context, input, ilen);
     mbedtls_sha1_finish(&sha1Context, output);
+#endif
     mbedtls_sha1_free(&sha1Context);
 }
