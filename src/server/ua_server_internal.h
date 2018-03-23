c6a975a69d3a2c0e19688aa9e2f5a7eca2049dfc
Julius Pfrommer <julius.pfrommer@web.de>
2018-03-23 09:37:34 +0100

    Set the correct copyright for @jpfr

--- src/server/ua_server_internal.h
+++ src/server/ua_server_internal.h
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
  *
- *    Copyright 2014-2017 (c) Julius Pfrommer, Fraunhofer IOSB
+ *    Copyright 2014-2017 (c) Fraunhofer IOSB (Author: Julius Pfrommer)
  *    Copyright 2014, 2017 (c) Florian Palm
  *    Copyright 2015-2016 (c) Sten Grüner
  *    Copyright 2015 (c) Chris Iatrou
