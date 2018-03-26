dd9696dede3139cb15ad8066364a00936497b0a0
Stefan Profanter <git@s.profanter.me>
2018-03-26 16:33:05 +0200

    Support OPC Foundation LDS server
    Since client encryption landed into master, this was an open todo for
    a long time. The multicast server example was updated to use the encryption
    client.
    
    Fixes #938

--- tests/testing-plugins/testing_clock.h
+++ tests/testing-plugins/testing_clock.h
@@ -21,8 +21,12 @@
 /* Forwards the testing clock by the given duration in ms */
 void UA_fakeSleep(UA_UInt32 duration);
 
-/* Sleep for the duration in milliseconds. Used to wait for workers to complete.
- * Does not do anything in single-threaded mode. */
+/* Sleep for the duration in milliseconds. Used to wait for workers to complete. */
 void UA_realSleep(UA_UInt32 duration);
+
+/* Sleep for the duration in milliseconds and update the current time.
+ * combines fakeSleep and realSleep.
+ * */
+void UA_comboSleep(UA_UInt32 duration);
 
 #endif /* TESTING_CLOCK_H_ */
