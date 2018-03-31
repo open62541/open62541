e3b06b228cb475db2073dff6985cec7035602761
Mark <mark.giraud@t-online.de>
2018-03-31 19:44:14 +0200

    Add common.h to cmake project

--- examples/common.h
+++ examples/common.h
@@ -1,6 +1,8 @@
 /* This work is licensed under a Creative Commons CCZero 1.0 Universal License.
  * See http://creativecommons.org/publicdomain/zero/1.0/ for more information. */
 
+#include "open62541.h"
+
 /* loadFile parses the certificate file.
  *
  * @param  path               specifies the file name given in argv[]
