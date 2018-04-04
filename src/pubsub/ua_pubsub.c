ac41f2f64872ccb5ed9ff615a5ca88bd3bf1fc83
Andreas Ebner <andreas.ebner@iosb.fraunhofer.de>
2018-04-04 14:56:27 +0200

    Add pubsub PublishedDataSet handling
    documentation fixes, code project adjustments
    
    removed hard dependency between main lib and log plugin
    
    pubsub API delte PubSub prefix

--- src/pubsub/ua_pubsub.c
+++ src/pubsub/ua_pubsub.c
@@ -5,6 +5,8 @@
  * Copyright (c) 2017-2018 Fraunhofer IOSB (Author: Andreas Ebner)
  */
 
+#include "ua_server_pubsub.h"
+#include "server/ua_server_internal.h"
 #include "ua_pubsub.h"
 #include "ua_pubsub_manager.h"
 
@@ -35,9 +37,6 @@ UA_PubSubConnectionConfig_copy(const UA_PubSubConnectionConfig *src, UA_PubSubCo
 /**
  * Get the current config of the Connection.
  *
- * @param server
- * @param connectionIdentifier
- * @param config
  * @return UA_STATUSCODE_GOOD on success
  */
 UA_StatusCode
@@ -46,7 +45,7 @@ UA_PubSubConnection_getConfig(UA_Server *server, UA_NodeId connectionIdentifier,
     if(!config)
         return UA_STATUSCODE_BADINVALIDARGUMENT;
 
-    UA_PubSubConnection *currentPubSubConnection = UA_PubSubManager_findConnectionbyId(server, connectionIdentifier);
+    UA_PubSubConnection *currentPubSubConnection = UA_PubSubConnection_findConnectionbyId(server, connectionIdentifier);
     if(!currentPubSubConnection)
         return UA_STATUSCODE_BADNOTFOUND;
 
@@ -55,6 +54,21 @@ UA_PubSubConnection_getConfig(UA_Server *server, UA_NodeId connectionIdentifier,
     UA_PubSubConnectionConfig_copy(currentPubSubConnection->config, &tmpPubSubConnectionConfig);
     *config = tmpPubSubConnectionConfig;
     return UA_STATUSCODE_GOOD;
+}
+
+/**
+ * Find a Connection by the connectionIdentifier.
+ *
+ * @return ptr to Connection or NULL if not found
+ */
+UA_PubSubConnection *
+UA_PubSubConnection_findConnectionbyId(UA_Server *server, UA_NodeId connectionIdentifier) {
+    for(size_t i = 0; i < server->pubSubManager.connectionsSize; i++){
+        if(UA_NodeId_equal(&connectionIdentifier, &server->pubSubManager.connections[i].identifier)){
+            return &server->pubSubManager.connections[i];
+        }
+    }
+    return NULL;
 }
 
 void
@@ -79,4 +93,99 @@ UA_PubSubConnection_delete(UA_PubSubConnection *connection) {
         connection->channel->close(connection->channel);
     }
     UA_free(connection->config);
+}
+
+/**********************************************/
+/*               PublishedDataSet             */
+/**********************************************/
+
+UA_StatusCode
+UA_PublishedDataSetConfig_copy(const UA_PublishedDataSetConfig *src,
+                               UA_PublishedDataSetConfig *dst) {
+    UA_StatusCode retVal = UA_STATUSCODE_GOOD;
+    memcpy(dst, src, sizeof(UA_PublishedDataSetConfig));
+    retVal |= UA_String_copy(&src->name, &dst->name);
+    switch(src->publishedDataSetType){
+        case UA_PUBSUB_DATASET_PUBLISHEDITEMS:
+            //no additional items
+            break;
+        case UA_PUBSUB_DATASET_PUBLISHEDITEMS_TEMPLATE:
+            if (src->config.itemsTemplate.variablesToAddSize > 0){
+                dst->config.itemsTemplate.variablesToAdd = (UA_PublishedVariableDataType *) UA_calloc(
+                        src->config.itemsTemplate.variablesToAddSize, sizeof(UA_PublishedVariableDataType));
+            }
+            for(size_t i = 0; i < src->config.itemsTemplate.variablesToAddSize; i++){
+                retVal |= UA_PublishedVariableDataType_copy(&src->config.itemsTemplate.variablesToAdd[i], &dst->config.itemsTemplate.variablesToAdd[i]);
+            }
+            retVal |= UA_DataSetMetaDataType_copy(&src->config.itemsTemplate.metaData, &dst->config.itemsTemplate.metaData);
+            break;
+        default:
+            return UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+    return retVal;
+}
+
+/**
+ * Get the current config of the PublishedDataSetField.
+ *
+ * @return UA_STATUSCODE_GOOD on success
+ */
+UA_StatusCode
+UA_PublishedDataSet_getConfig(UA_Server *server, UA_NodeId publishedDataSetIdentifier,
+                              UA_PublishedDataSetConfig *config){
+    if(!config)
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+
+    UA_PublishedDataSet *currentPublishedDataSet = UA_PublishedDataSet_findPDSbyId(server,
+                                                                                         publishedDataSetIdentifier);
+    if(!currentPublishedDataSet)
+        return UA_STATUSCODE_BADNOTFOUND;
+
+    UA_PublishedDataSetConfig tmpPublishedDataSetConfig;
+    //deep copy of the actual config
+    UA_PublishedDataSetConfig_copy(&currentPublishedDataSet->config, &tmpPublishedDataSetConfig);
+    *config = tmpPublishedDataSetConfig;
+    return UA_STATUSCODE_GOOD;
+}
+
+/**
+ * Get PDS by the dataSetIdentifier.
+ *
+ * @return ptr to PDS or NULL if not found
+ */
+UA_PublishedDataSet *
+UA_PublishedDataSet_findPDSbyId(UA_Server *server, UA_NodeId identifier){
+    for(size_t i = 0; i < server->pubSubManager.publishedDataSetsSize; i++){
+        if(UA_NodeId_equal(&server->pubSubManager.publishedDataSets[i].identifier, &identifier)){
+            return &server->pubSubManager.publishedDataSets[i];
+        }
+    }
+    return NULL;
+}
+
+void UA_PublishedDataSetConfig_deleteMembers(UA_PublishedDataSetConfig *pdsConfig){
+    //delete pds config
+    UA_String_deleteMembers(&pdsConfig->name);
+    switch (pdsConfig->publishedDataSetType){
+        case UA_PUBSUB_DATASET_PUBLISHEDITEMS:
+            //no additional items
+            break;
+        case UA_PUBSUB_DATASET_PUBLISHEDITEMS_TEMPLATE:
+            if (pdsConfig->config.itemsTemplate.variablesToAddSize > 0){
+                for(size_t i = 0; i < pdsConfig->config.itemsTemplate.variablesToAddSize; i++){
+                    UA_PublishedVariableDataType_deleteMembers(&pdsConfig->config.itemsTemplate.variablesToAdd[i]);
+                }
+                UA_free(pdsConfig->config.itemsTemplate.variablesToAdd);
+            }
+            UA_DataSetMetaDataType_deleteMembers(&pdsConfig->config.itemsTemplate.metaData);
+            break;
+        default:
+            break;
+    }
+}
+
+void UA_PublishedDataSet_delete(UA_PublishedDataSet *publishedDataSet){
+    UA_PublishedDataSetConfig_deleteMembers(&publishedDataSet->config);
+    //delete PDS
+    UA_DataSetMetaDataType_deleteMembers(&publishedDataSet->dataSetMetaData);
 }
--- src/pubsub/ua_pubsub.h
+++ src/pubsub/ua_pubsub.h
@@ -18,26 +18,46 @@ extern "C" {
 #include "ua_server.h"
 #include "ua_server_pubsub.h"
 
-//forward declarations
-struct UA_PubSubConnection;
-typedef struct UA_PubSubConnection UA_PubSubConnection;
+/* The configuration structs (public part of PubSub entities) are defined in include/ua_plugin_pubsub.h */
+
+/**********************************************/
+/*            PublishedDataSet                */
+/**********************************************/
+typedef struct{
+    UA_PublishedDataSetConfig config;
+    UA_DataSetMetaDataType dataSetMetaData;
+    LIST_HEAD(UA_ListOfPubSubDataSetField, UA_PubSubDataSetField) fields;
+    UA_NodeId identifier;
+    UA_UInt16 fieldSize;
+    UA_UInt16 promotedFieldsCount;
+} UA_PublishedDataSet;
+
+UA_StatusCode
+UA_PublishedDataSetConfig_copy(const UA_PublishedDataSetConfig *src, UA_PublishedDataSetConfig *dst);
+UA_PublishedDataSet *
+UA_PublishedDataSet_findPDSbyId(UA_Server *server, UA_NodeId identifier);
+void
+UA_PublishedDataSet_delete(UA_PublishedDataSet *publishedDataSet);
 
 /**********************************************/
 /*               Connection                   */
 /**********************************************/
 //the connection config (public part of connection) object is defined in include/ua_plugin_pubsub.h
-struct UA_PubSubConnection{
+typedef struct{
     UA_PubSubConnectionConfig *config;
     //internal fields
     UA_PubSubChannel *channel;
     UA_NodeId identifier;
-};
+} UA_PubSubConnection;
 
 UA_StatusCode
 UA_PubSubConnectionConfig_copy(const UA_PubSubConnectionConfig *src, UA_PubSubConnectionConfig *dst);
-
-void UA_PubSubConnectionConfig_deleteMembers(UA_PubSubConnectionConfig *connectionConfig);
-void UA_PubSubConnection_delete(UA_PubSubConnection *connection);
+UA_PubSubConnection *
+UA_PubSubConnection_findConnectionbyId(UA_Server *server, UA_NodeId connectionIdentifier);
+void
+UA_PubSubConnectionConfig_deleteMembers(UA_PubSubConnectionConfig *connectionConfig);
+void
+UA_PubSubConnection_delete(UA_PubSubConnection *connection);
 
 #ifdef __cplusplus
 } // extern "C"
