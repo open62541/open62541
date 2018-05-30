42be5fcdc32359410ee7b355fc5bef7fc488814e
AnzeS <anze.skerjanc@gmail.com>
2018-05-30 20:28:37 +0200

    renaming

--- src/ua_securechannel.h
+++ src/ua_securechannel.h
@@ -44,16 +44,16 @@ typedef struct UA_SessionHeader {
 } UA_SessionHeader;
 
 /* For chunked requests */
-struct ChunkData {
-    SIMPLEQ_ENTRY(ChunkData) pointers;
+struct ChunkPayload {
+    SIMPLEQ_ENTRY(ChunkPayload) pointers;
     UA_ByteString bytes;
 };
 
-struct ChunkEntry {
-    LIST_ENTRY(ChunkEntry) pointers;
+struct MessageEntry {
+    LIST_ENTRY(MessageEntry) pointers;
     UA_UInt32 requestId;
-    SIMPLEQ_HEAD(chunkdata_pointerlist, ChunkData) chunkData;
-    size_t chunkDataSize;
+    SIMPLEQ_HEAD(chunkpayload_pointerlist, ChunkPayload) chunkPayload;
+    size_t chunkPayloadSize;
 };
 
 typedef enum {
@@ -85,7 +85,7 @@ struct UA_SecureChannel {
     UA_UInt32 sendSequenceNumber;
 
     LIST_HEAD(session_pointerlist, UA_SessionHeader) sessions;
-    LIST_HEAD(chunk_pointerlist, ChunkEntry) chunks;
+    LIST_HEAD(chunk_pointerlist, MessageEntry) chunks;
 };
 
 UA_StatusCode
