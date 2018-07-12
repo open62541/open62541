da283ea780a84165d0476f2d5beb16b0e0eb52b9
Martin Lang <20433237+mlang-de@users.noreply.github.com>
2018-07-12 11:38:12 +0200

    remove UA_EXPORT

--- include/ua_server.h
+++ include/ua_server.h
@@ -1247,7 +1247,7 @@ UA_Server_triggerEvent(UA_Server *server, const UA_NodeId eventNodeId, const UA_
 /* Add a new namespace to the server. Returns the index of the new namespace */
 UA_UInt16 UA_EXPORT UA_Server_addNamespace(UA_Server *server, const char* name);
 
-UA_ServerConfig* UA_EXPORT
+UA_ServerConfig*
 UA_Server_getConfig(UA_Server *server);
 
 /* Get namespace by name from the server. */
