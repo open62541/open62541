2ead4681d1121a7d8973e15d1d12d1573617fcd5
Julius Pfrommer <julius.pfrommer@web.de>
2018-10-03 16:28:02 +0200

    CI: Disable unneeded MacOS package installs

--- tools/travis/travis_osx_before_install.sh
+++ tools/travis/travis_osx_before_install.sh
@@ -4,8 +4,8 @@ set -ev
 brew install check
 brew install userspace-rcu
 brew install --HEAD valgrind
-brew install graphviz
-brew install python
+# brew install graphviz
+# brew install python
 
 pip2 install --user sphinx
 pip2 install --user breathe
