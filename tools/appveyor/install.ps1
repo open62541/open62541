9d216d2eb0f2353cb9a6442abb7daa4140e0bc72
escherstair <ernestviga@gmail.com>
2018-10-24 12:04:36 +0200

    Update install.ps1

--- tools/appveyor/install.ps1
+++ tools/appveyor/install.ps1
@@ -4,7 +4,15 @@ try {
     & git submodule --quiet update --init --recursive
 
     Write-Host -ForegroundColor Green "`n### Installing CMake and python ###`n"
-    & cinst --no-progress cmake python2
+    if ($env:CC_SHORTNAME -eq "clang") {
+        # pacman may complain that the directory does not exist, thus create it.
+        # See https://github.com/open62541/open62541/issues/2068
+        & C:\msys64\usr\bin\mkdir -p /var/cache/pacman/pkg
+        & C:\msys64\usr\bin\pacman --noconfirm -S mingw-w64-i686-cmake
+        & cinst --no-progress python2
+    } else {
+        & cinst --no-progress cmake python2
+    }
     & C:\Python27\python.exe -m pip install --upgrade pip
     & C:\Python27\Scripts\pip.exe install six
 
