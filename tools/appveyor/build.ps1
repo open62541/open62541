1305d83f5e620d7d7b2599f7c3c7d134cdfbc1e3
escherstair <ernestviga@gmail.com>
2018-10-30 13:36:06 +0100

    Update build.ps1

--- tools/appveyor/build.ps1
+++ tools/appveyor/build.ps1
@@ -45,7 +45,7 @@ try {
     
     & dir "C:\Program Files (x86)\Windows Kits\8.0\bin\x64"
     
-    & dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0"
+    & dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin"
     
     & where.exe rc.exe
     
