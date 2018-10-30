9cd17e4fd57cd16c3ba94dcc5ad7e7af08c02c90
escherstair <ernestviga@gmail.com>
2018-10-30 12:25:31 +0100

    Update build.ps1

--- tools/appveyor/build.ps1
+++ tools/appveyor/build.ps1
@@ -43,7 +43,7 @@ try {
     
     Write-Host $env:PATH
     
-    & dir "C:\Program Files (x86)\Windows Kits\8.0"
+    & dir "C:\Program Files (x86)\Windows Kits\8.0\bin\x64"
     
     & where.exe rc.exe
     
