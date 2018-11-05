a6f0c6e1810a1304f6eb059eba7e3feee114919c
escherstair <ernestviga@gmail.com>
2018-11-05 17:11:57 +0100

    Update build.ps1

--- tools/appveyor/build.ps1
+++ tools/appveyor/build.ps1
@@ -15,6 +15,11 @@ try {
 
     if ($env:CC_SHORTNAME -eq "mingw") {
 
+    } elseif ($env:CC_SHORTNAME -eq "clang-mingw") {
+        # Workaround for http://llvm.org/bugs/show_bug.cgi?id=28089
+        Copy-Item 'C:\Program Files\LLVM' -destination C:\LLVM -recurse
+        $env:Path = 'C:\LLVM\bin;' + $env:Path
+        clang --version
     } else {
         $vcpkg_toolchain = '-DCMAKE_TOOLCHAIN_FILE="C:/Tools/vcpkg/scripts/buildsystems/vcpkg.cmake"'
         $vcpkg_triplet = '-DVCPKG_TARGET_TRIPLET="x86-windows-static"'
