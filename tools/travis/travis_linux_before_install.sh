16f6bf05bd3345c1dbe174669b12efb60993734d
Stefan Profanter <git@s.profanter.me>
2018-11-19 16:50:34 +0100

    CI: Check for empty LOCAL_PKG to avoid running `rm -rf /`

--- tools/travis/travis_linux_before_install.sh
+++ tools/travis/travis_linux_before_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ev
 
+if [ -z ${LOCAL_PKG+x} ] || [ -z "$LOCAL_PKG" ]; then
+    echo "LOCAL_PKG is not set. Aborting..."
+    exit 1
+fi
 
 if [ -z ${DOCKER+x} ] && [ -z ${SONAR+x} ]; then
 	# Only on non-docker builds required
@@ -25,7 +29,11 @@ if [ -z ${DOCKER+x} ] && [ -z ${SONAR+x} ]; then
     echo "=== The build environment is outdated ==="
 
     # Clean up
-    rm -rf $LOCAL_PKG/*
+    # additional safety measure to avoid rm -rf on root
+    # only execute it on travis
+    if ! [ -z ${TRAVIS+x} ]; then
+        echo "rm -rf $LOCAL_PKG/*"
+    fi
 
 	if [ "$CC" = "tcc" ]; then
 		mkdir tcc_install && cd tcc_install
