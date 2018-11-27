3b44a34bc1507d4f2ae0a5cc5e4c347163336aa7
escherstair <ernestviga@gmail.com>
2018-11-27 22:33:15 +0100

    Cleanup AppVeyor configuration files (#2242)

--- tools/appveyor/install.ps1
+++ tools/appveyor/install.ps1
@@ -3,31 +3,10 @@ $ErrorActionPreference = "Stop"
 try {
     & git submodule --quiet update --init --recursive
 
-    Write-Host -ForegroundColor Green "`n### Installing CMake and python ###`n"
-    & cinst --no-progress cmake python2
-    & C:\Python27\python.exe -m pip install --upgrade pip
-    & C:\Python27\Scripts\pip.exe install six
+    Write-Host -ForegroundColor Green "`n### CMake and Python pre-installed AppVeyor Windows build VMs ###`n"
 
     Write-Host -ForegroundColor Green "`n### Installing sphinx ###`n"
-    & C:\Python27\Scripts\pip.exe install --user sphinx sphinx_rtd_theme
-
-    # Write-Host -ForegroundColor Green "`n### Installing Miktex ###`n"
-    # if (-not (Test-Path "c:\miktex\texmfs\install\miktex\bin\pdflatex.exe")) {
-    #     & appveyor DownloadFile https://ftp.uni-erlangen.de/mirrors/CTAN/systems/win32/miktex/setup/windows-x86/miktex-portable-2.9.6753.exe
-    #     & 7z x miktex-portable-2.9.6753.exe -oc:\miktex -bso0 -bsp0
-
-    #     # Remove some big files to reduce size to be cached
-    #     Remove-Item -Path c:\miktex\texmfs\install\doc -Recurse
-    #     Remove-Item -Path c:\miktex\texmfs\install\miktex\bin\biber.exe
-    #     Remove-Item -Path c:\miktex\texmfs\install\miktex\bin\a5toa4.exe
-    # }
-
-    # Write-Host -ForegroundColor Green "`n### Installing graphviz ###`n"
-    # & cinst --no-progress graphviz
-    # if ($LASTEXITCODE -and $LASTEXITCODE -ne 0) {
-    #     Write-Host -ForegroundColor Red "`n`n*** Installing graphviz failed. Exiting ... ***"
-    #     exit $LASTEXITCODE
-    # }
+    & cinst sphinx --source python
 
     Write-Host -ForegroundColor Green "`n### Installing mbedtls ###`n"
 
@@ -53,6 +32,7 @@ try {
 
         Write-Host -ForegroundColor Green "`n### Installing DrMemory ###`n"
         & cinst --no-progress drmemory.portable
+        $env:Path = 'C:\Program Files (x86)\Dr. Memory\bin;' + $env:Path        
     }
 
 } catch {
