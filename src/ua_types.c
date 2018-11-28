8b9ed67a94d3c5fc6a539d00010a2159d448c94b
Stefan Profanter <git@s.profanter.me>
2018-11-28 14:24:40 +0100

    Stack: Fix heap use after free
    Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=11484
    
    Credit to oss-fuzz

--- src/ua_types.c
+++ src/ua_types.c
@@ -408,6 +408,7 @@ Variant_clear(UA_Variant *p, const UA_DataType *_) {
         if(p->arrayLength == 0)
             p->arrayLength = 1;
         UA_Array_delete(p->data, p->arrayLength, p->type);
+        p->data = NULL;
     }
     if((void*)p->arrayDimensions > UA_EMPTY_ARRAY_SENTINEL)
         UA_free(p->arrayDimensions);
