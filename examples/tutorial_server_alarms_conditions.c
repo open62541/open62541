f473ecb54995d525ad8f64045556a5bd8b1404d2
Sameer AL-Qadasi <sqadasi@hilscher.com>
2018-12-20 08:47:34 +0100

    Update tutorial_server_alarms_conditions.c
    modify includes

--- examples/tutorial_server_alarms_conditions.c
+++ examples/tutorial_server_alarms_conditions.c
@@ -1,7 +1,10 @@
 /* This work is licensed under a Creative Commons CCZero 1.0 Universal License.
  * See http://creativecommons.org/publicdomain/zero/1.0/ for more information. */
 
-#include "open62541.h"
+#include <ua_server.h>
+#include <ua_config_default.h>
+#include <ua_log_stdout.h>
+
 #include <signal.h>
 
 /**
