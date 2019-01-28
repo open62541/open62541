64baa2c13b35067832058339eb89dca08690a583
Mark Giraud <mark.giraud@iosb.fraunhofer.de>
2019-01-28 11:52:18 +0100

    Replace manual hook call with wrapper

--- plugins/networking/ua_tcp_listener_socket.c
+++ plugins/networking/ua_tcp_listener_socket.c
@@ -302,7 +302,7 @@ UA_TCP_ListenerSockets(UA_SocketConfig *socketConfig, UA_SocketHook creationHook
         UA_TCP_ListenerSocketFromAddrinfo(ai, socketConfig,
                                           &sock);
         /* Instead of allocating an array to return the sockets, we call a hook for each one */
-        creationHook.hook(creationHook.hookContext, sock);
+        UA_SocketHook_call(creationHook, sock);
     }
     UA_freeaddrinfo(res);
 
