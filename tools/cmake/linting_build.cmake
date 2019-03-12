58d08d5dc289200a5de75647b0e317127e16be18
Stefan Profanter <git@s.profanter.me>
2019-03-12 11:45:53 +0100

    Linting: Additional fixes and cleanup

--- tools/cmake/linting_build.cmake
+++ tools/cmake/linting_build.cmake
@@ -31,23 +31,3 @@ elseif(UA_ENABLE_STATIC_ANALYZER STREQUAL FULL)
     endif()
 endif()
 
-# adds new target "clangformat" to enforce clang-format rules
-find_program(CLANG_FORMAT_EXE NAMES "clang-format")
-if(CLANG_FORMAT_EXE)
-  file(GLOB_RECURSE FILES_TO_FORMAT
-       ${PROJECT_SOURCE_DIR}/arch/*.c
-       ${PROJECT_SOURCE_DIR}/plugins/*.c
-       ${PROJECT_SOURCE_DIR}/src/*.c
-       ${PROJECT_SOURCE_DIR}/arch/*.h
-       ${PROJECT_SOURCE_DIR}/include/*.h
-       ${PROJECT_SOURCE_DIR}/plugins/*.h
-       ${PROJECT_SOURCE_DIR}/src/*.h
-    )
-  add_custom_target(
-    clangformat COMMAND ${CLANG_FORMAT_EXE}
-        -style=file
-        -i
-        ${FILES_TO_FORMAT}
-  )
-endif()
-
