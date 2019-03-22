ccd1633d826057c908169489318cfad590bf5fad
Julius Pfrommer <julius.pfrommer@web.de>
2019-03-22 13:04:23 +0100

    PubSub: Realtime publisher example

---
+++ examples/pubsub_realtime/bufmalloc.h
@@ -0,0 +1,11 @@
+/* This work is licensed under a Creative Commons CCZero 1.0 Universal License.
+ * See http://creativecommons.org/publicdomain/zero/1.0/ for more information.
+ *
+ *    Copyright 2018 (c) Fraunhofer IOSB (Author: Julius Pfrommer)
+ */
+
+#include <open62541/config.h>
+
+void resetMembuf(void);
+void useMembufAlloc(void);
+void useNormalAlloc(void);
