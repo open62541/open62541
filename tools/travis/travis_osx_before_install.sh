101e167135422f87224d4bd804894d39b3b0d165
Stefan Profanter <git@s.profanter.me>
2019-04-30 16:36:56 +0200

    Remove Python-Sphinx from the OSX CI dependencies

--- tools/travis/travis_osx_before_install.sh
+++ tools/travis/travis_osx_before_install.sh
@@ -7,6 +7,4 @@ brew install graphviz
 brew install python
 brew install mbedtls
 
-pip2 install --user sphinx
-pip2 install --user breathe
-pip2 install --user sphinx_rtd_theme
+pip2 install --user six
