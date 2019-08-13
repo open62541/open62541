12980441bc67006fb62abd32c5a1608011416279
ichrispa <Chris_Paul.Iatrou@tu-dresden.de>
2019-08-13 20:53:52 +0200

    Unify license headers

--- tools/nodeset_compiler/nodeset_testing.py
+++ tools/nodeset_compiler/nodeset_testing.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+### This Source Code Form is subject to the terms of the Mozilla Public
+### License, v. 2.0. If a copy of the MPL was not distributed with this
+### file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+###    Copyright 2014-2015 (c) TU-Dresden (Author: Chris Iatrou)
+###    Copyright 2014-2017 (c) Fraunhofer IOSB (Author: Julius Pfrommer)
+###    Copyright 2016-2017 (c) Stefan Profanter, fortiss GmbH
+
 import codecs
 from nodeset import *
 
