5284910907162e165edf700c3f5ff164acc07cc2
Stefan Profanter <git@s.profanter.me>
2019-08-29 17:57:18 +0200

    fix: Start ctt server as default application in docker container
    The container can be built with:
    `docker build -t open62541 . && docker run -p 4840:4840 open62541`

--- tools/travis/travis_linux_script.sh
+++ tools/travis/travis_linux_script.sh
@@ -29,7 +29,7 @@ fi
 # Docker build test
 if ! [ -z ${DOCKER+x} ]; then
     docker build -t open62541 .
-    docker run -d -p 127.0.0.1:80:80 --name open62541 open62541 /bin/sh
+    docker run -d -p 127.0.0.1:4840:4840 --name open62541 open62541 /bin/sh
     docker ps | grep -q open62541
     # disabled since it randomly fails
     # docker ps | grep -q open62541
