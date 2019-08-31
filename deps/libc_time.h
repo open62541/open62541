055b1a0484aa677be4f8451465ac5d68577ba01f
Julius Pfrommer <julius.pfrommer@web.de>
2019-08-31 17:30:52 +0200

    fix(core): Simplify time conversion and fix an edge case for negative dates

--- deps/libc_time.h
+++ deps/libc_time.h
@@ -8,13 +8,9 @@ struct mytm {
     int tm_mday;
     int tm_mon;
     int tm_year;
-    int tm_wday;
-    int tm_yday;
 };
 
 int __secs_to_tm(long long t, struct mytm *tm);
-int __month_to_secs(int month, int is_leap);
-long long __year_to_secs(long long year, int *is_leap);
 long long __tm_to_secs(const struct mytm *tm);
 
 #endif /* LIBC_TIME_H_ */
