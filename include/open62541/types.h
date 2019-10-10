ad52836b5e3ed6826c57e0c370af620d5e6cc243
Stefan Profanter <git@s.profanter.me>
2019-10-10 11:47:17 +0200

    fix(json): signed integer overflow if value is UA_INT64_MIN
    Fixes: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=15334
    Credit to oss-fuzz

--- include/open62541/types.h
+++ include/open62541/types.h
@@ -104,8 +104,8 @@ typedef uint32_t UA_UInt32;
  * An integer value between -9 223 372 036 854 775 808 and
  * 9 223 372 036 854 775 807. */
 typedef int64_t UA_Int64;
-#define UA_INT64_MIN ((int64_t)-9223372036854775808)
-#define UA_INT64_MAX (int64_t)9223372036854775807
+#define UA_INT64_MAX (int64_t)9223372036854775807LL
+#define UA_INT64_MIN ((int64_t)-UA_INT64_MAX-1LL)
 
 /**
  * UInt64
@@ -113,7 +113,7 @@ typedef int64_t UA_Int64;
  * An integer value between 0 and 18 446 744 073 709 551 615. */
 typedef uint64_t UA_UInt64;
 #define UA_UINT64_MIN (uint64_t)0
-#define UA_UINT64_MAX (uint64_t)18446744073709551615
+#define UA_UINT64_MAX (uint64_t)18446744073709551615ULL
 
 /**
  * Float
