ba060096bcec7335b812820c3e2a31a9033097bf
Julius Pfrommer <julius.pfrommer@web.de>
2020-01-26 11:31:57 +0100

    fix(server) Don't send ERR if channel already closed

--- src/server/ua_server_binary.c
+++ src/server/ua_server_binary.c
@@ -744,14 +744,20 @@ processSecureChannelMessage(void *application, UA_SecureChannel *channel,
         break;
     }
     if(retval != UA_STATUSCODE_GOOD) {
-        UA_LOG_INFO_CHANNEL(&server->config.logger, channel,
-                            "Processing the message failed with StatusCode %s. "
-                            "Closing the channel.", UA_StatusCode_name(retval));
-        UA_TcpErrorMessage errMsg;
-        UA_TcpErrorMessage_init(&errMsg);
-        errMsg.error = retval;
-        UA_Connection_sendError(channel->connection, &errMsg);
-        Service_CloseSecureChannel(server, channel);
+        if(channel->connection) {
+            UA_LOG_INFO_CHANNEL(&server->config.logger, channel,
+                                "Processing the message failed with StatusCode %s. "
+                                "Closing the channel.", UA_StatusCode_name(retval));
+            UA_TcpErrorMessage errMsg;
+            UA_TcpErrorMessage_init(&errMsg);
+            errMsg.error = retval;
+            UA_Connection_sendError(channel->connection, &errMsg);
+            Service_CloseSecureChannel(server, channel);
+        } else {
+            UA_LOG_INFO_CHANNEL(&server->config.logger, channel,
+                                "Processing the message failed. Channel already closed "
+                                "with StatusCode %s. ", UA_StatusCode_name(retval));
+        }
     }
 }
 
