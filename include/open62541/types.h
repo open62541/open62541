f69c971d3f1d96c320ad0623dc9721ff3987edca
Julius Pfrommer <julius.pfrommer@web.de>
2020-03-22 19:46:38 +0100

    refactor(doc): Improve line lengths in types.h

--- include/open62541/types.h
+++ include/open62541/types.h
@@ -155,9 +155,11 @@ typedef struct {
 } UA_String;
 
 /* Copies the content on the heap. Returns a null-string when alloc fails */
-UA_String UA_EXPORT UA_String_fromChars(const char *src) UA_FUNC_ATTR_WARN_UNUSED_RESULT;
+UA_String UA_EXPORT
+UA_String_fromChars(const char *src) UA_FUNC_ATTR_WARN_UNUSED_RESULT;
 
-UA_Boolean UA_EXPORT UA_String_equal(const UA_String *s1, const UA_String *s2);
+UA_Boolean UA_EXPORT
+UA_String_equal(const UA_String *s1, const UA_String *s2);
 
 UA_EXPORT extern const UA_String UA_STRING_NULL;
 
