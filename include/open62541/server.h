dad9dbd85706ba2b22686a88372a136afdcdf857
Christian von Arnim <christian.von-arnim@isw.uni-stuttgart.de>
2020-04-02 20:57:07 +0200

    Add UA_Server_writeDataValue

--- include/open62541/server.h
+++ include/open62541/server.h
@@ -395,11 +395,28 @@ UA_Server_writeEventNotifier(UA_Server *server, const UA_NodeId nodeId,
                              &UA_TYPES[UA_TYPES_BYTE], &eventNotifier);
 }
 
+/**
+ * Writes an UA_Variant to a variable/variableType node.
+ * StatusCode is set to UA_STATUSCODE_GOOD, sourceTimestamp and
+ * serverTimestamp are set to UA_DateTime_now()
+ */
 static UA_INLINE UA_THREADSAFE UA_StatusCode
 UA_Server_writeValue(UA_Server *server, const UA_NodeId nodeId,
                      const UA_Variant value) {
     return __UA_Server_write(server, &nodeId, UA_ATTRIBUTEID_VALUE,
                              &UA_TYPES[UA_TYPES_VARIANT], &value);
+}
+
+/**
+ * Writes an UA_DataValue to a variable/variableType node.
+ * In contrast to UA_Server_writeValue, this functions can also write
+ * sourceTimestamp, serverTimestamp and statusCode.
+ */
+static UA_INLINE UA_THREADSAFE UA_StatusCode
+UA_Server_writeDataValue(UA_Server *server, const UA_NodeId nodeId,
+                     const UA_DataValue value) {
+    return __UA_Server_write(server, &nodeId, UA_ATTRIBUTEID_VALUE,
+                             &UA_TYPES[UA_TYPES_DATAVALUE], &value);
 }
 
 static UA_INLINE UA_THREADSAFE UA_StatusCode
