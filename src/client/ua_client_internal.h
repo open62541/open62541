e196c5c628cfae63f103e5bac67c2956d43ea6b2
Julius Pfrommer <julius.pfrommer@web.de>
2020-05-01 21:04:08 +0200

    refactor(client): Small cosmetic cleanup in ua_client_internal.h

--- src/client/ua_client_internal.h
+++ src/client/ua_client_internal.h
@@ -141,9 +141,9 @@ struct UA_Client {
 
     /* Subscriptions */
 #ifdef UA_ENABLE_SUBSCRIPTIONS
-    UA_UInt32 monitoredItemHandles;
     LIST_HEAD(, UA_Client_NotificationsAckNumber) pendingNotificationsAcks;
     LIST_HEAD(, UA_Client_Subscription) subscriptions;
+    UA_UInt32 monitoredItemHandles;
     UA_UInt16 currentlyOutStandingPublishRequests;
 #endif
 
