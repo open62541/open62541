0800093f82529a7c9dfe72123731f89a0eacd8c3
ChuongVhn <chuong.vhn@gmail.com>
2020-08-03 11:36:25 +0100

    Fix wrong option build_yocto script. Add verbose CMAKE options

--- build_x86_64_opc_ua.sh
+++ build_x86_64_opc_ua.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#set -x
 
 build_dir=`pwd`/_build_x86_64_opc_ua
 
-[ -d ${build_dir} ] && { echo "found build folder: ${build_dir}"; } || { mkdir ${build_dir} && { echo "build folder: ${build_dir} has been build"; } || exit 0; }
+[ -d ${build_dir} ] && { echo "found build folder: ${build_dir}"; } || { mkdir ${build_dir} \
+&& { echo "build folder: ${build_dir} has been build"; } || exit 0; }
 
 cd ${build_dir} || { echo "build directory not found"; exit 1; }
 
@@ -17,15 +19,15 @@ cd ${build_dir} || { echo "build directory not found"; exit 1; }
 #-DUA_ENABLE_IMMUTABLE_NODES=TRUE \
 #-DUA_MULTITHREADING=100 \
 
-BUILD_OPT="-DUA_ENABLE_PUBSUB=TRUE \
-	-DUA_ENABLE_SUBSCRIPTIONS=TRUE \
-	-DUA_ENABLE_SUBSCRIPTIONS_EVENTS=TRUE \
-	-DUA_NAMESPACE_ZERO=FULL \
-	-DUA_ENABLE_DISCOVERY=TRUE \
-	-DUA_ENABLE_DISCOVERY_MULTICAST=TRUE \
-    -DUA_ENABLE_PUBSUB_INFORMATIONMODEL=TRUE \
-	-DUA_LOGLEVEL=200 \
-    -DUA_DEBUG=TRUE" 
+BUILD_OPTS="-DUA_ENABLE_PUBSUB=TRUE \
+-DUA_ENABLE_SUBSCRIPTIONS=TRUE \
+-DUA_ENABLE_SUBSCRIPTIONS_EVENTS=TRUE \
+-DUA_NAMESPACE_ZERO=FULL \
+-DUA_ENABLE_DISCOVERY=TRUE \
+-DUA_ENABLE_DISCOVERY_MULTICAST=TRUE \
+-DUA_ENABLE_PUBSUB_INFORMATIONMODEL=TRUE \
+-DUA_LOGLEVEL=200 \
+-DUA_DEBUG=TRUE" 
 
 DEST_DIR_LOCAL="/usr"
 
@@ -35,7 +37,7 @@ DEST_DIR_LOCAL="/usr"
 find . -maxdepth 1 -name "*" -type f -exec rm -vf {} +
 
 #build static libraries first
-cmake ${BUILD_OPT} -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${DEST_DIR_LOCAL} ..
+cmake ${BUILD_OPTS} -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${DEST_DIR_LOCAL} -LA ..
 cmake --build .
 sudo make install
 
@@ -45,6 +47,6 @@ sudo make install
 find . -maxdepth 1 -name "*" -type f -exec rm -vf {} +
 
 #then build shared libraries
-cmake ${BUILD_OPT} -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${DEST_DIR_LOCAL} ..
+cmake ${BUILD_OPTS} -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${DEST_DIR_LOCAL} -LA ..
 cmake --build .
 sudo make install
--- build_yocto_x86_64_opc_ua.sh
+++ build_yocto_x86_64_opc_ua.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# set -x
 
 setup_script="$(find /home/developer/vrte/sdk/linux-x86_64 -maxdepth 4 -name "environment-setup*")"
 
@@ -17,7 +18,8 @@ fi
 
 build_dir=`pwd`/_build_yocto_x86_64_opc_ua
 
-[ -d ${build_dir} ] && { echo "found build folder: ${build_dir}"; } || { mkdir ${build_dir} && { echo "build folder: ${build_dir} has been build"; } || exit 0; }
+[ -d ${build_dir} ] && { echo "found build folder: ${build_dir}"; } || { mkdir ${build_dir} \
+&& { echo "build folder: ${build_dir} has been build"; } || exit 0; }
 
 cd ${build_dir} || { echo "build directory not found"; exit 1; }
 
@@ -33,14 +35,14 @@ cd ${build_dir} || { echo "build directory not found"; exit 1; }
 #-DUA_MULTITHREADING=100 \
 
 BUILD_OPTS="-DUA_ENABLE_PUBSUB=TRUE \
-	-DUA_ENABLE_SUBSCRIPTIONS=TRUE \
-	-DUA_ENABLE_SUBSCRIPTIONS_EVENTS=TRUE \
-	-DUA_NAMESPACE_ZERO=FULL \
-	-DUA_ENABLE_DISCOVERY=TRUE \
-	-DUA_ENABLE_DISCOVERY_MULTICAST=TRUE \
-    -DUA_ENABLE_PUBSUB_INFORMATIONMODEL=TRUE \
-	-DUA_LOGLEVEL=200 \
-    -DUA_DEBUG=TRUE" 
+-DUA_ENABLE_SUBSCRIPTIONS=TRUE \
+-DUA_ENABLE_SUBSCRIPTIONS_EVENTS=TRUE \
+-DUA_NAMESPACE_ZERO=FULL \
+-DUA_ENABLE_DISCOVERY=TRUE \
+-DUA_ENABLE_DISCOVERY_MULTICAST=TRUE \
+-DUA_ENABLE_PUBSUB_INFORMATIONMODEL=TRUE \
+-DUA_LOGLEVEL=200 \
+-DUA_DEBUG=TRUE" 
 
 DEST_DIR_YOCTO="/home/developer/vrte/sdk/linux-x86_64/usr"
 
@@ -50,16 +52,16 @@ DEST_DIR_YOCTO="/home/developer/vrte/sdk/linux-x86_64/usr"
 find . -maxdepth 1 -name "*" -type f -exec rm -vf {} +
 
 #build static libraries first
-cmake ${BUILD_OPTS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${DEST_DIR_YOCTO} ..
+cmake ${BUILD_OPTS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${DEST_DIR_YOCTO} -LA ..
 cmake --build .
 sudo make install
 
 #need to clean-up build folder otherwise cmake does not recognize new bulid configuration
 #need to retain install folder
 #we are in builddir now
-find . -maxdepth 1 -name "*" -type f -exec rm -vf {} \;
+find . -maxdepth 1 -name "*" -type f -exec rm -vf {} +
 
 #then build shared libraries
-cmake ${BUILD_OPT} -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${DEST_DIR_YOCTO} ..
+cmake ${BUILD_OPTS} -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${DEST_DIR_YOCTO} -LA ..
 cmake --build .
 sudo make install
