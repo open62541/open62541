42dec73f74348b01b8be54ac5910d4af010c32e3
Thomas Fischer <thomas_fischer@siemens.com>
2020-08-05 11:00:29 +0200

    include plugins only when pubsub is enabled

--- src/pubsub/ua_pubsub_config.c
+++ src/pubsub/ua_pubsub_config.c
@@ -7,10 +7,13 @@
  */
 
 #include <open62541/plugin/log_stdout.h>
+
+#ifdef UA_ENABLE_PUBSUB
 #ifdef UA_ENABLE_PUBSUB_ETH_UADP
 #include <open62541/plugin/pubsub_ethernet.h>
 #endif
 #include <open62541/plugin/pubsub_udp.h>
+#endif
 
 #include "pubsub/ua_pubsub.h"
 #include "pubsub/ua_pubsub_manager.h"
