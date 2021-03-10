a0fe9c014f6c513dcee70894d94cb782e54d798c
Julius Pfrommer <julius.pfrommer@web.de>
2021-03-10 23:15:55 +0100

    refactor(server): Small simplification in deconstructRefTree

--- src/server/ua_services_nodemanagement.c
+++ src/server/ua_services_nodemanagement.c
@@ -1684,22 +1684,28 @@ deconstructRefTree(UA_Server *server, UA_Session *session,
         if(member->head.nodeClass == UA_NODECLASS_OBJECT ||
            member->head.nodeClass == UA_NODECLASS_VARIABLE) {
             const UA_Node *type = getNodeType(server, &member->head);
-            if(type) {
-                const UA_NodeTypeLifecycle *lifecycle;
-                if(member->head.nodeClass == UA_NODECLASS_OBJECT)
-                    lifecycle = &type->objectTypeNode.lifecycle;
-                else
-                    lifecycle = &type->variableTypeNode.lifecycle;
-                if(lifecycle->destructor) {
-                    UA_UNLOCK(server->serviceMutex);
-                    lifecycle->destructor(server,
-                                          &session->sessionId, session->sessionHandle,
-                                          &type->head.nodeId, type->head.context,
-                                          &member->head.nodeId, &context);
-                    UA_LOCK(server->serviceMutex);
-                }
-                UA_NODESTORE_RELEASE(server, type);
+            if(!type)
+                continue;
+
+            /* Get the lifecycle */
+            const UA_NodeTypeLifecycle *lifecycle;
+            if(member->head.nodeClass == UA_NODECLASS_OBJECT)
+                lifecycle = &type->objectTypeNode.lifecycle;
+            else
+                lifecycle = &type->variableTypeNode.lifecycle;
+
+            /* Call the destructor */
+            if(lifecycle->destructor) {
+                UA_UNLOCK(server->serviceMutex);
+                lifecycle->destructor(server,
+                                      &session->sessionId, session->sessionHandle,
+                                      &type->head.nodeId, type->head.context,
+                                      &member->head.nodeId, &context);
+                UA_LOCK(server->serviceMutex);
             }
+
+            /* Release the type node */
+            UA_NODESTORE_RELEASE(server, type);
         }
 
         /* Call the global destructor */
