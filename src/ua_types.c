552736f2bdae88c548596b962024202b8156a98a
Julius Pfrommer <julius.pfrommer@web.de>
2021-03-31 15:03:04 +0200

    refactor(core): Better documentation of "borrowed" variant content

--- src/ua_types.c
+++ src/ua_types.c
@@ -584,14 +584,19 @@ UA_ExtensionObject_setValueCopy(UA_ExtensionObject *eo,
 /* Variant */
 static void
 Variant_clear(UA_Variant *p, const UA_DataType *_) {
-    if(p->storageType != UA_VARIANT_DATA)
+    /* The content is "borrowed" */
+    if(p->storageType == UA_VARIANT_DATA_NODELETE)
         return;
+
+    /* Delete the value */
     if(p->type && p->data > UA_EMPTY_ARRAY_SENTINEL) {
         if(p->arrayLength == 0)
             p->arrayLength = 1;
         UA_Array_delete(p->data, p->arrayLength, p->type);
         p->data = NULL;
     }
+
+    /* Delete the array dimensions */
     if((void*)p->arrayDimensions > UA_EMPTY_ARRAY_SENTINEL)
         UA_free(p->arrayDimensions);
 }
