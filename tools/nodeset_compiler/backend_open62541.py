9b37912918c54976e82489a7ded5b41994c33db1
wenshan1 <Bin.Lan@windriver.com>
2021-04-15 17:30:40 +0800

    fix(nc): The generated statement is too long in namespace0_generated in namespace0_generated.c
    This too long statement will generate a lot of temporary variables
    when it is running, causing the task to run it to allocate a large
    stack.
    This fix is to split the too long statement to lots of small statement.

--- tools/nodeset_compiler/backend_open62541.py
+++ tools/nodeset_compiler/backend_open62541.py
@@ -8,6 +8,7 @@
 ###    Copyright 2014-2015 (c) TU-Dresden (Author: Chris Iatrou)
 ###    Copyright 2014-2017 (c) Fraunhofer IOSB (Author: Julius Pfrommer)
 ###    Copyright 2016-2017 (c) Stefan Profanter, fortiss GmbH
+###    Copyright 2021 (c) Wind River Systems, Inc.
 
 
 from __future__ import print_function
@@ -315,26 +316,18 @@ UA_StatusCode retVal = UA_STATUSCODE_GOOD;""" % (outfilebase))
         writec("}")
 
     if functionNumber > 0:
-
-        # concatenate method calls with "&&" operator.
-        # The first method which does not return UA_STATUSCODE_GOOD (=0) will cause aborting
-        # the remaining calls and retVal will be set to that error code.
-        writec("bool dummy = (")
         for i in range(0, functionNumber):
-            writec("{concat}!(retVal = function_{outfilebase}_{idx}_begin(server, ns))".format(
-                outfilebase=outfilebase, idx=str(i), concat= "" if i == 0 else "&& "))
+            writec("if((retVal = function_{outfilebase}_{idx}_begin(server, ns)) != UA_STATUSCODE_GOOD) return retVal;".format(
+                outfilebase=outfilebase, idx=str(i)))
             if i in reftypes_functionNumbers:
-                writec("&& !(retVal = function_{outfilebase}_{idx}_finish(server, ns))".format(
+                writec("if((retVal = function_{outfilebase}_{idx}_finish(server, ns)) != UA_STATUSCODE_GOOD) return retVal;".format(
                     outfilebase=outfilebase, idx=str(i)))
 
         for i in reversed(range(0, functionNumber)):
             if i in reftypes_functionNumbers:
                 continue
-            writec("&& !(retVal = function_{outfilebase}_{idx}_finish(server, ns))".format(
+            writec("if((retVal = function_{outfilebase}_{idx}_finish(server, ns)) != UA_STATUSCODE_GOOD) return retVal;".format(
                 outfilebase=outfilebase, idx=str(i)))
-
-        # use (void)(dummy) to avoid unused variable error.
-        writec("); (void)(dummy);")
 
     writec("return retVal;\n}")
     outfileh.flush()
