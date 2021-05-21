5e49b670b8cb3dde9a2e62b303ee6f1336423dea
Jan Hermes <jan@hermes-technology.de>
2021-05-21 08:59:42 +0200

    ci: enable and use pubsub encryption tests
    Currently there is at least one unit-test that uses
    pubsub encryption that is never executed in the continous
    integration. This ci extension enables the test of pubsub encryption.

--- tools/ci.sh
+++ tools/ci.sh
@@ -136,6 +136,25 @@ function unit_tests_encryption_openssl {
     make test ARGS="-V"
 }
 
+function unit_tests_encryption_mbedtls_pubsub {
+    mkdir -p build; cd build; rm -rf *
+    cmake -DCMAKE_BUILD_TYPE=Debug \
+          -DUA_BUILD_EXAMPLES=ON \
+          -DUA_BUILD_UNIT_TESTS=ON \
+          -DUA_ENABLE_DISCOVERY=ON \
+          -DUA_ENABLE_DISCOVERY_MULTICAST=ON \
+          -DUA_ENABLE_ENCRYPTION=ON \
+          -DUA_ENABLE_ENCRYPTION_MBEDTLS=ON \
+          -DUA_ENABLE_PUBSUB=ON \
+          -DUA_ENABLE_PUBSUB_DELTAFRAMES=OFF \
+          -DUA_ENABLE_PUBSUB_INFORMATIONMODEL=OFF \
+          -DUA_ENABLE_PUBSUB_MONITORING=ON \
+          -DUA_ENABLE_PUBSUB_ENCRYPTION=ON \
+          ..
+    make ${MAKEOPTS}
+    make test ARGS="-V"
+}
+
 ##########################################
 # Build and Run Unit Tests with Valgrind #
 ##########################################
