6e9fa07ecfdd680024e468263fe9661740179487
jackybek <jackybek@gmail.com>
2021-09-11 16:57:34 +0800

    feat(PubSub): Fields for secured MQTT connection

--- deps/mqtt-c/mqtt.h
+++ deps/mqtt-c/mqtt.h
@@ -666,7 +666,11 @@ enum MQTTConnectFlags {
     MQTT_CONNECT_WILL_QOS_2 = (2u & 0x03) << 3,
     MQTT_CONNECT_WILL_RETAIN = 32u,
     MQTT_CONNECT_PASSWORD = 64u,
-    MQTT_CONNECT_USER_NAME = 128u
+    MQTT_CONNECT_USER_NAME = 128u,
+    MQTT_CONNECT_CAFILEPATH = 256u,
+    MQTT_CONNECT_CAPATH = 512u,
+    MQTT_CONNECT_CLIENTCERTPATH = 1024u,
+    MQTT_CONNECT_CLIENTKEYPATH = 2048u
 };
 
 /**
@@ -714,6 +718,11 @@ ssize_t mqtt_pack_connection_request(uint8_t* buf, size_t bufsz,
                                      size_t will_message_size,
                                      const char* user_name,
                                      const char* password,
+                                     const char* caFilePath,
+                                     const char* caPath,
+                                     const char* clientCertPath,
+                                     const char* clientKeyPath,
+                                     bool useTLS,
                                      uint8_t connect_flags,
                                      uint16_t keep_alive);
 
@@ -1429,6 +1438,11 @@ enum MQTTErrors mqtt_connect(struct mqtt_client *client,
                              size_t will_message_size,
                              const char* user_name,
                              const char* password,
+                             const char* caFilePath,
+                             const char* caPath,
+                             const char* clientCertPath,
+                             const char* clientKeyPath,
+                             bool useTLS,
                              uint8_t connect_flags,
                              uint16_t keep_alive);
 
