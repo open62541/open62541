0ebc06f7a64dee9af8696790173b10b69c95a00f
Simon Küppers <simon.kueppers@2pi-labs.com>
2021-09-16 13:23:07 +0200

    Added file header and MPLv2 license

--- tools/nodeset_compiler/nodeset_resolver.py
+++ tools/nodeset_compiler/nodeset_resolver.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+### This Source Code Form is subject to the terms of the Mozilla Public
+### License, v. 2.0. If a copy of the MPL was not distributed with this
+### file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+###    Copyright 2021 (c) Simon Kueppers, 2pi-Labs GmbH
+
+
 import logging
 import argparse
 import sys
