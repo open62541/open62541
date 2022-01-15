dfc88176349d999751457170a94241649f5de7c6
Julius Pfrommer <julius.pfrommer@web.de>
2022-01-15 13:31:42 +0100

    feat(server): Enable the MonitoringQueueOverflowCount diagnostics

--- src/server/ua_subscription.h
+++ src/server/ua_subscription.h
@@ -300,7 +300,7 @@ struct UA_Subscription {
     UA_UInt32 notificationsCount;
     UA_UInt32 latePublishRequestCount;
     UA_UInt32 discardedMessageCount;
-    UA_UInt32 monitoringQueueOverflowCount; /* TODO: Update the statistics */
+    UA_UInt32 monitoringQueueOverflowCount;
     UA_UInt32 eventQueueOverFlowCount; /* TODO: Update the statistics */
 #endif
 };
