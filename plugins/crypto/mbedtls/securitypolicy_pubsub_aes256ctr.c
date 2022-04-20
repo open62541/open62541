2a79629d23b79907b7dcaaff8184707cd3ab85c8
Andreas Ebner <andreas.ebner@iosb.fraunhofer.de>
2022-04-20 15:22:06 +0200

    fix(sec) remove use of mbedtls internal headers

--- plugins/crypto/mbedtls/securitypolicy_pubsub_aes256ctr.c
+++ plugins/crypto/mbedtls/securitypolicy_pubsub_aes256ctr.c
@@ -14,7 +14,6 @@
 #include <mbedtls/aes.h>
 #include <mbedtls/ctr_drbg.h>
 #include <mbedtls/entropy.h>
-#include <mbedtls/entropy_poll.h>
 #include <mbedtls/error.h>
 #include <mbedtls/md.h>
 #include <mbedtls/sha1.h>
@@ -303,9 +302,8 @@ policyContext_newContext_sp_pubsub_aes256ctr(UA_PubSubSecurityPolicy *securityPo
         goto error;
     }
 
-    /* Add the system entropy source */
-    mbedErr = mbedtls_entropy_add_source(&pc->entropyContext, mbedtls_platform_entropy_poll,
-                                         NULL, 0, MBEDTLS_ENTROPY_SOURCE_STRONG);
+    mbedErr = mbedtls_entropy_self_test(0);
+
     if(mbedErr) {
         retval = UA_STATUSCODE_BADSECURITYCHECKSFAILED;
         goto error;
