173f9d4926c595e1e2db13f45738de5418579c07
Noel Graf <69737119+NoelGraf@users.noreply.github.com>
2022-05-02 14:20:01 +0200

    fix(nc): Adjustment of the check when searching for HasTypeDefinition references.

--- tools/nodeset_compiler/nodes.py
+++ tools/nodeset_compiler/nodes.py
@@ -151,7 +151,7 @@ class Node(object):
 
     def popTypeDef(self):
         for ref in self.references:
-            if ref.referenceType.ns == 0 and ref.referenceType.i == 40 and ref.isForward:
+            if ref.referenceType == NodeId("ns=0;i=40") and ref.isForward:
                 self.references.remove(ref)
                 return ref
         return Reference(NodeId(), NodeId(), NodeId(), False)
