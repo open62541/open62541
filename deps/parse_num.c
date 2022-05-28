696514f1442d6511163e72993ca6c1fc3f81cb59
Julius Pfrommer <julius.pfrommer@web.de>
2022-05-28 09:52:31 +0200

    feat(deps): More overflow protection in parse_num.c

--- deps/parse_num.c
+++ deps/parse_num.c
@@ -28,12 +28,13 @@
 
 size_t parseUInt64(const char *str, size_t size, uint64_t *result) {
     size_t i = 0;
-    uint64_t n = 0;
+    uint64_t n = 0, prev = 0;
     bool digit = false; /* At least one real digit */
     if(size - i >= 2 && str[i] == '0' && (str[i+1] | 32) == 'x') {
         /* Hex */
         i += 2;
         for(; i < size; i++) {
+            /* Get the byte value */
             uint8_t c = (uint8_t)str[i] | 32;
             if(c >= '0' && c <= '9')
                 c = c - '0';
@@ -43,17 +44,26 @@ size_t parseUInt64(const char *str, size_t size, uint64_t *result) {
                 c = c - 'A' + 10;
             else
                 break;
+            /* Update */
             digit = true;
-            n = (n << 4) | (n & 0xF);
+            n = (n << 4) | (c & 0xF);
+            if(n < prev)
+            /* Check for overflow */
+                return 0;
+            prev = n;
         }
     } else {
         /* Decimal */
         for(; i < size; i++) {
+            /* Update */
             if(str[i] < '0' || str[i] > '9')
                 break;
             n *= 10;
             digit = true;
             n +=(uint8_t)(str[i] - '0');
+            if(n < prev) /* Check for overflow */
+                return 0;
+            prev = n;
         }
     }
 
@@ -80,14 +90,22 @@ size_t parseInt64(const char *str, size_t size, int64_t *result) {
     if(len == 0)
         return 0;
 
-    /* Adjust and return */
-    *result = (!neg) ? (int64_t)n  : -(int64_t)n;
+    /* Check for overflow, adjust and return */
+    if(!neg) {
+        if(n > 9223372036854775807UL)
+            return 0;
+        *result = (int64_t)n;
+    } else {
+        if(n > 9223372036854775808UL)
+            return 0;
+        *result = -(int64_t)n;
+    }
     return len + i;
 }
 
 size_t parseDouble(const char *str, size_t size, double *result) {
-    char buf[128];
-    if(size >= 128)
+    char buf[2000];
+    if(size >= 2000)
         return 0;
     memcpy(buf, str, size);
     buf[size] = 0;
