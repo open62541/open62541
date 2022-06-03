afe1d262e6c7167e2fd2054a8c501c7aa1c9f42e
Julius Pfrommer <julius.pfrommer@web.de>
2022-06-03 16:01:42 +0200

    refactor(deps): Make atoi a standalone library

--- deps/atoi.h
+++ deps/atoi.h
@@ -5,10 +5,28 @@
 extern "C" {
 #endif
 
-#include <open62541/types.h>
+#if !defined(_MSC_VER) || _MSC_VER >= 1800
+# include <stddef.h>
+# include <stdint.h>
+# include <stdbool.h> /* C99 Boolean */
+#else
+# include "ms_stdint.h"
+# if !defined(__bool_true_false_are_defined)
+#  define bool unsigned char
+#  define true 1
+#  define false 0
+#  define __bool_true_false_are_defined
+# endif
+#endif
 
-size_t atoiUnsigned(const char *pos, size_t size, UA_UInt64 *result);
-size_t atoiSigned(const char *pos, size_t size, UA_Int64 *result);
+/* Parse integer numbers. Returns the number of parsed digits until the first
+ * non-valid character. Returns 0 upon failure.
+ *
+ * Hex numbers start with 0x.
+ * Signed numbers may have a +/- prefix. */
+
+size_t atoiUnsigned(const char *pos, size_t size, uint64_t *result);
+size_t atoiSigned(const char *pos, size_t size, int64_t *result);
     
 #ifdef __cplusplus
 }
