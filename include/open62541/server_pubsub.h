5336835558ffbd7204363f1c4151748672c7c73e
Julius Pfrommer <julius.pfrommer@web.de>
2022-06-13 20:44:56 +0200

    feat(pubsub): Make UA_Server_updateWriterGroupConfig thread-safe

--- include/open62541/server_pubsub.h
+++ include/open62541/server_pubsub.h
@@ -517,7 +517,7 @@ UA_StatusCode UA_EXPORT
 UA_Server_getWriterGroupConfig(UA_Server *server, const UA_NodeId writerGroup,
                                UA_WriterGroupConfig *config);
 
-UA_StatusCode UA_EXPORT
+UA_StatusCode UA_EXPORT UA_THREADSAFE
 UA_Server_updateWriterGroupConfig(UA_Server *server, UA_NodeId writerGroupIdentifier,
                                   const UA_WriterGroupConfig *config);
 
