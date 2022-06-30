b9e9ca13bf407925078f9a7cf9dc1fc197024e18
Muddasir Shakil <muddasir.shakil@linutronix.de>
2022-06-30 17:35:33 +0200

    Add UA_PubSubKeyStorage struct
    A structured storage is required to store all the keys and related information,
    such as which security group, what security policy, how many PubSub Groups, and
    when to move to next keys. The UA_PubSubKeyStorage struct holds the list of keys
    used to secure the pubsub message for a security group and related information.
    
    Signed-off-by: Muddasir Shakil <muddasir.shakil@linutronix.de>

--- src/pubsub/ua_pubsub_keystorage.h
+++ src/pubsub/ua_pubsub_keystorage.h
@@ -34,6 +34,90 @@ typedef struct UA_PubSubKeyListItem {
     TAILQ_ENTRY(UA_PubSubKeyListItem) keyListEntry;
 } UA_PubSubKeyListItem;
 
+/* Queue Definition*/
+typedef TAILQ_HEAD(keyListItems, UA_PubSubKeyListItem) keyListItems;
+
+/**
+ * @brief This structure holds all info and keys related to one SecurityGroup.
+ * it is used as a list.
+ */
+typedef struct UA_PubSubKeyStorage {
+
+    /**
+     * security group id of the security group related to this storage
+     */
+    UA_String securityGroupID;
+
+    /**
+     * none-owning pointer to the security policy related to this storage
+     */
+    UA_PubSubSecurityPolicy *policy;
+
+    /**
+     * in case of the SKS server, the key storage structure is deleted when removing the
+     * security group.
+     * in case of publisher / subscriber, one key storage structure is
+     * referenced by multiple reader / writer groups have a reference count to manage free
+     */
+    UA_UInt32 referenceCount;
+
+    /**
+     * array of keys. the elements inside this array have a next pointer.
+     * keyList can therefore be used as linked list.
+     */
+    keyListItems keyList;
+
+    /**
+     * size of the keyList
+     */
+    size_t keyListSize;
+
+    /**
+     * The maximum number of Past keys a keystorage is allowed to store
+     */
+    UA_UInt32 maxPastKeyCount;
+
+    /**
+     * The maximum number of Future keys a keyStorage is allowed to store
+     */
+    UA_UInt32 maxFutureKeyCount;
+
+    /*
+     * The maximum keylist size, calculated from maxPastKeyCount and maxFutureKeyCount
+     */
+    UA_UInt32 maxKeyListSize;
+
+    /**
+     * The SecurityTokenId that appears in the header of messages secured with the
+     * CurrentKey. It starts at 1 and is incremented by 1 each time the KeyLifetime
+     * elapses even if no keys are requested. If the CurrentTokenId increments past the
+     * maximum value of UInt32 it restarts a 1.
+     */
+    UA_UInt32 currentTokenId;
+
+    /**
+     *  the current key used to secure the messages
+     */
+    UA_PubSubKeyListItem *currentItem;
+
+    /**
+     * keyLifeTime used to update the CurrentKey from the Local KeyStorage
+     */
+    UA_Duration keyLifeTime;
+
+    /**
+     * id used to register the callback to retrieve the keys related to this security
+     * group
+     */
+    UA_UInt64 callBackId;
+
+    /**
+     * Pointer to the key storage list
+     */
+    LIST_ENTRY(UA_PubSubKeyStorage) keyStorageList;
+
+} UA_PubSubKeyStorage;
+
 #endif
 
 _UA_END_DECLS
