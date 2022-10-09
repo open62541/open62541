147f0e6959398b66b3a8bb8f4aff570372d2a005
Julius Pfrommer <julius.pfrommer@web.de>
2022-10-09 20:53:23 +0200

    feat(el): Add eventloo_common.* with KeyValue parameter validation

---
+++ arch/eventloop_common.c
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *    Copyright 2022 (c) Fraunhofer IOSB (Author: Julius Pfrommer)
+ */
+
+#include "eventloop_common.h"
+
+UA_StatusCode
+UA_KeyValueRestriction_validate(const UA_KeyValueRestriction *restrictions, size_t restrictionsSize,
+                                const UA_KeyValuePair *params, size_t paramsSize) {
+    for(size_t i = 0; i < restrictionsSize; i++) {
+        const UA_KeyValueRestriction *r = &restrictions[i];
+        const UA_Variant *val = UA_KeyValueMap_get(params, paramsSize, r->name);
+
+        /* Value not present but required? */
+        if(!val) {
+            if(r->required)
+                return UA_STATUSCODE_BADINTERNALERROR;
+            continue;
+        }
+
+        /* Type matches */
+        if(val->type != r->type)
+            return UA_STATUSCODE_BADINTERNALERROR;
+
+        /* Scalar / array is allowed */
+        UA_Boolean scalar = UA_Variant_isScalar(val);
+        if(scalar && !r->scalar)
+            return UA_STATUSCODE_BADINTERNALERROR;
+        if(!scalar && !r->array)
+            return UA_STATUSCODE_BADCONNECTIONREJECTED;
+    }
+
+    return UA_STATUSCODE_GOOD;
+}
