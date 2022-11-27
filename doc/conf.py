e1327aa943e675b2df26cfeeca3de2d0d0694cea
Julius Pfrommer <julius.pfrommer@web.de>
2022-11-27 17:42:19 +0100

    refactor(doc): Improve the styling of the generated PDF

--- doc/conf.py
+++ doc/conf.py
@@ -215,21 +215,53 @@ latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 'papersize': 'a4paper',
 
-# The chapter title style
-'fncychap': '\\usepackage[Sonny]{fncychap}',
-
 # Font settings
 'fontpkg': r"""
-\usepackage{sourcesanspro}
-\usepackage{newtx}
 \usepackage[scaled=.98,varqu,varl]{zi4}
+\usepackage[default,semibold]{sourcesanspro}
 """,
 
 # The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+'pointsize': '11pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+# Clean up the header and footer
+'preamble': r"""
+\fancypagestyle{normal}{
+    \fancyhf{}
+    \fancyfoot[RO,LE]{\thepage}
+    \renewcommand{\headrulewidth}{0pt}
+    \renewcommand{\footrulewidth}{0pt}
+}
+\fancypagestyle{plain}{
+    \fancyhf{}
+    \fancyfoot[RO,LE]{\thepage}
+    \renewcommand{\headrulewidth}{0pt}
+    \renewcommand{\footrulewidth}{0pt}
+}""",
+
+'maketitle': r"""
+    \makeatletter
+ \begin{titlepage}%
+      \begingroup % for PDF information dictionary
+       \def\endgraf{ }\def\and{\& }%
+       \pdfstringdefDisableCommands{\def\\{, }}% overwrite hyperref setup
+       \hypersetup{pdfauthor={\@author}, pdftitle={\@title}}%
+      \endgroup
+    \begin{flushright}%
+	\vspace*{2\baselineskip}
+      \begin{minipage}{.75\linewidth}
+      \sphinxlogo
+      \end{minipage} \bigskip \par
+      \py@HeaderFamily
+      {\Huge \@title \par}
+      {\itshape\LARGE \py@release\releaseinfo \par}
+      \bigskip
+      {\large \@date \par}%
+    \end{flushright}
+  \end{titlepage}%
+  \if@openright\cleardoublepage\else\clearpage\fi
+\makeatother
+""",
 
 # Latex figure (float) alignment
 #'figure_align': 'htbp',
