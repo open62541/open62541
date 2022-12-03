5df60835bd6a0abed35135b333a3f82a9ad363a7
Julius Pfrommer <julius.pfrommer@web.de>
2022-12-03 09:23:35 +0100

    refactor(pubsub): Split out Connection and DataSet into separate compilation units

---
+++ src/pubsub/ua_pubsub_connection.c
@@ -0,0 +1,111 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2017-2022 Fraunhofer IOSB (Author: Andreas Ebner)
+ * Copyright (c) 2019 Fraunhofer IOSB (Author: Julius Pfrommer)
+ * Copyright (c) 2019 Kalycito Infotech Private Limited
+ * Copyright (c) 2021 Fraunhofer IOSB (Author: Jan Hermes)
+ * Copyright (c) 2022 Siemens AG (Author: Thomas Fischer)
+ * Copyright (c) 2022 Fraunhofer IOSB (Author: Noel Graf)
+ */
+
+#include "ua_pubsub.h"
+#include "server/ua_server_internal.h"
+
+#ifdef UA_ENABLE_PUBSUB /* conditional compilation */
+
+UA_StatusCode
+UA_PubSubConnectionConfig_copy(const UA_PubSubConnectionConfig *src,
+                               UA_PubSubConnectionConfig *dst) {
+    UA_StatusCode res = UA_STATUSCODE_GOOD;
+    memcpy(dst, src, sizeof(UA_PubSubConnectionConfig));
+    if (src->publisherIdType == UA_PUBLISHERIDTYPE_STRING) {
+        res |= UA_String_copy(&src->publisherId.string, &dst->publisherId.string);
+    }
+    res |= UA_String_copy(&src->name, &dst->name);
+    res |= UA_Variant_copy(&src->address, &dst->address);
+    res |= UA_String_copy(&src->transportProfileUri, &dst->transportProfileUri);
+    res |= UA_Variant_copy(&src->connectionTransportSettings,
+                           &dst->connectionTransportSettings);
+    if(src->connectionPropertiesSize > 0) {
+        dst->connectionProperties = (UA_KeyValuePair *)
+            UA_calloc(src->connectionPropertiesSize, sizeof(UA_KeyValuePair));
+        if(!dst->connectionProperties) {
+            UA_PubSubConnectionConfig_clear(dst);
+            return UA_STATUSCODE_BADOUTOFMEMORY;
+        }
+        for(size_t i = 0; i < src->connectionPropertiesSize; i++){
+            res |= UA_QualifiedName_copy(&src->connectionProperties[i].key,
+                                            &dst->connectionProperties[i].key);
+            res |= UA_Variant_copy(&src->connectionProperties[i].value,
+                                      &dst->connectionProperties[i].value);
+        }
+    }
+    if(res != UA_STATUSCODE_GOOD)
+        UA_PubSubConnectionConfig_clear(dst);
+    return res;
+}
+
+UA_StatusCode
+UA_Server_getPubSubConnectionConfig(UA_Server *server, const UA_NodeId connection,
+                                    UA_PubSubConnectionConfig *config) {
+    if(!config)
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    UA_PubSubConnection *currentPubSubConnection =
+        UA_PubSubConnection_findConnectionbyId(server, connection);
+    if(!currentPubSubConnection)
+        return UA_STATUSCODE_BADNOTFOUND;
+    return UA_PubSubConnectionConfig_copy(currentPubSubConnection->config, config);
+}
+
+UA_PubSubConnection *
+UA_PubSubConnection_findConnectionbyId(UA_Server *server, UA_NodeId connectionIdentifier) {
+    UA_PubSubConnection *pubSubConnection;
+    TAILQ_FOREACH(pubSubConnection, &server->pubSubManager.connections, listEntry){
+        if(UA_NodeId_equal(&connectionIdentifier, &pubSubConnection->identifier))
+            break;
+    }
+    return pubSubConnection;
+}
+
+void
+UA_PubSubConnectionConfig_clear(UA_PubSubConnectionConfig *connectionConfig) {
+    if (connectionConfig->publisherIdType == UA_PUBLISHERIDTYPE_STRING) {
+        UA_String_clear(&connectionConfig->publisherId.string);
+    }
+    UA_String_clear(&connectionConfig->name);
+    UA_String_clear(&connectionConfig->transportProfileUri);
+    UA_Variant_clear(&connectionConfig->connectionTransportSettings);
+    UA_Variant_clear(&connectionConfig->address);
+    for(size_t i = 0; i < connectionConfig->connectionPropertiesSize; i++){
+        UA_QualifiedName_clear(&connectionConfig->connectionProperties[i].key);
+        UA_Variant_clear(&connectionConfig->connectionProperties[i].value);
+    }
+    UA_free(connectionConfig->connectionProperties);
+}
+
+void
+UA_PubSubConnection_clear(UA_Server *server, UA_PubSubConnection *connection) {
+    /* Remove WriterGroups */
+    UA_WriterGroup *writerGroup, *tmpWriterGroup;
+    LIST_FOREACH_SAFE(writerGroup, &connection->writerGroups,
+                      listEntry, tmpWriterGroup) {
+        removeWriterGroup(server, writerGroup->identifier);
+    }
+
+    /* Remove ReaderGroups */
+    UA_ReaderGroup *readerGroups, *tmpReaderGroup;
+    LIST_FOREACH_SAFE(readerGroups, &connection->readerGroups, listEntry, tmpReaderGroup)
+        removeReaderGroup(server, readerGroups->identifier);
+
+    UA_NodeId_clear(&connection->identifier);
+    if(connection->channel)
+        connection->channel->close(connection->channel);
+
+    UA_PubSubConnectionConfig_clear(connection->config);
+    UA_free(connection->config);
+}
+
+
+#endif /* UA_ENABLE_PUBSUB */
