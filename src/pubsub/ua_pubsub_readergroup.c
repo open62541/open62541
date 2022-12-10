42d1be9158c158beecd88781e5bd8f2742e2f9e0
Julius Pfrommer <julius.pfrommer@web.de>
2022-12-10 11:26:24 +0100

    fix seteredergroupoperational

--- src/pubsub/ua_pubsub_readergroup.c
+++ src/pubsub/ua_pubsub_readergroup.c
@@ -470,7 +470,7 @@ UA_Server_setReaderGroupOperational(UA_Server *server, const UA_NodeId readerGro
     UA_LOCK(&server->serviceMutex);
     UA_StatusCode ret = UA_STATUSCODE_BADNOTFOUND;
     UA_ReaderGroup *rg = UA_ReaderGroup_findRGbyId(server, readerGroupId);
-    if(!rg)
+    if(rg)
         ret = UA_ReaderGroup_setPubSubState(server, rg, UA_PUBSUBSTATE_OPERATIONAL,
                                             UA_STATUSCODE_GOOD);
     UA_UNLOCK(&server->serviceMutex);
