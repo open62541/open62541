5e53b34f6b73af6a76d933e8113bb2f291ce2b0e
Marius Dege <marius.dege@basyskom.com>
2023-04-24 10:59:04 +0200

    Add stophandler to inheritance example

--- examples/server_inheritance.c
+++ examples/server_inheritance.c
@@ -4,6 +4,15 @@
 #include <open62541/plugin/log_stdout.h>
 #include <open62541/server.h>
 
+#include <signal.h>
+
+UA_Boolean running = true;
+
+static void stopHandler(int sign) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "received ctrl-c");
+    running = false;
+}
+
 /**
  * This will create a type structure and some instances of the types:
  *
@@ -246,12 +255,15 @@ createCustomInheritance(UA_Server *server) {
 }
 
 int main(void) {
+    signal(SIGINT, stopHandler);
+    signal(SIGTERM, stopHandler);
+
     UA_Server *server = UA_Server_new();
 
     createMammals(server);
     createCustomInheritance(server);
 
-    UA_StatusCode retval = UA_Server_runUntilInterrupt(server);
+    UA_StatusCode retval = UA_Server_run(server, &running);
 
     UA_Server_delete(server);
     return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
