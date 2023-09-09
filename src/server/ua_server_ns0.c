30589b22aab75bfa2a0566190a075de660bc7bb5
Julius Pfrommer <julius.pfrommer@web.de>
2023-09-09 13:13:07 +0200

    fix(server): Use "SubtypeOf" instead of "HasSuperType" as inverse ReferenceType name

--- src/server/ua_server_ns0.c
+++ src/server/ua_server_ns0.c
@@ -100,7 +100,7 @@ UA_Server_createNS0_base(UA_Server *server) {
     hassubtype_attr.displayName = UA_LOCALIZEDTEXT("", "HasSubtype");
     hassubtype_attr.isAbstract = false;
     hassubtype_attr.symmetric = false;
-    hassubtype_attr.inverseName = UA_LOCALIZEDTEXT("", "HasSupertype");
+    hassubtype_attr.inverseName = UA_LOCALIZEDTEXT("", "SubtypeOf");
     ret |= addNode_raw(server, UA_NODECLASS_REFERENCETYPE, UA_NS0ID_HASSUBTYPE, "HasSubtype",
                        &hassubtype_attr, &UA_TYPES[UA_TYPES_REFERENCETYPEATTRIBUTES]);
 
