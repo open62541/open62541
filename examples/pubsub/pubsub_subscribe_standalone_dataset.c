open62541/open62541

commit 928931ec36d4441fc12d27b11470ea96f7dc9b9b

refactor(core): Move architecture-specific definition out of the public headers

examples/pubsub/pubsub_subscribe_standalone_dataset.c
@@ -14,8 +14,8 @@
 #include <open62541/server_pubsub.h>
 #include <open62541/server_config_default.h>
 
-
 #include <signal.h>
+#include <stdio.h>
 
 UA_NodeId connectionIdentifier;
 UA_NodeId readerGroupIdentifier;

examples/pubsub/server_pubsub_publisher_on_demand.c
@@ -30,6 +30,7 @@
 #include <open62541/server_config_default.h>
 
 #include <signal.h>
+#include <stdio.h>
 
 UA_NodeId connectionIdent, publishedDataSetIdent, writerGroupIdent;
 

examples/pubsub/tutorial_pubsub_publish.c
@@ -28,6 +28,8 @@
 #include <open62541/server.h>
 #include <open62541/server_pubsub.h>
 
+#include <stdio.h>
+
 UA_NodeId connectionIdent, publishedDataSetIdent, writerGroupIdent;
 
 static void

arch/eventloop_mqtt.c
@@ -10,6 +10,15 @@
 
 #include <open62541/plugin/eventloop.h>
 
+#include <limits.h>
+
+#if defined(_MSC_VER)
+# include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#else
+# include <unistd.h> /* ssize_t */
+#endif
+
 struct MQTTConnectionManager;
 typedef struct MQTTConnectionManager MQTTConnectionManager;
 

arch/eventloop_posix.h
@@ -14,6 +14,13 @@
 
 #if defined(UA_ARCHITECTURE_POSIX) || defined(UA_ARCHITECTURE_WIN32)
 
+/* Include architecture-specific definitions */
+#if defined(UA_ARCHITECTURE_WIN32)
+#include "win32/ua_architecture.h"
+#elif defined(UA_ARCHITECTURE_POSIX)
+#include "posix/ua_architecture.h"
+#endif
+
 #include "common/ua_timer.h"
 #include "open62541_queue.h"
 

arch/posix/ua_architecture.h
@@ -73,8 +73,6 @@ void UA_sleep_ms(unsigned long ms);
 #define UA_POLLIN POLLIN
 #define UA_POLLOUT POLLOUT
 
-#define UA_ENABLE_LOG_COLORS
-
 #define UA_getnameinfo(sa, salen, host, hostlen, serv, servlen, flags) \
     getnameinfo(sa, salen, host, hostlen, serv, servlen, flags)
 #define UA_poll poll
@@ -105,15 +103,6 @@ void UA_sleep_ms(unsigned long ms);
 # define UA_if_nametoindex if_nametoindex
 #endif
 
-/* Use the standard malloc */
-#include <stdlib.h>
-#ifndef UA_free
-# define UA_free free
-# define UA_malloc malloc
-# define UA_calloc calloc
-# define UA_realloc realloc
-#endif
-
 #include <stdio.h>
 #include <strings.h>
 #define UA_snprintf snprintf
@@ -131,58 +120,6 @@ void UA_sleep_ms(unsigned long ms);
     LOG; \
     errno = 0; \
 }
-
-#if UA_MULTITHREADING >= 100
-
-#include <pthread.h>
-
-typedef struct {
-    pthread_mutex_t mutex;
-    int mutexCounter;
-} UA_Lock;
-
-#define UA_LOCK_STATIC_INIT {PTHREAD_MUTEX_INITIALIZER, 0}
-
-static UA_INLINE void
-UA_LOCK_INIT(UA_Lock *lock) {
-    pthread_mutex_init(&lock->mutex, NULL);
-    lock->mutexCounter = 0;
-}
-
-static UA_INLINE void
-UA_LOCK_DESTROY(UA_Lock *lock) {
-    pthread_mutex_destroy(&lock->mutex);
-}
-
-static UA_INLINE void
-UA_LOCK(UA_Lock *lock) {
-    pthread_mutex_lock(&lock->mutex);
-    UA_assert(lock->mutexCounter == 0);
-    lock->mutexCounter++;
-}
-
-static UA_INLINE void
-UA_UNLOCK(UA_Lock *lock) {
-    UA_assert(lock->mutexCounter == 1);
-    lock->mutexCounter--;
-    pthread_mutex_unlock(&lock->mutex);
-}
-
-static UA_INLINE void
-UA_LOCK_ASSERT(UA_Lock *lock, int num) {
-    UA_assert(lock->mutexCounter == num);
-}
-
-#else
-#define UA_EMPTY_STATEMENT                                                               \
-    do {                                                                                 \
-    } while(0)
-#define UA_LOCK_INIT(lock) UA_EMPTY_STATEMENT
-#define UA_LOCK_DESTROY(lock) UA_EMPTY_STATEMENT
-#define UA_LOCK(lock) UA_EMPTY_STATEMENT
-#define UA_UNLOCK(lock) UA_EMPTY_STATEMENT
-#define UA_LOCK_ASSERT(lock, num) UA_EMPTY_STATEMENT
-#endif
 
 #if defined(__APPLE__) && defined(_SYS_QUEUE_H_)
 //  in some compilers there's already a _SYS_QUEUE_H_ which is included first and doesn't

arch/win32/ua_architecture.h
@@ -131,14 +131,6 @@ void UA_sleep_ms(unsigned long ms);
 # undef maxStringLength
 #endif
 
-/* Use the standard malloc */
-#ifndef UA_free
-# define UA_free free
-# define UA_malloc malloc
-# define UA_calloc calloc
-# define UA_realloc realloc
-#endif
-
 #ifdef __CODEGEARC__
 #define _snprintf_s(a,b,c,...) snprintf(a,b,__VA_ARGS__)
 #endif
@@ -157,48 +149,6 @@ void UA_sleep_ms(unsigned long ms);
     LocalFree(errno_str); \
 }
 #define UA_LOG_SOCKET_ERRNO_GAI_WRAP UA_LOG_SOCKET_ERRNO_WRAP
-
-#if UA_MULTITHREADING >= 100
-
-typedef struct {
-    CRITICAL_SECTION mutex;
-    int mutexCounter;
-} UA_Lock;
-
-static UA_INLINE void
-UA_LOCK_INIT(UA_Lock *lock) {
-    InitializeCriticalSection(&lock->mutex);
-    lock->mutexCounter = 0;
-}
-
-static UA_INLINE void
-UA_LOCK_DESTROY(UA_Lock *lock) {
-    DeleteCriticalSection(&lock->mutex);
-}
-
-static UA_INLINE void
-UA_LOCK(UA_Lock *lock) {
-    EnterCriticalSection(&lock->mutex);
-    UA_assert(++(lock->mutexCounter) == 1);
-}
-
-static UA_INLINE void
-UA_UNLOCK(UA_Lock *lock) {
-    UA_assert(--(lock->mutexCounter) == 0);
-    LeaveCriticalSection(&lock->mutex);
-}
-
-static UA_INLINE void
-UA_LOCK_ASSERT(UA_Lock *lock, int num) {
-    UA_assert(lock->mutexCounter == num);
-}
-#else
-#define UA_LOCK_INIT(lock)
-#define UA_LOCK_DESTROY(lock)
-#define UA_LOCK(lock)
-#define UA_UNLOCK(lock)
-#define UA_LOCK_ASSERT(lock, num)
-#endif
 
 /* Fix redefinition of SLIST_ENTRY on mingw winnt.h */
 #if !defined(_SYS_QUEUE_H_) && defined(SLIST_ENTRY)

examples/access_control/client_access_control.c
@@ -9,6 +9,7 @@
 #include <open62541/client_highlevel.h>
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(void) {
     UA_Client *client = UA_Client_new();

commit faaaa0127c814f3413433ed43e97f60d4d719565

fix(ex): add missing headers
In 928931ec36d4 ("refactor(core): Move architecture-specific definition out of the public headers"),
headers were removed from the core headers, so some examples do no
longer build. Therefore, add them directly.

Fixes: 928931ec36d4 ("refactor(core): Move architecture-specific definition out of the public headers")
Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>

examples/pubsub/pubsub_subscribe_standalone_dataset.c
@@ -16,6 +16,7 @@
 
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 UA_NodeId connectionIdentifier;
 UA_NodeId readerGroupIdentifier;

examples/pubsub/server_pubsub_publisher_on_demand.c
@@ -31,6 +31,7 @@
 
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 UA_NodeId connectionIdent, publishedDataSetIdent, writerGroupIdent;
 

examples/pubsub/server_pubsub_rt_field_information_model.c
@@ -8,6 +8,7 @@
 #include <open62541/server_config_default.h>
 
 #include <signal.h>
+#include <stdlib.h>
 
 UA_Boolean running = true;
 UA_NodeId publishedDataSetIdent, dataSetFieldIdent, writerGroupIdent, connectionIdentifier;

examples/pubsub/tutorial_pubsub_connection.c
@@ -6,6 +6,8 @@
 #include <open62541/server.h>
 #include <open62541/server_pubsub.h>
 
+#include <stdlib.h>
+
 /**
  * The PubSub connection example demonstrate the PubSub TransportLayer configuration and
  * the dynamic creation of PubSub Connections on runtime.

examples/pubsub/tutorial_pubsub_publish.c
@@ -29,6 +29,7 @@
 #include <open62541/server_pubsub.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 
 UA_NodeId connectionIdent, publishedDataSetIdent, writerGroupIdent;
 

examples/pubsub/tutorial_pubsub_subscribe.c
@@ -19,6 +19,7 @@
 #include <open62541/server_pubsub.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 
 UA_NodeId connectionIdentifier;
 UA_NodeId readerGroupIdentifier;

examples/pubsub_realtime/pubsub_interrupt_publish.c
@@ -13,6 +13,7 @@
 #define _XOPEN_SOURCE 500
 #endif /* __STDC_VERSION__ */
 
+#include <errno.h>
 #include <signal.h>
 #include <stdio.h>
 #include <time.h>