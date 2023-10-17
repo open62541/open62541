c70356c18443d713085a9ce5057ce2dc6eeadc11
Julius Pfrommer <julius.pfrommer@web.de>
2023-10-17 23:26:19 +0200

    refactor(server): Simplify the Subscription state machine to a single enum

--- src/server/ua_subscription.h
+++ src/server/ua_subscription.h
@@ -226,12 +226,11 @@ UA_MonitoredItem_ensureQueueSpace(UA_Server *server, UA_MonitoredItem *mon);
 
 /* We use only a subset of the states defined in the standard */
 typedef enum {
-    /* UA_SUBSCRIPTIONSTATE_CLOSED */
-    /* UA_SUBSCRIPTIONSTATE_CREATING */
+    UA_SUBSCRIPTIONSTATE_DISABLED,
+    UA_SUBSCRIPTIONSTATE_REMOVING,
     UA_SUBSCRIPTIONSTATE_NORMAL,
     UA_SUBSCRIPTIONSTATE_LATE,
     UA_SUBSCRIPTIONSTATE_KEEPALIVE,
-    UA_SUBSCRIPTIONSTATE_REMOVING
 } UA_SubscriptionState;
 
 /* Subscriptions are managed in a server-wide linked list. If they are attached
@@ -253,7 +252,6 @@ struct UA_Subscription {
     UA_UInt32 maxKeepAliveCount;
     UA_Double publishingInterval; /* in ms */
     UA_UInt32 notificationsPerPublish;
-    UA_Boolean publishingEnabled;
     UA_Byte priority;
 
     /* Runtime information */
@@ -319,12 +317,10 @@ void
 UA_Subscription_delete(UA_Server *server, UA_Subscription *sub);
 
 UA_StatusCode
-Subscription_registerPublishCallback(UA_Server *server,
-                                     UA_Subscription *sub);
+Subscription_enable(UA_Server *server, UA_Subscription *sub);
 
 void
-Subscription_unregisterPublishCallback(UA_Server *server,
-                                       UA_Subscription *sub);
+Subscription_disable(UA_Server *server, UA_Subscription *sub);
 
 void
 Subscription_resetLifetime(UA_Subscription *sub);
