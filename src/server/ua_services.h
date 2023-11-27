6ab6949494d32717cbea9d58ff5b00f78fb21e78
Andreas Ebner <andreas.ebner@iosb.fraunhofer.de>
2023-11-27 08:23:54 +0100

    feat(server) interfaces and behavior for async read added, skeleton for async write added

--- src/server/ua_services.h
+++ src/server/ua_services.h
@@ -343,6 +343,13 @@ void Service_ReadAsync(UA_Server *server, UA_Session *session, UA_UInt32 request
 void Service_Write(UA_Server *server, UA_Session *session,
                    const UA_WriteRequest *request,
                    UA_WriteResponse *response);
+# if UA_MULTITHREADING >= 100
+void
+Service_WriteAsync(UA_Server *server, UA_Session *session, UA_UInt32 requestId,
+                   const UA_WriteRequest *request, UA_WriteResponse *response,
+                   UA_Boolean *finished);
+#endif
+
 
 /**
  * HistoryRead Service
