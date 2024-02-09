3f381ea37a8e2c0e352a0e05f49859627f753ac1
jas67 <jas67@users.noreply.github.com>
2024-02-09 09:42:02 -0500

    fix(examples): Added stdlib.h include server_file_based_config.c (#6268)

--- examples/file_based_server/server_file_based_config.c
+++ examples/file_based_server/server_file_based_config.c
@@ -1,6 +1,7 @@
 /* This work is licensed under a Creative Commons CCZero 1.0 Universal License.
  * See http://creativecommons.org/publicdomain/zero/1.0/ for more information. */
 
+#include <stdlib.h>
 #include <open62541/server.h>
 #include <open62541/server_config_default.h>
 #include <open62541/plugin/log_stdout.h>
