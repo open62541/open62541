e1426904adbdfe3d0bb448ad0ac2cac723cabfea
Julius Pfrommer <julius.pfrommer@web.de>
2024-02-10 23:18:44 +0100

    fix(core): Fix build with UA_ENABLE_TYPEDESCRIPTION disabled

--- src/ua_types.c
+++ src/ua_types.c
@@ -97,11 +97,13 @@ UA_cleanupDataTypeWithCustom(const UA_DataTypeArray *customTypes) {
         if (customTypes->cleanup) {
             for(size_t i = 0; i < customTypes->typesSize; ++i) {
                 const UA_DataType *type = &customTypes->types[i];
+#ifdef UA_ENABLE_TYPEDESCRIPTION
                 UA_free((void*)(uintptr_t)type->typeName);
                 for(size_t j = 0; j < type->membersSize; ++j) {
                     const UA_DataTypeMember *m = &type->members[j];
                     UA_free((void*)(uintptr_t)m->memberName);
                 }
+#endif
                 UA_free((void*)type->members);
             }
             UA_free((void*)(uintptr_t)customTypes->types);
