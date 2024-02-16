3e0463f214c2fc7426092280c121571a7b391b79
Florian Düwel <florian.duewel@iosb.fraunhofer.de>
2024-02-16 16:43:58 +0100

    add license to check_eventfilter_parser.c file

--- tests/server/check_eventfilter_parser.c
+++ tests/server/check_eventfilter_parser.c
@@ -1,10 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 #include "open62541/types_generated.h"
 #include "open62541/util.h"
-
-#include <stdio.h>
 #include <stdlib.h>
 #include "check.h"
-
 
 static UA_EventFilter filter;
 
