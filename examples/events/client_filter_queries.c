88409873530bb383baa452dabdbc8a80d207cd55
Florian Düwel <florian.duewel@iosb.fraunhofer.de>
2024-03-04 12:00:29 +0100

    feat (client): query language to generate OPC UA eventfilter

---
+++ examples/events/client_filter_queries.c
@@ -0,0 +1,242 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *    Copyright 2023-2024 (c) Fraunhofer IOSB (Author: Florian Düwel)
+ */
+
+#include <open62541/client.h>
+#include <open62541/client_config_default.h>
+#include <open62541/client_subscriptions.h>
+#include <open62541/plugin/log_stdout.h>
+#include <open62541/eventfilter_parser_examples.h>
+
+#include <signal.h>
+#include <stdio.h>
+
+/*
+ * This Tutorial repeats the client_eventfilter.c tutorial,
+ * however the filter are created based on the Query Language for Eventfilter
+ */
+static void check_eventfilter(UA_EventFilter *filter, UA_Boolean print_filter){
+    UA_EventFilter empty_filter;
+    UA_EventFilter_init(&empty_filter);
+    if(memcmp(&empty_filter, filter, sizeof(UA_EventFilter)) == 0){
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                    "Failed to parse the EventFilter");
+    }
+    else{
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                     "EventFilter parsing succeeded");
+        if(print_filter){
+            UA_String out = UA_STRING_NULL;
+            UA_print(filter, &UA_TYPES[UA_TYPES_EVENTFILTER], &out);
+            printf("%.*s\n", (int)out.length, out.data);
+            UA_String_clear(&out);
+        }
+    }
+}
+
+static UA_Boolean running = true;
+static UA_StatusCode
+read_queries(UA_UInt16 filterSelection, UA_EventFilter *filter){
+    switch(filterSelection){
+        case 0 : {
+            char *inp = "SELECT\n"
+                           "PATH \"/Message\", PATH \"/0:Severity\", PATH \"/EventType\"\n"
+                           "WHERE\n"
+                           "OR($\"ref_1\", $\"ref_2\")\n"
+                           "FOR\n"
+                           "$\"ref_2\":= OFTYPE ns=1;i=5003\n"
+                           "$\"ref_1\":= OFTYPE i=3035";
+
+            UA_ByteString case_0 = UA_String_fromChars(inp);
+            UA_EventFilter_parse(filter, &case_0);
+            check_eventfilter(filter, UA_FALSE);
+            UA_ByteString_clear(&case_0);
+            break;
+        }
+        case 1 : {
+            /*query can be found in the file example_queries/case_1 */
+            UA_ByteString case_1 = UA_String_fromChars(CASE_1);
+            UA_EventFilter_parse(filter, &case_1);
+            check_eventfilter(filter, UA_FALSE);
+            UA_ByteString_clear(&case_1);
+            break;
+        }
+        case 2 : {
+            /*query can be found in the file example_queries/case_2 */
+            UA_ByteString case_2 = UA_String_fromChars(CASE_2);
+            UA_EventFilter_parse(filter, &case_2);
+            check_eventfilter(filter, UA_FALSE);
+            UA_ByteString_clear(&case_2);
+            break;
+        }
+        case 3 : {
+            char *inp = "SELECT\n"
+                         "\n"
+                         "PATH \"/Message\",\n"
+                         "PATH \"/Severity\",\n"
+                         "PATH \"/EventType\"\n"
+                         "\n"
+                         "WHERE\n"
+                         "AND((OFTYPE ns=1;i=5001), $1)\n"
+                         "\n"
+                         "FOR\n"
+                         "$1:=  AND($20, $30)\n"
+                         "$20:= INT64 99 == 99\n"
+                         "$30:= TYPEID i=5000 PATH \"/Severity\" > 99";
+
+            UA_ByteString case_3 = UA_String_fromChars(inp);
+            UA_EventFilter_parse(filter, &case_3);
+            check_eventfilter(filter, UA_FALSE);
+            UA_ByteString_clear(&case_3);
+            break;
+        }
+        case 4 : {
+            char *inp = "SELECT\n"
+                        "\n"
+                        "PATH \"/Message\",\n"
+                        "PATH \"/0:Severity\",\n"
+                        "PATH \"/EventType\"\n"
+                        "\n"
+                        "WHERE\n"
+                        "\n"
+                        "AND($4, TYPEID i=5000 PATH \"/Severity\" GREATERTHAN $\"ref\")\n"
+                        "\n"
+                        "FOR\n"
+                        "$\"ref\":= 99\n"
+                        "$4:= OFTYPE ns=1;i=5000";
+            UA_ByteString case_4 = UA_String_fromChars(inp);
+            UA_EventFilter_parse(filter, &case_4);
+            check_eventfilter(filter, UA_FALSE);
+            UA_ByteString_clear(&case_4);
+            break;
+        }
+        default:
+            UA_EventFilter_clear(filter);
+            return UA_STATUSCODE_BADCONFIGURATIONERROR;
+    }
+    return UA_STATUSCODE_GOOD;
+}
+
+static void
+handler_events_filter(UA_Client *client, UA_UInt32 subId, void *subContext,
+                      UA_UInt32 monId, void *monContext,
+                      size_t nEventFields, UA_Variant *eventFields) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Received Event Notification (Filter passed)");
+    for(size_t i = 0; i < nEventFields; ++i) {
+        if(UA_Variant_hasScalarType(&eventFields[i], &UA_TYPES[UA_TYPES_UINT16])) {
+            UA_UInt16 severity = *(UA_UInt16 *)eventFields[i].data;
+            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Severity: %u", severity);
+        } else if (UA_Variant_hasScalarType(&eventFields[i], &UA_TYPES[UA_TYPES_LOCALIZEDTEXT])) {
+            UA_LocalizedText *lt = (UA_LocalizedText *)eventFields[i].data;
+            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                        "Message: '%.*s'", (int)lt->text.length, lt->text.data);
+        } else if (UA_Variant_hasScalarType(&eventFields[i], &UA_TYPES[UA_TYPES_NODEID])) {
+            UA_String nodeIdName = UA_STRING_ALLOC("");
+            UA_NodeId_print((UA_NodeId *)eventFields[i].data, &nodeIdName);
+            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                        "TypeNodeId: '%.*s'", (int)nodeIdName.length, nodeIdName.data);
+            UA_String_clear(&nodeIdName);
+        } else {
+#ifdef UA_ENABLE_TYPEDESCRIPTION
+            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                        "Don't know how to handle type: '%s'", eventFields[i].type->typeName);
+#else
+            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                        "Don't know how to handle type, enable UA_ENABLE_TYPEDESCRIPTION "
+                        "for typename");
+#endif
+        }
+    }
+}
+
+
+static UA_StatusCode create_event_filter_with_monitored_item(UA_UInt16 filterSelection, UA_Client *client, UA_EventFilter *filter, UA_CreateSubscriptionResponse *response, UA_MonitoredItemCreateResult *result){
+    /* read the eventfilter query string and create the corresponding eventfilter */
+    UA_StatusCode retval = read_queries(filterSelection, filter);
+    if(retval != UA_STATUSCODE_GOOD) {
+            UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Failed to parse the filter query with statuscode %s \n", UA_StatusCode_name(retval));
+            return retval;
+    }
+    /* Create a subscription */
+    UA_CreateSubscriptionRequest request = UA_CreateSubscriptionRequest_default();
+    *response = UA_Client_Subscriptions_create(client, request, NULL, NULL, NULL);
+    if(response->responseHeader.serviceResult != UA_STATUSCODE_GOOD)
+        return response->responseHeader.serviceResult;
+
+    UA_UInt32 subId = response->subscriptionId;
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Create subscription succeeded, id %u", subId);
+
+    /* Add a MonitoredItem */
+    UA_MonitoredItemCreateRequest item;
+    UA_MonitoredItemCreateRequest_init(&item);
+    item.itemToMonitor.nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER); // Root->Objects->Server
+    item.itemToMonitor.attributeId = UA_ATTRIBUTEID_EVENTNOTIFIER;
+    item.monitoringMode = UA_MONITORINGMODE_REPORTING;
+    item.requestedParameters.filter.encoding = UA_EXTENSIONOBJECT_DECODED;
+    item.requestedParameters.filter.content.decoded.data = filter;
+    item.requestedParameters.filter.content.decoded.type = &UA_TYPES[UA_TYPES_EVENTFILTER];
+
+    UA_UInt32 monId = 0;
+    *result = UA_Client_MonitoredItems_createEvent(client, subId,
+                                                     UA_TIMESTAMPSTORETURN_BOTH,
+                                                     item,
+                                                     &monId, handler_events_filter, NULL);
+
+    if(result->statusCode != UA_STATUSCODE_GOOD) {
+            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                            "Could not add the MonitoredItem with %s",
+                            UA_StatusCode_name(result->statusCode));
+            //UA_MonitoredItemCreateResult_clear(&result);
+            return UA_STATUSCODE_BAD;
+    }
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                            "Monitoring 'Root->Objects->Server', id %u",
+                            response->subscriptionId);
+    monId = result->monitoredItemId;
+    return UA_STATUSCODE_GOOD;
+}
+
+static void stopHandler(int sig) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
+    running = false;
+}
+
+int main(int argc, char *argv[]) {
+    signal(SIGINT, stopHandler);
+    signal(SIGTERM, stopHandler);
+
+    if(argc < 2) {
+        printf("Usage: tutorial_client_event_filter <opc.tcp://server-url>\n");
+        return 0;
+    }
+    UA_Client *client = UA_Client_new();
+    UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+    UA_StatusCode retval = UA_Client_connect(client, argv[1]);
+    if(retval != UA_STATUSCODE_GOOD) {
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Could not connect");
+        UA_Client_delete(client);
+        return 0;
+    }
+
+    UA_EventFilter filter;
+    UA_EventFilter_init(&filter);
+    UA_CreateSubscriptionResponse *response = UA_CreateSubscriptionResponse_new();
+    UA_MonitoredItemCreateResult *result = UA_MonitoredItemCreateResult_new();
+    retval = create_event_filter_with_monitored_item(3, client, &filter, response, result);
+    if(retval == UA_STATUSCODE_GOOD){
+        while(running)
+            UA_Client_run_iterate(client, true);
+    }
+    /* Delete the subscription */
+    UA_Client_Subscriptions_deleteSingle(client, response->subscriptionId);
+    UA_MonitoredItemCreateResult_delete(result);
+    UA_CreateSubscriptionResponse_delete(response);
+    UA_EventFilter_clear(&filter);
+    UA_Client_disconnect(client);
+    UA_Client_delete(client);
+
+    return 0;
+}
---
+++ src/util/eventfilter_parser/eventfilter_parser.h
@@ -0,0 +1,133 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *    Copyright 2023-2024 (c) Fraunhofer IOSB (Author: Florian Düwel)
+ */
+
+#ifndef OPEN62541_NODEID_SETUP_FUNCTIONS_H
+#define OPEN62541_NODEID_SETUP_FUNCTIONS_H
+
+
+#include "open62541/plugin/log_stdout.h"
+#include "open62541/server.h"
+#include "open62541/types_generated.h"
+#include "open62541_queue.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "cj5.h"
+
+_UA_BEGIN_DECLS
+
+
+typedef struct UA_Parsed_EventFilter {
+    UA_EventFilter filter;
+    UA_StatusCode status;
+}UA_Parsed_EventFilter;
+
+typedef enum OperandIdentifier{
+    ELEMENTOPERAND,
+    EXTENSIONOBJECT
+}OperandIdentifier;
+
+typedef struct UA_Parsed_Operand{
+    OperandIdentifier identifier;
+    union{
+        char *element_ref;
+        UA_ExtensionObject extension;
+    }value;
+}UA_Parsed_Operand;
+
+typedef struct UA_Parsed_Operator{
+    UA_FilterOperator filter;
+    size_t nbr_children;
+    UA_Parsed_Operand *children;
+    size_t ContentFilterArrayPosition;
+}UA_Parsed_Operator;
+
+typedef enum ElementIdentifier{
+    PARSEDOPERAND,
+    parsed_operator
+}ElementIdentifier;
+
+typedef union UA_Parsed_Element{
+    UA_Parsed_Operator oper;
+    UA_Parsed_Operand operand;
+}UA_Parsed_Element;
+
+typedef struct Parsed_Element{
+    char *ref;
+    ElementIdentifier identifier;
+    UA_Parsed_Element element;
+    TAILQ_ENTRY(Parsed_Element) element_entries;
+}UA_Parsed_Element_List;
+
+typedef TAILQ_HEAD(parsed_filter_elements, Parsed_Element) parsed_filter_elements;
+
+typedef struct UA_Element_List{
+    parsed_filter_elements head;
+}UA_Element_List;
+
+typedef union UA_Local_Operand{
+    UA_SimpleAttributeOperand sao;
+    UA_NodeId id;
+    char *str;
+    UA_LiteralOperand literal;
+} UA_Local_Operand;
+
+typedef struct counters{
+    size_t branch_element_number;
+    size_t for_operator_reference;
+    size_t operand_ctr;
+}UA_Counters;
+
+void save_string(char *str, char **local_str);
+void create_next_operand_element(UA_Element_List *elements, UA_Parsed_Operand *operand, char *ref);
+UA_StatusCode create_content_filter(UA_Element_List *elements, UA_ContentFilter *filter, char *first_element, UA_StatusCode status);
+void add_new_operator(UA_Element_List *global, char *operator_ref, UA_Parsed_Operator *element);
+UA_StatusCode append_select_clauses(UA_SimpleAttributeOperand **select_clauses, size_t *sao_size, UA_ExtensionObject *extension, UA_StatusCode status);
+UA_StatusCode set_up_browsepath(UA_QualifiedName **q_name_list, size_t *size, char *str, UA_StatusCode status);
+UA_StatusCode create_literal_operand(char *string, UA_LiteralOperand *lit, UA_StatusCode status);
+UA_StatusCode create_nodeId_from_string(char *identifier, UA_NodeId *id, UA_StatusCode status);
+void handle_elementoperand(UA_Parsed_Operand *operand, char *ref);
+void handle_sao(UA_SimpleAttributeOperand *simple, UA_Parsed_Operand *operand);
+void add_operand_from_branch(char **ref, size_t *operand_ctr, UA_Parsed_Operand *operand, UA_Element_List *global);
+void set_up_variant_from_nodeId(UA_NodeId *id, UA_Variant *litvalue);
+void handle_oftype_nodeId(UA_Parsed_Operator *element, UA_NodeId *id);
+void handle_literal_operand(UA_Parsed_Operand *operand, UA_LiteralOperand *literalValue);
+void set_up_typeid(UA_Local_Operand *operand);
+void handle_between_operator(UA_Parsed_Operator *element, UA_Parsed_Operand *operand_1, UA_Parsed_Operand *operand_2, UA_Parsed_Operand *operand_3);
+void handle_two_operands_operator(UA_Parsed_Operator *element, UA_Parsed_Operand *operand_1, UA_Parsed_Operand *operand_2, UA_FilterOperator *filter);
+void init_item_list(UA_Element_List *global, UA_Counters *ctr);
+void create_branch_element(UA_Element_List *global, size_t *branch_element_number, UA_FilterOperator filteroperator, char *ref_1, char *ref_2, char **ref);
+void handle_for_operator(UA_Element_List *global, size_t *for_operator_reference, char **ref, UA_Parsed_Operator *element);
+void change_element_reference(UA_Element_List *global, char *element_name, char *new_element_reference);
+void add_in_list_children(UA_Element_List *global, UA_Parsed_Operand *oper);
+void create_in_list_operator(UA_Element_List *global, UA_Parsed_Operand *oper, char *element_ref);
+void append_string(char **string, char *yytext);
+void set_up_variant_from_bool(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_string(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_bstring(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_float(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_double(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_sbyte(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_statuscode(char *yytext, UA_Variant *litvalue);
+UA_StatusCode set_up_variant_from_expnodeid(char *yytext, UA_Variant *litvalue, UA_StatusCode status);
+void set_up_variant_from_time(const char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_byte(char *yytext, UA_Variant *litvalue);
+UA_StatusCode set_up_variant_from_qname(char *str, UA_Variant *litvalue, UA_StatusCode status);
+void set_up_variant_from_guid(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_int64(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_localized(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_uint16(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_uint32(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_uint64(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_int16(char *yytext, UA_Variant *litvalue);
+void set_up_variant_from_int32(char *yytext, UA_Variant *litvalue);
+void create_nodeid_element(UA_Element_List *elements, UA_NodeId *id, char *ref);
+void add_child_operands(UA_Parsed_Operand *operand_list, size_t operand_list_size, UA_Parsed_Operator *element, UA_FilterOperator oper);
+
+_UA_END_DECLS
+
+#endif //OPEN62541_NODEID_SETUP_FUNCTIONS_H
