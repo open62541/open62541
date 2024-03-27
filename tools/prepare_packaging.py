9e0b766fbba73be8d703b6ccbfdeec5bfd200b48
Julius Pfrommer <julius.pfrommer@web.de>
2024-03-27 17:39:23 +0100

    fix(build): Reactivate building of Debian packages

--- tools/prepare_packaging.py
+++ tools/prepare_packaging.py
@@ -88,21 +88,6 @@ install_file = os.path.join(target_debian_path,
                   "libopen62541-{}.{}-dev.install".format(version_major, version_minor))
 shutil.copy(install_file_template, install_file)
 
-install_file_template = os.path.join(debian_path, "libopen62541-tools.install-template")
-install_file = os.path.join(target_debian_path,
-                  "libopen62541-{}.{}-tools.install".format(version_major, version_minor))
-shutil.copy(install_file_template, install_file)
-
-install_file_template = os.path.join(debian_path, "libopen62541-doc.doc-base-template")
-install_file = os.path.join(target_debian_path,
-                  "libopen62541-{}.{}-doc.doc-base".format(version_major, version_minor))
-shutil.copy(install_file_template, install_file)
-
-install_file_template = os.path.join(debian_path, "libopen62541-doc.install-template")
-install_file = os.path.join(target_debian_path,
-                  "libopen62541-{}.{}-doc.install".format(version_major, version_minor))
-shutil.copy(install_file_template, install_file)
-
 # Create rule file and replace template variables
 rule_file_template = os.path.join(debian_path, "rules-template")
 rule_file = os.path.join(target_debian_path, "rules")
