be2f2f1a7a5af760e7cc32bab40e361f5862d789
Julius Pfrommer <julius.pfrommer@web.de>
2024-05-01 19:55:52 +0200

    refactor(util): Make the logger configurable for the EventFilter query parser

--- tests/server/check_eventfilter_parser.c
+++ tests/server/check_eventfilter_parser.c
@@ -10,7 +10,7 @@ static UA_EventFilter filter;
 START_TEST(Case_0) {
     char *inp = "SELECT /Message#Value, /Severity";
     UA_String case0 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case0);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case0, NULL);
     ck_assert_int_eq(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
@@ -19,7 +19,7 @@ START_TEST(Case_1) {
     char *inp = "SELECT /Message, /Severity, /EventType "
                 "WHERE OFTYPE ns=1;i=5001";
     UA_String case1 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case1);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case1, NULL);
     ck_assert_int_eq(res, UA_STATUSCODE_GOOD);
 
     UA_NodeId typeId = UA_NODEID_NUMERIC(1, 5001);
@@ -33,7 +33,7 @@ START_TEST(Case_2) {
     char *inp = "SELECT /Message, /Severity, /EventType "
                 "WHERE /Severity >= 1000";
     UA_String case2 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case2);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case2, NULL);
     ck_assert_int_eq(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
@@ -47,8 +47,8 @@ START_TEST(Case_3) {
     UA_EventFilter filter, filter2;
     UA_String case1 = UA_STRING(inp);
     UA_String case2 = UA_STRING(inp2);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case1);
-    res |= UA_EventFilter_parse(&filter2, &case2);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case1, NULL);
+    res |= UA_EventFilter_parse(&filter2, case2, NULL);
     ck_assert_int_eq(res, UA_STATUSCODE_GOOD);
     ck_assert(UA_EventFilter_equal(&filter, &filter));
     UA_EventFilter_clear(&filter);
@@ -64,8 +64,8 @@ START_TEST(Case_4) {
     UA_EventFilter filter, filter2;
     UA_String case1 = UA_STRING(inp);
     UA_String case2 = UA_STRING(inp2);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case1);
-    res |= UA_EventFilter_parse(&filter2, &case2);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case1, NULL);
+    res |= UA_EventFilter_parse(&filter2, case2, NULL);
     ck_assert_int_eq(res, UA_STATUSCODE_GOOD);
     ck_assert(UA_EventFilter_equal(&filter, &filter));
     UA_EventFilter_clear(&filter);
@@ -81,8 +81,8 @@ START_TEST(Case_5) {
     UA_EventFilter filter, filter2;
     UA_String case1 = UA_STRING(inp);
     UA_String case2 = UA_STRING(inp2);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case1);
-    res |= UA_EventFilter_parse(&filter2, &case2);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case1, NULL);
+    res |= UA_EventFilter_parse(&filter2, case2, NULL);
     ck_assert_int_eq(res, UA_STATUSCODE_GOOD);
     ck_assert(UA_EventFilter_equal(&filter, &filter));
     UA_EventFilter_clear(&filter);
@@ -97,8 +97,8 @@ START_TEST(Case_6) {
     UA_EventFilter filter, filter2;
     UA_String case1 = UA_STRING(inp);
     UA_String case2 = UA_STRING(inp2);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case1);
-    res |= UA_EventFilter_parse(&filter2, &case2);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case1, NULL);
+    res |= UA_EventFilter_parse(&filter2, case2, NULL);
     ck_assert_int_eq(res, UA_STATUSCODE_GOOD);
     ck_assert(UA_EventFilter_equal(&filter, &filter));
     UA_EventFilter_clear(&filter);
@@ -110,7 +110,7 @@ START_TEST(Case_7) {
     char *inp = "SELECT /Severity WHERE $test1 "
                 "FOR $test1 := $test2, $test2 := $test1";
     UA_String case7 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case7);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case7, NULL);
     ck_assert_int_ne(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
@@ -120,7 +120,7 @@ START_TEST(Case_8) {
     char *inp = "SELECT /Severity WHERE $test1 "
         "FOR $test1 := 123, $test1 := 456";
     UA_String case8 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case8);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case8, NULL);
     ck_assert_int_ne(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
@@ -129,7 +129,7 @@ START_TEST(Case_8) {
 START_TEST(Case_9) {
     char *inp = "SELECT /Severity WHERE $test1";
     UA_String case9 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case9);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case9, NULL);
     ck_assert_int_ne(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
@@ -138,7 +138,7 @@ START_TEST(Case_9) {
 START_TEST(Case_10) {
     char *inp = "SELECT $select1 FOR $select1 := /Severity";
     UA_String case10 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case10);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case10, NULL);
     ck_assert_int_eq(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
@@ -147,7 +147,7 @@ START_TEST(Case_10) {
 START_TEST(Case_11) {
     char *inp = "SELECT $select1";
     UA_String case11 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case11);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case11, NULL);
     ck_assert_int_ne(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
@@ -157,7 +157,7 @@ START_TEST(Case_12) {
     char *inp = "SELECT /Severity "
                 "WHERE /Value == {\"Type\": 3,\"Body\": [1,2,1,5],\"Dimension\": [2,2]}";
     UA_String case1 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case1);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case1, NULL);
     ck_assert_int_eq(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
@@ -166,7 +166,7 @@ START_TEST(Case_12) {
 START_TEST(Case_13) {
     char *inp = "SELECT2 /Severity";
     UA_String case1 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case1);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case1, NULL);
     ck_assert_int_ne(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
@@ -175,7 +175,7 @@ START_TEST(Case_13) {
 START_TEST(Case_14) {
     char *inp = "SELECT WHERE";
     UA_String case1 = UA_STRING(inp);
-    UA_StatusCode res = UA_EventFilter_parse(&filter, &case1);
+    UA_StatusCode res = UA_EventFilter_parse(&filter, case1, NULL);
     ck_assert_int_ne(res, UA_STATUSCODE_GOOD);
     UA_EventFilter_clear(&filter);
 } END_TEST
