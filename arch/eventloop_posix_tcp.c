1abe34be7f652e3ecaf4dfaa52762e8a4bf2becc
Julius Pfrommer <julius.pfrommer@web.de>
2024-05-09 14:45:48 +0200

    feat(arch): Set REUSEADDR on all TCP sockets right before closing them
    This ensures the server can be restarted without waiting for the
    timeout. And port-stealing is prevented during the runtime of the
    server.

--- arch/eventloop_posix_tcp.c
+++ arch/eventloop_posix_tcp.c
@@ -75,6 +75,11 @@ TCP_delayedClose(void *application, void *context) {
                  "TCP %u\t| Delayed closing of the connection",
                  (unsigned)conn->rfd.fd);
 
+    /* Ensure reuse is possible right away. Port-stealing is no longer an issue
+     * as the socket gets closed anyway. And we do not want to wait for the
+     * timeout to open a new socket for the same address and port. */
+    UA_EventLoopPOSIX_setReusable(conn->rfd.fd);
+
     /* Deregister from the EventLoop */
     UA_EventLoopPOSIX_deregisterFD(el, &conn->rfd);
 
@@ -447,6 +452,7 @@ TCP_registerListenSocket(UA_POSIXConnectionManager *pcm, struct addrinfo *ai,
 
     /* Only validate, don't actually start listening */
     if(validate) {
+        UA_EventLoopPOSIX_setReusable(listenSocket); /* Ensure reuse is possible */
         UA_close(listenSocket);
         return UA_STATUSCODE_GOOD;
     }
@@ -457,6 +463,7 @@ TCP_registerListenSocket(UA_POSIXConnectionManager *pcm, struct addrinfo *ai,
            UA_LOG_WARNING(el->eventLoop.logger, UA_LOGCATEGORY_NETWORK,
                           "TCP %u\t| Error listening on the socket (%s)",
                           (unsigned)listenSocket, errno_str));
+        UA_EventLoopPOSIX_setReusable(listenSocket); /* Ensure reuse is possible */
         UA_close(listenSocket);
         return UA_STATUSCODE_BADINTERNALERROR;
     }
@@ -467,6 +474,7 @@ TCP_registerListenSocket(UA_POSIXConnectionManager *pcm, struct addrinfo *ai,
         UA_LOG_WARNING(el->eventLoop.logger, UA_LOGCATEGORY_NETWORK,
                        "TCP %u\t| Error allocating memory for the socket",
                        (unsigned)listenSocket);
+        UA_EventLoopPOSIX_setReusable(listenSocket); /* Ensure reuse is possible */
         UA_close(listenSocket);
         return UA_STATUSCODE_BADINTERNALERROR;
     }
@@ -486,6 +494,7 @@ TCP_registerListenSocket(UA_POSIXConnectionManager *pcm, struct addrinfo *ai,
                        "TCP %u\t| Error registering the socket",
                        (unsigned)listenSocket);
         UA_free(newConn);
+        UA_EventLoopPOSIX_setReusable(listenSocket); /* Ensure reuse is possible */
         UA_close(listenSocket);
         return res;
     }
