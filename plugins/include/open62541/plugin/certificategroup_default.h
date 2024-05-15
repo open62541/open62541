dd6ce3a88d9d59ba496bb5223e19505ec15d8b1f
Noel Graf <noel.gerome.graf@gmail.com>
2024-05-15 10:43:45 +0200

    feat(server): Implement filestore backend for certificate groups

--- plugins/include/open62541/plugin/certificategroup_default.h
+++ plugins/include/open62541/plugin/certificategroup_default.h
@@ -30,6 +30,16 @@ UA_CertificateVerification_Trustlist(UA_CertificateGroup *certGroup,
                                      const UA_ByteString *certificateRevocationList,
                                      size_t certificateRevocationListSize);
 
+/*
+ * **Configuration parameters for the Certificate Memorystore backend**
+ *
+ * 0:max-trust-listsize [uint32]
+ *    The maximum trust list size in bytes. (default 64KiB).
+ *
+ * 0:max-rejected-listsize [uint32]
+ *    The maximum number of certificate files that can be stored in the rejected list.
+ *    (default: 100).
+ */
 UA_EXPORT UA_StatusCode
 UA_CertificateGroup_Memorystore(UA_CertificateGroup *certGroup,
                                 UA_NodeId *certificateGroupId,
@@ -51,6 +61,21 @@ UA_CertificateVerification_CertFolders(UA_CertificateGroup *certGroup,
                                        const char *issuerListFolder,
                                        const char *revocationListFolder);
 #endif
+
+/*
+ * **Configuration parameters for the Certificate Filestore backend**
+ *
+ * 0:max-trust-listsize [uint32]
+ *    The maximum trust list size in bytes. (default 64KiB).
+ *
+ * 0:max-rejected-listsize [uint32]
+ *    The maximum number of certificate files that can be stored in the rejected list.
+ *    (default: 100).
+ */
+UA_EXPORT UA_StatusCode
+UA_CertificateGroup_Filestore(UA_CertificateGroup *certGroup,
+                              UA_NodeId *certificateGroupId,
+                              const UA_String *storePath);
 #endif
 
 #endif
