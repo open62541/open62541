6bae4af47adb3271433287cb5631180bfd7f8f89
Julius Pfrommer <julius.pfrommer@web.de>
2024-06-10 21:24:54 +0200

    refactor(examples): Clean up server_pubsub_subscriber_rt_level.c

--- examples/pubsub_realtime/server_pubsub_subscriber_rt_level.c
+++ examples/pubsub_realtime/server_pubsub_subscriber_rt_level.c
@@ -5,25 +5,26 @@
 #include <open62541/server.h>
 #include <open62541/server_pubsub.h>
 #include <open62541/server_config_default.h>
+#include <open62541/types.h>
 #include <open62541/types_generated.h>
 
 #include <stdio.h>
-#include <signal.h>
 #include <stdlib.h>
 #include <assert.h>
 
 #define PUBSUB_CONFIG_FIELD_COUNT 10
 
 /**
- * The main target of this example is to reduce the time spread and effort during the subscribe cycle.
- * This RT level example is based on buffered DataSetMessages and NetworkMessages. Since changes in the
- * PubSub-configuration will invalidate the buffered frames, the PubSub configuration
- * must be frozen after the configuration phase.
+ * The main target of this example is to reduce the time spread and effort
+ * during the subscribe cycle. This RT level example is based on buffered
+ * DataSetMessages and NetworkMessages. Since changes in the
+ * PubSub-configuration will invalidate the buffered frames, the PubSub
+ * configuration must be frozen after the configuration phase.
  *
- * After the PubSub-configuration freeze, the NetworkMessages and DataSetMessages
- * will be calculated and buffered. During the subscribe cycle, decoding will happen only to the necessary offsets and
- * the buffered NetworkMessage will only be updated. Support only for one DSM.
- * PUBSUB_CONFIG_FASTPATH_STATIC_VALUES -> TODO
+ * After the PubSub-configuration freeze, the NetworkMessages and
+ * DataSetMessages will be calculated and buffered. During the subscribe cycle,
+ * decoding will happen only to the necessary offsets and the buffered
+ * NetworkMessage will only be updated.
  */
 
 UA_NodeId connectionIdentifier;
@@ -32,11 +33,9 @@ UA_NodeId readerIdentifier;
 
 UA_DataSetReaderConfig readerConfig;
 
-UA_UInt32    *repeatedFieldValues[PUBSUB_CONFIG_FIELD_COUNT];
+/* Simulate a custom data sink (e.g. shared memory) */
+UA_UInt32     repeatedFieldValues[PUBSUB_CONFIG_FIELD_COUNT];
 UA_DataValue *repeatedDataValueRT[PUBSUB_CONFIG_FIELD_COUNT];
-
-static UA_UInt32 sSubscribedDataSink[PUBSUB_CONFIG_FIELD_COUNT];    /* simulate a custom data sink (e.g. shared memory) */
-static UA_UInt32 sSubscribedTargetVarDataOffset[PUBSUB_CONFIG_FIELD_COUNT];
 
 /* If the external data source is written over the information model, the
  * externalDataWriteCallback will be triggered. The user has to take care and assure
@@ -60,21 +59,19 @@ externalDataReadNotificationCallback(UA_Server *server, const UA_NodeId *session
 }
 
 static void
-subscribeAfterWriteCallback(
-   UA_Server *server,
-   const UA_NodeId *dataSetReaderId,
-   const UA_NodeId *readerGroupId,
-   const UA_NodeId *targetVariableId,
-   void *targetVariableContext,
-   UA_DataValue **externalDataValue) {    // received value has already been copied to param externalDataValue
-
+subscribeAfterWriteCallback(UA_Server *server, const UA_NodeId *dataSetReaderId,
+                            const UA_NodeId *readerGroupId,
+                            const UA_NodeId *targetVariableId,
+                            void *targetVariableContext,
+                            UA_DataValue **externalDataValue) {
     (void) server;
     (void) dataSetReaderId;
     (void) readerGroupId;
+    (void) targetVariableContext;
 
     assert(targetVariableId != 0);
-    assert(targetVariableContext != 0);
     assert(externalDataValue != 0);
+
     UA_String strId;
     UA_String_init(&strId);
     UA_NodeId_print(targetVariableId, &strId);
@@ -84,50 +81,33 @@ subscribeAfterWriteCallback(
     UA_String_clear(&strId);
 }
 
-/* callback gets triggered before subscriber has received data
-    received data hasn't been copied/handled yet */
+/* Callback gets triggered before subscriber has received data received data
+ * hasn't been copied/handled yet */
 static void
-subscribeBeforeWriteCallback(
-    UA_Server *server,
-    const UA_NodeId *dataSetReaderId,
-    const UA_NodeId *readerGroupId,
-    const UA_NodeId *targetVariableId,
-    void *targetVariableContext,  /* custom target variable context (e.g. shared mem handle, offset) */
-    UA_DataValue **externalDataValue) { /* received value */
-
+subscribeBeforeWriteCallback(UA_Server *server, const UA_NodeId *dataSetReaderId,
+                             const UA_NodeId *readerGroupId, const UA_NodeId *targetVariableId,
+                             void *targetVariableContext, UA_DataValue **externalDataValue) {
     (void) server;
     (void) dataSetReaderId;
     (void) readerGroupId;
+    (void) targetVariableContext;
 
     assert(targetVariableId != 0);
-    assert(targetVariableContext != 0);
     assert(externalDataValue != 0);
-    void *data = (**externalDataValue).value.data;
-    UA_UInt32 dataSize = (**externalDataValue).value.type->memSize;
-    assert(data != 0);
-    assert(dataSize == sizeof(UA_UInt32)); /* in this simple example all dataset fields are of type UInt32 */
 
     UA_String strId;
     UA_String_init(&strId);
     UA_NodeId_print(targetVariableId, &strId);
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "subscribeBeforeWriteCallback(): "
-        "WriteUpdate() for node Id = '%.*s'. New Value Data = %u", (UA_Int32) strId.length, strId.data,
-        *(UA_UInt32*) data);
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                "subscribeBeforeWriteCallback(): "
+                "WriteUpdate() for node Id = '%.*s'",
+                (UA_Int32) strId.length, strId.data);
     UA_String_clear(&strId);
-
-    /* We can prepare custom data sink before data is copied
-        meta information is available at targetVariableContext
-        data sink could be a shared memory or any other custom implementation which represents variables of the engineering system
-        in this example we simulate a custom data sink */
-    UA_UInt32 offset = *((UA_UInt32*) targetVariableContext);
-    assert(offset < PUBSUB_CONFIG_FIELD_COUNT);
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "subscribeBeforeWriteCallback(): "
-        "offset = %u", offset);
-    memcpy(sSubscribedDataSink + offset, data, dataSize);
 }
 
 /* Define MetaData for TargetVariables */
-static void fillTestDataSetMetaData(UA_DataSetMetaDataType *pMetaData) {
+static void
+fillTestDataSetMetaData(UA_DataSetMetaDataType *pMetaData) {
     if(pMetaData == NULL)
         return;
 
@@ -187,7 +167,7 @@ addReaderGroup(UA_Server *server) {
     UA_ReaderGroupConfig readerGroupConfig;
     memset (&readerGroupConfig, 0, sizeof(UA_ReaderGroupConfig));
     readerGroupConfig.name = UA_STRING("ReaderGroup1");
-    readerGroupConfig.rtLevel = UA_PUBSUB_RT_FIXED_SIZE;
+    readerGroupConfig.rtLevel = UA_PUBSUB_RT_DETERMINISTIC;
     retval |= UA_Server_addReaderGroup(server, connectionIdentifier, &readerGroupConfig,
                                        &readerGroupIdentifier);
     return retval;
@@ -211,8 +191,7 @@ addSubscribedVariables (UA_Server *server) {
         oAttr.displayName.text = folderName;
         folderBrowseName.namespaceIndex = 1;
         folderBrowseName.name = folderName;
-    }
-    else {
+    } else {
         oAttr.displayName = UA_LOCALIZEDTEXT ("en-US", "Subscribed Variables");
         folderBrowseName = UA_QUALIFIEDNAME (1, "Subscribed Variables");
     }
@@ -226,32 +205,35 @@ addSubscribedVariables (UA_Server *server) {
     /* Set the subscribed data to TargetVariable type */
     readerConfig.subscribedDataSetType = UA_PUBSUB_SDS_TARGET;
     /* Create the TargetVariables with respect to DataSetMetaData fields */
-    readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariablesSize = readerConfig.dataSetMetaData.fieldsSize;
-    readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables = (UA_FieldTargetVariable *)
-            UA_calloc(readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariablesSize, sizeof(UA_FieldTargetVariable));
+    readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariablesSize =
+        readerConfig.dataSetMetaData.fieldsSize;
+    readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables =
+        (UA_FieldTargetVariable *)UA_calloc(
+            readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariablesSize,
+            sizeof(UA_FieldTargetVariable));
     for(size_t i = 0; i < readerConfig.dataSetMetaData.fieldsSize; i++) {
         /* Variable to subscribe data */
         UA_VariableAttributes vAttr = UA_VariableAttributes_default;
         vAttr.description = UA_LOCALIZEDTEXT ("en-US", "Subscribed UInt32");
         vAttr.displayName = UA_LOCALIZEDTEXT ("en-US", "Subscribed UInt32");
         vAttr.dataType    = UA_TYPES[UA_TYPES_UINT32].typeId;
-        // Initialize the values at first to create the buffered NetworkMessage with correct size and offsets
+        // Initialize the values at first to create the buffered NetworkMessage
+        // with correct size and offsets
         UA_Variant value;
         UA_Variant_init(&value);
         UA_UInt32 intValue = 0;
         UA_Variant_setScalar(&value, &intValue, &UA_TYPES[UA_TYPES_UINT32]);
         vAttr.value = value;
         retval |= UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, (UA_UInt32)i + 50000),
-                                           folderId,
-                                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
+                                           folderId, UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
                                            UA_QUALIFIEDNAME(1, "Subscribed UInt32"),
                                            UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE),
                                            vAttr, NULL, &newnodeId);
-        repeatedFieldValues[i] = UA_UInt32_new();
-        *repeatedFieldValues[i] = 0;
+        repeatedFieldValues[i] = 0;
         repeatedDataValueRT[i] = UA_DataValue_new();
-        UA_Variant_setScalar(&repeatedDataValueRT[i]->value, repeatedFieldValues[i],
+        UA_Variant_setScalar(&repeatedDataValueRT[i]->value, &repeatedFieldValues[i],
                              &UA_TYPES[UA_TYPES_UINT32]);
+        repeatedDataValueRT[i]->value.storageType = UA_VARIANT_DATA_NODELETE;
         repeatedDataValueRT[i]->hasValue = true;
 
         /* Set the value backend of the above create node to 'external value source' */
@@ -262,17 +244,18 @@ addSubscribedVariables (UA_Server *server) {
         valueBackend.backend.external.callback.notificationRead = externalDataReadNotificationCallback;
         UA_Server_setVariableNode_valueBackend(server, newnodeId, valueBackend);
 
+        UA_FieldTargetVariable *tv =
+            &readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables[i];
+        UA_FieldTargetDataType *ftdt = &tv->targetVariable;
+
         /* For creating Targetvariables */
-        UA_FieldTargetDataType_init(&readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables[i].targetVariable);
-        readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables[i].targetVariable.attributeId  = UA_ATTRIBUTEID_VALUE;
-        readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables[i].targetVariable.targetNodeId = newnodeId;
+        UA_FieldTargetDataType_init(ftdt);
+        ftdt->attributeId  = UA_ATTRIBUTEID_VALUE;
+        ftdt->targetNodeId = newnodeId;
         /* set both before and after write callback to show the usage */
-        /* configure before write callback */
-        readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables[i].beforeWrite = subscribeBeforeWriteCallback;
-        sSubscribedTargetVarDataOffset[i] = (UA_UInt32) i;
-        readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables[i].targetVariableContext = &sSubscribedTargetVarDataOffset[i];
-        /* configure after write callback */
-        readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables[i].afterWrite = subscribeAfterWriteCallback;
+        tv->beforeWrite = subscribeBeforeWriteCallback;
+        tv->externalDataValue = &repeatedDataValueRT[i];
+        tv->afterWrite = subscribeAfterWriteCallback;
     }
 
     return retval;
@@ -284,8 +267,7 @@ addDataSetReader(UA_Server *server) {
     if(server == NULL)
         return UA_STATUSCODE_BADINTERNALERROR;
 
-    UA_StatusCode retval = UA_STATUSCODE_GOOD;
-    memset (&readerConfig, 0, sizeof(UA_DataSetReaderConfig));
+    memset(&readerConfig, 0, sizeof(UA_DataSetReaderConfig));
     readerConfig.name = UA_STRING("DataSet Reader 1");
     /* Parameters to filter which DataSetMessage has to be processed
      * by the DataSetReader */
@@ -301,43 +283,38 @@ addDataSetReader(UA_Server *server) {
     readerConfig.expectedEncoding = UA_PUBSUB_RT_RAW;
     readerConfig.messageSettings.content.decoded.type = &UA_TYPES[UA_TYPES_UADPDATASETREADERMESSAGEDATATYPE];
     UA_UadpDataSetReaderMessageDataType *dataSetReaderMessage = UA_UadpDataSetReaderMessageDataType_new();
-    dataSetReaderMessage->networkMessageContentMask           = (UA_UadpNetworkMessageContentMask)(UA_UADPNETWORKMESSAGECONTENTMASK_PUBLISHERID |
-                                                                 (UA_UadpNetworkMessageContentMask)UA_UADPNETWORKMESSAGECONTENTMASK_GROUPHEADER |
-                                                                 (UA_UadpNetworkMessageContentMask) UA_UADPNETWORKMESSAGECONTENTMASK_SEQUENCENUMBER |
-                                                                 (UA_UadpNetworkMessageContentMask)UA_UADPNETWORKMESSAGECONTENTMASK_WRITERGROUPID |
-                                                                 (UA_UadpNetworkMessageContentMask)UA_UADPNETWORKMESSAGECONTENTMASK_PAYLOADHEADER);
-    dataSetReaderMessage->dataSetMessageContentMask           = (UA_UadpDataSetMessageContentMask)(UA_UADPDATASETMESSAGECONTENTMASK_SEQUENCENUMBER);
+    dataSetReaderMessage->networkMessageContentMask = (UA_UadpNetworkMessageContentMask)
+        (UA_UADPNETWORKMESSAGECONTENTMASK_PUBLISHERID | UA_UADPNETWORKMESSAGECONTENTMASK_GROUPHEADER |
+         UA_UADPNETWORKMESSAGECONTENTMASK_SEQUENCENUMBER | UA_UADPNETWORKMESSAGECONTENTMASK_WRITERGROUPID |
+         UA_UADPNETWORKMESSAGECONTENTMASK_PAYLOADHEADER);
+    dataSetReaderMessage->dataSetMessageContentMask = UA_UADPDATASETMESSAGECONTENTMASK_SEQUENCENUMBER;
     readerConfig.messageSettings.content.decoded.data = dataSetReaderMessage;
 
     readerConfig.dataSetFieldContentMask = UA_DATASETFIELDCONTENTMASK_RAWDATA;
 
     /* Setting up Meta data configuration in DataSetReader */
     fillTestDataSetMetaData(&readerConfig.dataSetMetaData);
+
+    UA_StatusCode retval = UA_STATUSCODE_GOOD;
     retval |= addSubscribedVariables(server);
+    retval |= UA_Server_addDataSetReader(server, readerGroupIdentifier,
+                                         &readerConfig, &readerIdentifier);
 
-    retval |= UA_Server_addDataSetReader(server, readerGroupIdentifier, &readerConfig,
-                                         &readerIdentifier);
-
-    for(size_t i = 0; i < readerConfig.dataSetMetaData.fieldsSize; i++)
-        UA_FieldTargetDataType_clear(&readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables[i].targetVariable);
+    for(size_t i = 0; i < readerConfig.dataSetMetaData.fieldsSize; i++) {
+        UA_FieldTargetVariable *tv =
+            &readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables[i];
+        UA_FieldTargetDataType *ftdt = &tv->targetVariable;
+        UA_FieldTargetDataType_clear(ftdt);
+    }
 
     UA_free(readerConfig.subscribedDataSet.subscribedDataSetTarget.targetVariables);
     UA_free(readerConfig.dataSetMetaData.fields);
-
     UA_UadpDataSetReaderMessageDataType_delete(dataSetReaderMessage);
     return retval;
 }
 
-UA_Boolean running = true;
-static void stopHandler(int sign) {
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "received ctrl-c");
-    running = false;
-}
-
 static int
 run(UA_String *transportProfile, UA_NetworkAddressUrlDataType *networkAddressUrl) {
-    signal(SIGINT, stopHandler);
-    signal(SIGTERM, stopHandler);
     /* Return value initialized to Status Good */
     UA_StatusCode retval = UA_STATUSCODE_GOOD;
     UA_Server *server = UA_Server_new();
@@ -364,23 +341,16 @@ run(UA_String *transportProfile, UA_NetworkAddressUrlDataType *networkAddressUrl
     UA_Server_freezeReaderGroupConfiguration(server, readerGroupIdentifier);
     UA_Server_setReaderGroupOperational(server, readerGroupIdentifier);
 
-    retval = UA_Server_run(server, &running);
+    retval = UA_Server_runUntilInterrupt(server);
 
     UA_Server_unfreezeReaderGroupConfiguration(server, readerGroupIdentifier);
     UA_Server_delete(server);
 
-    for (UA_Int32 iterator = 0; iterator <  PUBSUB_CONFIG_FIELD_COUNT; iterator++)
-    {
-        UA_free(repeatedFieldValues[iterator]);
-        UA_free(repeatedDataValueRT[iterator]);
+    for(UA_Int32 i = 0; i < PUBSUB_CONFIG_FIELD_COUNT; i++) {
+        UA_DataValue_delete(repeatedDataValueRT[i]);
     }
 
     return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
-}
-
-static void
-usage(char *progname) {
-    printf("usage: %s <uri> [device]\n", progname);
 }
 
 int main(int argc, char **argv) {
@@ -388,7 +358,7 @@ int main(int argc, char **argv) {
     UA_NetworkAddressUrlDataType networkAddressUrl = {UA_STRING_NULL , UA_STRING("opc.udp://224.0.0.22:4840/")};
     if(argc > 1) {
         if(strcmp(argv[1], "-h") == 0) {
-            usage(argv[0]);
+            printf("usage: %s <uri> [device]\n", argv[0]);
             return EXIT_SUCCESS;
         } else if(strncmp(argv[1], "opc.udp://", 10) == 0) {
             networkAddressUrl.url = UA_STRING(argv[1]);
@@ -399,7 +369,6 @@ int main(int argc, char **argv) {
                 printf("Error: UADP/ETH needs an interface name\n");
                 return EXIT_FAILURE;
             }
-
             networkAddressUrl.networkInterface = UA_STRING(argv[2]);
             networkAddressUrl.url = UA_STRING(argv[1]);
         } else {
@@ -410,4 +379,3 @@ int main(int argc, char **argv) {
 
     return run(&transportProfile, &networkAddressUrl);
 }
-
