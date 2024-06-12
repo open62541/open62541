7a6cc8ad991ec81d597bbb3a5bd5c2dcbc73ffe3
Julius Pfrommer <julius.pfrommer@web.de>
2024-06-12 04:00:06 +0200

    refactor(examples): Clean up server_pubsub_rt_field_information_model.c

--- examples/pubsub_realtime/server_pubsub_rt_field_information_model.c
+++ examples/pubsub_realtime/server_pubsub_rt_field_information_model.c
@@ -7,49 +7,19 @@
 #include <open62541/plugin/log_stdout.h>
 #include <open62541/server_config_default.h>
 
-#include <signal.h>
 #include <stdlib.h>
 
-UA_Boolean running = true;
 UA_NodeId publishedDataSetIdent, dataSetFieldIdent, writerGroupIdent, connectionIdentifier;
 UA_UInt32 *integerRTValue, *integerRTValue2;
 UA_NodeId rtNodeId1, rtNodeId2;
 
-static void stopHandler(int sign) {
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "received ctrl-c");
-    running = false;
-}
-
-// Info: It is still possible to create a RT-PubSub configuration without an information model
-// node. Just set the DSF flags to 'rtInformationModelNode' -> UA_TRUE and
-// 'rtInformationModelNode' -> UA_FALSE and provide the PTR to your self managed value source.
-
-/* The following PubSub configuration does not differ from the 'normal' configuration */
-static void
-addMinimalPubSubConfiguration(UA_Server * server){
-    /* Add one PubSubConnection */
-    UA_PubSubConnectionConfig connectionConfig;
-    memset(&connectionConfig, 0, sizeof(connectionConfig));
-    connectionConfig.name = UA_STRING("UDP-UADP Connection 1");
-    connectionConfig.transportProfileUri = UA_STRING("http://opcfoundation.org/UA-Profile/Transport/pubsub-udp-uadp");
-    connectionConfig.enabled = UA_TRUE;
-    UA_NetworkAddressUrlDataType networkAddressUrl = {UA_STRING_NULL , UA_STRING("opc.udp://224.0.0.22:4840/")};
-    UA_Variant_setScalar(&connectionConfig.address, &networkAddressUrl, &UA_TYPES[UA_TYPES_NETWORKADDRESSURLDATATYPE]);
-    connectionConfig.publisherIdType = UA_PUBLISHERIDTYPE_UINT16;
-    connectionConfig.publisherId.uint16 = 2234;
-    UA_Server_addPubSubConnection(server, &connectionConfig, &connectionIdentifier);
-    /* Add one PublishedDataSet */
-    UA_PublishedDataSetConfig publishedDataSetConfig;
-    memset(&publishedDataSetConfig, 0, sizeof(UA_PublishedDataSetConfig));
-    publishedDataSetConfig.publishedDataSetType = UA_PUBSUB_DATASET_PUBLISHEDITEMS;
-    publishedDataSetConfig.name = UA_STRING("Demo PDS");
-    /* Add one DataSetField to the PDS */
-    UA_Server_addPublishedDataSet(server, &publishedDataSetConfig, &publishedDataSetIdent);
-}
+/* Info: It is still possible to create a RT-PubSub configuration without an
+ * information model node. Just set the DSF flags to 'rtInformationModelNode' ->
+ * true and 'rtInformationModelNode' -> false and provide the PTR to your self
+ * managed value source. */
 
 static UA_NodeId
 addVariable(UA_Server *server, char *name) {
-    UA_NodeId outNodeId;
     /* Define the attribute of the myInteger variable node */
     UA_VariableAttributes attr = UA_VariableAttributes_default;
     UA_UInt32 myInteger = 42;
@@ -60,12 +30,13 @@ addVariable(UA_Server *server, char *name) {
     attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
 
     /* Add the variable node to the information model */
+    UA_NodeId outNodeId;
     UA_QualifiedName myIntegerName = UA_QUALIFIEDNAME(1, name);
     UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
     UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
-    UA_Server_addVariableNode(server, UA_NODEID_NULL, parentNodeId,
-                              parentReferenceNodeId, myIntegerName,
-                              UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), attr, NULL, &outNodeId);
+    UA_Server_addVariableNode(server, UA_NODEID_NULL, parentNodeId, parentReferenceNodeId,
+                              myIntegerName, UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE),
+                              attr, NULL, &outNodeId);
     return outNodeId;
 }
 
@@ -74,26 +45,16 @@ addVariable(UA_Server *server, char *name) {
  * that the write leads not to synchronization issues and race conditions. */
 static UA_StatusCode
 externalDataWriteCallback(UA_Server *server, const UA_NodeId *sessionId,
-                                  void *sessionContext, const UA_NodeId *nodeId,
-                                  void *nodeContext, const UA_NumericRange *range,
-                                  const UA_DataValue *data){
-    //It's possible to create a new DataValue here and use an atomic ptr switch
-    //to update the value without the need for locks e.g. UA_atomic_cmpxchg();
-
-    //!!The user must take about synchronization.!!
+                          void *sessionContext, const UA_NodeId *nodeId,
+                          void *nodeContext, const UA_NumericRange *range,
+                          const UA_DataValue *data) {
+    /* It's possible to create a new DataValue here and use an atomic ptr switch
+     * to update the value without the need for locks e.g. UA_atomic_cmpxchg(); */
     if(UA_NodeId_equal(nodeId, &rtNodeId1)){
         memcpy(integerRTValue, data->value.data, sizeof(UA_UInt32));
     } else if(UA_NodeId_equal(nodeId, &rtNodeId2)){
         memcpy(integerRTValue2, data->value.data, sizeof(UA_UInt32));
     }
-    return UA_STATUSCODE_GOOD;
-}
-
-static UA_StatusCode
-externalDataReadNotificationCallback(UA_Server *server, const UA_NodeId *sessionId,
-                                     void *sessionContext, const UA_NodeId *nodeid,
-                                     void *nodeContext, const UA_NumericRange *range){
-    //allow read without any preparation
     return UA_STATUSCODE_GOOD;
 }
 
@@ -117,16 +78,33 @@ cyclicValueUpdateCallback_UpdateToStack(UA_Server *server, void *data) {
 }
 
 int main(void){
-    signal(SIGINT, stopHandler);
-    signal(SIGTERM, stopHandler);
-
     UA_Server *server = UA_Server_new();
-    UA_ServerConfig *config = UA_Server_getConfig(server);
-    UA_ServerConfig_setDefault(config);
 
-    addMinimalPubSubConfiguration(server);
+    /* Add one PubSubConnection */
+    UA_PubSubConnectionConfig connectionConfig;
+    memset(&connectionConfig, 0, sizeof(connectionConfig));
+    connectionConfig.name = UA_STRING("UDP-UADP Connection 1");
+    connectionConfig.transportProfileUri =
+        UA_STRING("http://opcfoundation.org/UA-Profile/Transport/pubsub-udp-uadp");
+    connectionConfig.enabled = UA_TRUE;
+    UA_NetworkAddressUrlDataType networkAddressUrl =
+        {UA_STRING_NULL , UA_STRING("opc.udp://224.0.0.22:4840/")};
+    UA_Variant_setScalar(&connectionConfig.address, &networkAddressUrl,
+                         &UA_TYPES[UA_TYPES_NETWORKADDRESSURLDATATYPE]);
+    connectionConfig.publisherIdType = UA_PUBLISHERIDTYPE_UINT16;
+    connectionConfig.publisherId.uint16 = 2234;
+    UA_Server_addPubSubConnection(server, &connectionConfig, &connectionIdentifier);
 
-    /*add rt configuration*/
+    /* Add one PublishedDataSet */
+    UA_PublishedDataSetConfig publishedDataSetConfig;
+    memset(&publishedDataSetConfig, 0, sizeof(UA_PublishedDataSetConfig));
+    publishedDataSetConfig.publishedDataSetType = UA_PUBSUB_DATASET_PUBLISHEDITEMS;
+    publishedDataSetConfig.name = UA_STRING("Demo PDS");
+
+    /* Add one DataSetField to the PDS */
+    UA_Server_addPublishedDataSet(server, &publishedDataSetConfig, &publishedDataSetIdent);
+
+    /* Add RT configuration */
     UA_WriterGroupConfig writerGroupConfig;
     memset(&writerGroupConfig, 0, sizeof(UA_WriterGroupConfig));
     writerGroupConfig.name = UA_STRING("Demo WriterGroup");
@@ -134,14 +112,13 @@ int main(void){
     writerGroupConfig.enabled = UA_FALSE;
     writerGroupConfig.writerGroupId = 100;
     writerGroupConfig.encodingMimeType = UA_PUBSUB_ENCODING_UADP;
-    writerGroupConfig.messageSettings.encoding             = UA_EXTENSIONOBJECT_DECODED;
-    writerGroupConfig.messageSettings.content.decoded.type = &UA_TYPES[UA_TYPES_UADPWRITERGROUPMESSAGEDATATYPE];
     UA_UadpWriterGroupMessageDataType writerGroupMessage;
     UA_UadpWriterGroupMessageDataType_init(&writerGroupMessage);
-    writerGroupMessage.networkMessageContentMask = (UA_UadpNetworkMessageContentMask) ((UA_UadpNetworkMessageContentMask) UA_UADPNETWORKMESSAGECONTENTMASK_PUBLISHERID |
-                                                                                       (UA_UadpNetworkMessageContentMask) UA_UADPNETWORKMESSAGECONTENTMASK_GROUPHEADER |
-                                                                                       (UA_UadpNetworkMessageContentMask) UA_UADPNETWORKMESSAGECONTENTMASK_WRITERGROUPID |(UA_UadpNetworkMessageContentMask) UA_UADPNETWORKMESSAGECONTENTMASK_PAYLOADHEADER);
-    writerGroupConfig.messageSettings.content.decoded.data = &writerGroupMessage;
+    writerGroupMessage.networkMessageContentMask = (UA_UadpNetworkMessageContentMask)
+        (UA_UADPNETWORKMESSAGECONTENTMASK_PUBLISHERID | UA_UADPNETWORKMESSAGECONTENTMASK_GROUPHEADER |
+         UA_UADPNETWORKMESSAGECONTENTMASK_WRITERGROUPID | UA_UADPNETWORKMESSAGECONTENTMASK_PAYLOADHEADER);
+    UA_ExtensionObject_setValue(&writerGroupConfig.messageSettings, &writerGroupMessage,
+                                &UA_TYPES[UA_TYPES_UADPWRITERGROUPMESSAGEDATATYPE]);
     writerGroupConfig.rtLevel = UA_PUBSUB_RT_FIXED_SIZE;
     UA_Server_addWriterGroup(server, connectionIdentifier, &writerGroupConfig, &writerGroupIdent);
 
@@ -154,49 +131,46 @@ int main(void){
     dataSetWriterConfig.keyFrameCount = 10;
     /* Encode fields as RAW-Encoded */
     dataSetWriterConfig.dataSetFieldContentMask = UA_DATASETFIELDCONTENTMASK_RAWDATA;
-    UA_Server_addDataSetWriter(server, writerGroupIdent, publishedDataSetIdent, &dataSetWriterConfig, &dataSetWriterIdent);
+    UA_Server_addDataSetWriter(server, writerGroupIdent, publishedDataSetIdent,
+                               &dataSetWriterConfig, &dataSetWriterIdent);
 
-    /* add new node to the information model with external data source backend*/
+    /* Add new nodes */
     rtNodeId1 = addVariable(server, "RT value source 1");
-    /* create external value source for the node */
+    rtNodeId2 = addVariable(server, "RT value source 2");
+
+    /* Set the value backend to 'external value source' */
     integerRTValue = UA_UInt32_new();
     UA_DataValue *dataValueRT = UA_DataValue_new();
     dataValueRT->hasValue = UA_TRUE;
     UA_Variant_setScalar(&dataValueRT->value, integerRTValue, &UA_TYPES[UA_TYPES_UINT32]);
-    /* Set the value backend of the above create node to 'external value source' */
     UA_ValueBackend valueBackend;
     valueBackend.backendType = UA_VALUEBACKENDTYPE_EXTERNAL;
     valueBackend.backend.external.value = &dataValueRT;
     valueBackend.backend.external.callback.userWrite = externalDataWriteCallback;
-    valueBackend.backend.external.callback.notificationRead = externalDataReadNotificationCallback;
     UA_Server_setVariableNode_valueBackend(server, rtNodeId1, valueBackend);
 
-    /* setup RT DataSetField config */
+    /* Setup RT DataSetField config */
+    UA_NodeId dsfNodeId;
     UA_DataSetFieldConfig dsfConfig;
     memset(&dsfConfig, 0, sizeof(UA_DataSetFieldConfig));
     dsfConfig.field.variable.rtValueSource.rtInformationModelNode = UA_TRUE;
     dsfConfig.field.variable.publishParameters.publishedVariable = rtNodeId1;
     dsfConfig.field.variable.fieldNameAlias = UA_STRING("Field 1");
-    UA_NodeId dsfNodeId;
     UA_Server_addDataSetField(server, publishedDataSetIdent, &dsfConfig, &dsfNodeId);
 
-    /* add second new node to the information model with external data source backend*/
-    rtNodeId2 = addVariable(server, "RT value source 2");
-    /* create external value source for the node */
+    /* Set the value backend of the above create node to 'external value source' */
     integerRTValue2 = UA_UInt32_new();
     *integerRTValue2 = 1000;
     UA_DataValue *dataValue2RT = UA_DataValue_new();
-    dataValue2RT->hasValue = UA_TRUE;
+    dataValue2RT->hasValue = true;
     UA_Variant_setScalar(&dataValue2RT->value, integerRTValue2, &UA_TYPES[UA_TYPES_UINT32]);
-    /* Set the value backend of the above create node to 'external value source' */
     UA_ValueBackend valueBackend2;
     valueBackend2.backendType = UA_VALUEBACKENDTYPE_EXTERNAL;
     valueBackend2.backend.external.value = &dataValue2RT;
     valueBackend2.backend.external.callback.userWrite = externalDataWriteCallback;
-    valueBackend2.backend.external.callback.notificationRead = externalDataReadNotificationCallback;
     UA_Server_setVariableNode_valueBackend(server, rtNodeId2, valueBackend2);
 
-    /* setup second DataSetField config */
+    /* Setup second DataSetField config */
     UA_DataSetFieldConfig dsfConfig2;
     memset(&dsfConfig2, 0, sizeof(UA_DataSetFieldConfig));
     dsfConfig2.field.variable.rtValueSource.rtInformationModelNode = UA_TRUE;
@@ -208,18 +182,16 @@ int main(void){
     UA_Server_freezeWriterGroupConfiguration(server, writerGroupIdent);
     UA_Server_setWriterGroupOperational(server, writerGroupIdent);
 
-    UA_Server_addRepeatedCallback(server, cyclicValueUpdateCallback_UpdateToMemory, NULL,
-                                  1000, NULL);
+    UA_Server_addRepeatedCallback(server, cyclicValueUpdateCallback_UpdateToMemory,
+                                  NULL, 1000, NULL);
 
-    UA_Server_addRepeatedCallback(server, cyclicValueUpdateCallback_UpdateToStack, NULL,
-                                  5000, NULL);
+    UA_Server_addRepeatedCallback(server, cyclicValueUpdateCallback_UpdateToStack,
+                                  NULL, 5000, NULL);
 
-    UA_StatusCode retval = UA_Server_run(server, &running);
+    UA_StatusCode retval = UA_Server_runUntilInterrupt(server);
+
     UA_Server_delete(server);
-
-    /* Free external data source */
     UA_DataValue_delete(dataValueRT);
     UA_DataValue_delete(dataValue2RT);
-
     return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
 }
