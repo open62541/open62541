913ea88d1a60750e2601851014dba980f217d181
Julius Pfrommer <julius.pfrommer@web.de>
2024-06-15 11:36:07 +0200

    refactor(pubsub): Use UA_PUBSUB_RT_DETERMINISTIC as the ORed combination of the other options

--- include/open62541/server_pubsub.h
+++ include/open62541/server_pubsub.h
@@ -488,7 +488,7 @@ typedef enum {
     UA_PUBSUB_RT_NONE = 0,
     UA_PUBSUB_RT_DIRECT_VALUE_ACCESS = 1,
     UA_PUBSUB_RT_FIXED_SIZE = 2,
-    UA_PUBSUB_RT_DETERMINISTIC = 4,
+    UA_PUBSUB_RT_DETERMINISTIC = 3,
 } UA_PubSubRTLevel;
 
 typedef struct {
