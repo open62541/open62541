bc3a05b0f0a457308b956cf6232c9c1aab66b2e2
estr Eckerstorfer Andreas <estr@keba.com>
2024-09-13 08:35:24 +0200

    fix(pubsub): Set externalDataValue in subscribed data set if not set by user
    With commit 3a59e733de9d007bc1944c5f78dd5686070577d2 the setting of the
    externalDataValue was commented in the function
    UA_ReaderGroup_freezeConfiguration. If the externalDataValue is not set
    by the user, the performance of the update decreases drastically.
    
    Thus the externalDataValue is now set in the function
    UA_ReaderGroup_freezeConfiguration, if it has not been set by the user
    and if the user provided the backend type UA_VALUEBACKENDTYPE_EXTERNAL
    in the variable node configuration.

--- src/pubsub/ua_pubsub_readergroup.c
+++ src/pubsub/ua_pubsub_readergroup.c
@@ -628,24 +628,21 @@ UA_ReaderGroup_freezeConfiguration(UA_Server *server, UA_ReaderGroup *rg) {
 
     size_t fieldsSize = dsr->config.dataSetMetaData.fieldsSize;
     for(size_t i = 0; i < fieldsSize; i++) {
-        /* TODO: Use the datasource from the node */
-        /* UA_FieldTargetVariable *tv = */
-        /*     &dsr->config.subscribedDataSet.subscribedDataSetTarget.targetVariables[i]; */
-        /* const UA_VariableNode *rtNode = (const UA_VariableNode *) */
-        /*     UA_NODESTORE_GET(server, &tv->targetVariable.targetNodeId); */
-        /* if(!rtNode || */
-        /*    rtNode->valueBackend.backendType != UA_VALUEBACKENDTYPE_EXTERNAL) { */
-        /*     UA_LOG_WARNING_READER(server->config.logging, dsr, */
-        /*                           "PubSub-RT configuration fail: PDS contains field " */
-        /*                           "without external data source."); */
-        /*     UA_NODESTORE_RELEASE(server, (const UA_Node *) rtNode); */
-        /*     return UA_STATUSCODE_BADNOTSUPPORTED; */
-        /* } */
+        /* Use the datasource from the node, if available and no externalDataValue
+           has been set in the configuration */
+        UA_FieldTargetVariable *tv =
+            &dsr->config.subscribedDataSet.subscribedDataSetTarget.targetVariables[i];
+        if(tv->externalDataValue == NULL) {
+            const UA_VariableNode *rtNode = (const UA_VariableNode *)
+                UA_NODESTORE_GET(server, &tv->targetVariable.targetNodeId);
+            if(rtNode &&
+               rtNode->valueBackend.backendType == UA_VALUEBACKENDTYPE_EXTERNAL) {
+                /* Set the external data source in the tv */
+                tv->externalDataValue = rtNode->valueBackend.backend.external.value;
+            }
 
-        /* /\* Set the external data source in the tv *\/ */
-        /* tv->externalDataValue = rtNode->valueBackend.backend.external.value; */
-
-        /* UA_NODESTORE_RELEASE(server, (const UA_Node *) rtNode); */
+            UA_NODESTORE_RELEASE(server, (const UA_Node *) rtNode);
+        }
 
         UA_FieldMetaData *field = &dsr->config.dataSetMetaData.fields[i];
         if((UA_NodeId_equal(&field->dataType, &UA_TYPES[UA_TYPES_STRING].typeId) ||
