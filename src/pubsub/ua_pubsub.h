41647d019f1c5ebefce142e9230778cfcdff71a7
Julius Pfrommer <julius.pfrommer@web.de>
2024-09-29 23:35:45 +0200

    feat(pubsub): Silence errors on PubSub connections with a timeout to avoid logging floods

--- src/pubsub/ua_pubsub.h
+++ src/pubsub/ua_pubsub.h
@@ -162,6 +162,8 @@ typedef struct UA_PubSubConnection {
 
     UA_UInt16 configurationFreezeCounter;
 
+    UA_DateTime silenceErrorUntil; /* Avoid generating too many logs */
+
     UA_Boolean deleteFlag; /* To be deleted - in addition to the PubSubState */
     UA_DelayedCallback dc; /* For delayed freeing */
 } UA_PubSubConnection;
