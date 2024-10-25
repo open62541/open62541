c16bed5573f0865f303967955cc965d1046d65a2
Julius Pfrommer <julius.pfrommer@web.de>
2024-10-25 15:43:18 +0200

    refactor(arch): Split the clock implementation into architectures

--- arch/posix/clock_posix.c
+++ arch/posix/clock_posix.c
@@ -4,51 +4,16 @@
  *    Copyright 2016-2017 (c) Fraunhofer IOSB (Author: Julius Pfrommer)
  *    Copyright 2017 (c) Stefan Profanter, fortiss GmbH
  *    Copyright 2017 (c) Thomas Stalder, Blue Time Concept SA
- *    Copyright 2024 (c) Julian Weiß, PRIMES GmbH
  */
 
 #include <open62541/types.h>
 
-/* This file contains archicture-specific access to the system clock for
- * UA_DateTime_now. etc. Just add additional architecture below.
- *
- * Note that the EventLoop plugin provides its own internal time source (which
+/* Note that the EventLoop plugin provides its own internal time source (which
  * is typically just the normal system time). All internal access to the time
  * source should be through the EventLoop. The below is therefore for developer
- * convenience to just use UA_DateTime_now. */
+ * convenience to just use UA_DateTime_now(). */
 
-#if defined(UA_ARCHITECTURE_ZEPHYR)
-#include <time.h>
-
-#include <sys/time.h>
-#include <zephyr/kernel.h>
-
-UA_DateTime
-UA_DateTime_now(void) {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (tv.tv_sec * UA_DATETIME_SEC) + (tv.tv_usec * UA_DATETIME_USEC) +
-           UA_DATETIME_UNIX_EPOCH;
-}
-
-/* Credit to
- * https://stackoverflow.com/questions/13804095/get-the-time-zone-gmt-offset-in-c */
-UA_Int64
-UA_DateTime_localTimeUtcOffset(void) {
-    time_t rawtime = time(NULL);
-    struct tm *ptm = gmtime(&rawtime);
-    /* Request mktime() to look up dst in timezone database */
-    ptm->tm_isdst = -1;
-    time_t gmt = mktime(ptm);
-    return (UA_Int64)(difftime(rawtime, gmt) * UA_DATETIME_SEC);
-}
-
-UA_DateTime
-UA_DateTime_nowMonotonic(void) {
-    return k_uptime_get();
-}
-
-#elif defined(UA_ARCHITECTURE_POSIX)
+#if defined(UA_ARCHITECTURE_POSIX)
 
 #include <time.h>
 #include <sys/time.h>
