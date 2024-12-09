cc347ab7376b839b28a5dee581c4d0acef6c740c
Rolf Kalbermatter <15158041+RolfKal@users.noreply.github.com>
2024-12-09 11:23:19 +0100

    fis(pubsub): fix some copy past error after apparently changing an API to work on the server rather than the PubSubManager reference

--- src/pubsub/ua_pubsub_keystorage.c
+++ src/pubsub/ua_pubsub_keystorage.c
@@ -213,7 +213,7 @@ UA_PubSubKeyStorage_addKeyRolloverCallback(UA_Server *server,
         return UA_STATUSCODE_GOOD;
     }
     
-    UA_LOCK_ASSERT(&psm->sc.server->serviceMutex, 1);
+    UA_LOCK_ASSERT(&server->serviceMutex, 1);
 
     UA_DateTime dateTimeToNextKey = UA_DateTime_nowMonotonic() +
         (UA_DateTime)(UA_DATETIME_MSEC * timeToNextMs);
