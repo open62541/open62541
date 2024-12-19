dcda2cc09cab7fd38e523a3280a117ef1632aec6
Thomas Nittel <144451640+ThomasNittel@users.noreply.github.com>
2024-12-19 13:23:02 +0100

    Example client_subscription_loop.c: Async functions used in callbacks
    Async functions have to be used in callbacks else the error message "Cannot run EventLoop from the run method itself" occurs under Windows 10 with Microsoft Visual Studio 17 2022.

--- examples/client_subscription_loop.c
+++ examples/client_subscription_loop.c
@@ -51,6 +51,55 @@ subscriptionInactivityCallback (UA_Client *client, UA_UInt32 subId, void *subCon
 }
 
 static void
+monCallback(UA_Client *client, void *userdata,
+    UA_UInt32 requestId, void *r) {
+    UA_CreateMonitoredItemsResponse *monResponse = (UA_CreateMonitoredItemsResponse *)r;
+    if(0 < monResponse->resultsSize && monResponse->results[0].statusCode == UA_STATUSCODE_GOOD)
+    {
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                    "Monitoring UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME', id %u",
+                    monResponse->results[0].monitoredItemId);
+    }
+}
+
+static void
+createSubscriptionCallback(UA_Client *client, void *userdata, UA_UInt32 requestId, void *r)
+{
+    UA_CreateSubscriptionResponse *response = (UA_CreateSubscriptionResponse *)r;
+    if (response->subscriptionId == 0)
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+            "response->subscriptionId == 0, %u", response->subscriptionId);
+    else if (response->responseHeader.serviceResult != UA_STATUSCODE_GOOD)
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+            "Create subscription failed, serviceResult %u",
+            response->responseHeader.serviceResult);
+    else
+    {
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+            "Create subscription succeeded, id %u",
+            response->subscriptionId);
+
+        /* Add a MonitoredItem */
+        UA_NodeId currentTime =
+            UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME);
+        UA_CreateMonitoredItemsRequest req;
+        UA_CreateMonitoredItemsRequest_init(&req);
+        UA_MonitoredItemCreateRequest monRequest =
+            UA_MonitoredItemCreateRequest_default(currentTime);
+        req.itemsToCreate = &monRequest;
+        req.itemsToCreateSize = 1;
+        req.subscriptionId = response->subscriptionId;
+
+        UA_Client_DataChangeNotificationCallback dataChangeNotificationCallback[1] = { handler_currentTimeChanged };
+        UA_StatusCode res =
+            UA_Client_MonitoredItems_createDataChanges_async(client, req, NULL, dataChangeNotificationCallback, NULL, monCallback, NULL, NULL);
+        if (res != UA_STATUSCODE_GOOD)
+            UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                "UA_Client_MonitoredItems_createDataChanges_async ", UA_StatusCode_name(res));
+    }
+}
+
+static void
 stateCallback(UA_Client *client, UA_SecureChannelState channelState,
               UA_SessionState sessionState, UA_StatusCode recoveryStatus) {
     switch(channelState) {
@@ -76,28 +125,9 @@ stateCallback(UA_Client *client, UA_SecureChannelState channelState,
         /* A new session was created. We need to create the subscription. */
         /* Create a subscription */
         UA_CreateSubscriptionRequest request = UA_CreateSubscriptionRequest_default();
-        UA_CreateSubscriptionResponse response =
-            UA_Client_Subscriptions_create(client, request, NULL, NULL, deleteSubscriptionCallback);
-            if(response.responseHeader.serviceResult == UA_STATUSCODE_GOOD)
-                UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
-                            "Create subscription succeeded, id %u",
-                            response.subscriptionId);
-            else
-                return;
-
-            /* Add a MonitoredItem */
-            UA_NodeId currentTimeNode = UA_NS0ID(SERVER_SERVERSTATUS_CURRENTTIME);
-            UA_MonitoredItemCreateRequest monRequest =
-                UA_MonitoredItemCreateRequest_default(currentTimeNode);
-
-            UA_MonitoredItemCreateResult monResponse =
-                UA_Client_MonitoredItems_createDataChange(client, response.subscriptionId,
-                                                          UA_TIMESTAMPSTORETURN_BOTH, monRequest,
-                                                          NULL, handler_currentTimeChanged, NULL);
-            if(monResponse.statusCode == UA_STATUSCODE_GOOD)
-                UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
-                            "Monitoring UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME', id %u",
-                            monResponse.monitoredItemId);
+        UA_StatusCode StatusCode = 
+            UA_Client_Subscriptions_create_async(client, request, NULL, NULL, deleteSubscriptionCallback, 
+                                                 createSubscriptionCallback, NULL, NULL);
         }
         break;
     case UA_SESSIONSTATE_CLOSED:
