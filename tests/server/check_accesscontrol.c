02fbcc243cffe170b9dc5fe2e04982ee843b53a4
Julius Pfrommer <julius.pfrommer@web.de>
2025-05-27 20:30:59 +0200

    feat(tests): Add a test for UA_Server_getSessionParameter

--- tests/server/check_accesscontrol.c
+++ tests/server/check_accesscontrol.c
@@ -7,6 +7,7 @@
 #include <open62541/server.h>
 #include <open62541/server_config_default.h>
 #include <open62541/types.h>
+#include <open62541/plugin/accesscontrol_default.h>
 
 #include <check.h>
 
@@ -85,6 +86,64 @@ START_TEST(Client_pass_fail) {
     UA_Client_delete(client);
 } END_TEST
 
+static UA_Boolean
+allowBrowseNode(UA_Server *s, UA_AccessControl *ac,
+                const UA_NodeId *sessionId, void *sessionContext,
+                const UA_NodeId *nodeId, void *nodeContext) {
+    UA_Variant attribute;
+    UA_Variant_init(&attribute);
+    UA_Server_getSessionParameter(s, sessionId, "my_custom_attribute", &attribute);
+    return true;
+}
+
+static void setCustomAccessControl(UA_ServerConfig* config) {
+    UA_Boolean allowAnonymous = true;
+    UA_AccessControl_default(config, allowAnonymous, NULL, NULL, 0, NULL);
+    config->accessControl.allowBrowseNode = allowBrowseNode;
+}
+
+START_TEST(Server_sessionParameter) {
+    server = UA_Server_new();
+    UA_ServerConfig *config = UA_Server_getConfig(server);
+    UA_ServerConfig_setDefault(config);
+    setCustomAccessControl(config);
+    UA_Server_run_startup(server);
+
+    running = true;
+    THREAD_CREATE(server_thread, serverloop);
+
+    UA_Client *client = UA_Client_new();
+    UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+    UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://localhost:4840");
+
+    ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
+
+    UA_BrowseDescription bd;
+    UA_BrowseDescription_init(&bd);
+    bd.nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER);
+    bd.browseDirection = UA_BROWSEDIRECTION_BOTH;
+    bd.includeSubtypes = true;
+    bd.referenceTypeId = UA_NODEID_NUMERIC(0, UA_NS0ID_HIERARCHICALREFERENCES);
+
+    UA_BrowseRequest request;
+    UA_BrowseRequest_init(&request);
+    request.nodesToBrowse = &bd;
+    request.nodesToBrowseSize = 1;
+
+    UA_BrowseResponse br = UA_Client_Service_browse(client, request);
+    UA_assert(br.resultsSize == 1);
+    UA_assert(br.results[0].referencesSize > 0);
+    UA_assert(br.results[0].statusCode == UA_STATUSCODE_GOOD);
+    UA_BrowseResponse_clear(&br);
+
+    UA_Client_disconnect(client);
+    UA_Client_delete(client);
+
+    running = false;
+    THREAD_JOIN(server_thread);
+    UA_Server_run_shutdown(server);
+    UA_Server_delete(server);
+} END_TEST
 
 static Suite* testSuite_Client(void) {
     Suite *s = suite_create("Client");
@@ -95,6 +154,10 @@ static Suite* testSuite_Client(void) {
     tcase_add_test(tc_client_user, Client_user_fail);
     tcase_add_test(tc_client_user, Client_pass_fail);
     suite_add_tcase(s,tc_client_user);
+
+    TCase *tc_server = tcase_create("Server-Side Access Control");
+    tcase_add_test(tc_server, Server_sessionParameter);
+    suite_add_tcase(s,tc_server);
     return s;
 }
 
