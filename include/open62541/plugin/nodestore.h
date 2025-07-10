14b5c16c6e448ca87626bdfd379c0f831a9a5c75
Julius Pfrommer <julius.pfrommer@web.de>
2025-07-10 20:17:36 +0200

    refactor(server): Cosmetic line-break improvements in open62541/plugin/nodestore.h

--- include/open62541/plugin/nodestore.h
+++ include/open62541/plugin/nodestore.h
@@ -592,12 +592,12 @@ typedef struct {
  * ---------- */
 
 typedef UA_StatusCode
-(*UA_MethodCallback)(UA_Server *server, const UA_NodeId *sessionId,
-                     void *sessionContext, const UA_NodeId *methodId,
-                     void *methodContext, const UA_NodeId *objectId,
-                     void *objectContext, size_t inputSize,
-                     const UA_Variant *input, size_t outputSize,
-                     UA_Variant *output);
+(*UA_MethodCallback)(UA_Server *server,
+                     const UA_NodeId *sessionId, void *sessionContext,
+                     const UA_NodeId *methodId, void *methodContext,
+                     const UA_NodeId *objectId, void *objectContext,
+                     size_t inputSize, const UA_Variant *input,
+                     size_t outputSize, UA_Variant *output);
 
 typedef struct {
     UA_NodeHead head;
