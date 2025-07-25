d50e141e9bd29131e475b43ccf2cb5aa6a4936ec
Julius Pfrommer <julius.pfrommer@web.de>
2025-07-26 00:02:02 +0200

    feat(client): Allow a predefined list of datatypenodes for UA_Client_getRemoteDataTypes

--- src/client/ua_client_util.c
+++ src/client/ua_client_util.c
@@ -44,7 +44,6 @@ browseDataTypesRecursive(UA_Client *client, NodeIdTree *tree,
     bd.browseDirection = UA_BROWSEDIRECTION_FORWARD;
     bd.referenceTypeId = UA_NS0ID(HASSUBTYPE);
     bd.nodeClassMask = UA_NODECLASS_DATATYPE;
-    bd.resultMask = UA_BROWSERESULTMASK_DISPLAYNAME; // XX
 
     /* Browse */
     UA_BrowseResult br = UA_Client_browse(client, NULL, 0, &bd);
@@ -93,103 +92,163 @@ browseDataTypesRecursive(UA_Client *client, NodeIdTree *tree,
     return res;
 }
 
+static UA_StatusCode
+getRemoteDataTypes(UA_Client *client, UA_ReadRequest *req,
+                   UA_DataTypeArray **outCustomTypes) {
+    UA_ReadResponse rr = UA_Client_Service_read(client, *req);
+    UA_StatusCode res = rr.responseHeader.serviceResult;
+    if(res == UA_STATUSCODE_GOOD && rr.resultsSize != req->nodesToReadSize)
+        res = UA_STATUSCODE_BADINTERNALERROR;
+    if(res != UA_STATUSCODE_GOOD) {
+        UA_ReadResponse_clear(&rr);
+        return res;
+    }
+
+    /* Allocate the arrays */
+    size_t typesSize = req->nodesToReadSize / 2;
+    UA_DataTypeArray *dta = (UA_DataTypeArray*)
+        UA_calloc(1, sizeof(UA_DataTypeArray));
+    if(!dta) {
+        UA_ReadResponse_clear(&rr);
+        return UA_STATUSCODE_BADOUTOFMEMORY;
+    }
+    dta->cleanup = true;
+    dta->types = (UA_DataType*)UA_calloc(typesSize, sizeof(UA_DataType));
+    if(!dta->types) {
+        UA_ReadResponse_clear(&rr);
+        UA_cleanupDataTypeWithCustom(dta);
+        return UA_STATUSCODE_BADOUTOFMEMORY;
+    }
+
+    /* Create the new types */
+    for(size_t i = 0; i < typesSize; i++) {
+        UA_DataValue *typeDef = &rr.results[i];
+        if(typeDef->hasStatus && typeDef->status != UA_STATUSCODE_GOOD)
+            continue;
+        if(!typeDef->hasValue || !UA_Variant_hasScalarType(&typeDef->value,
+                                                           &UA_TYPES[UA_TYPES_STRUCTUREDEFINITION]))
+            continue;
+        UA_StructureDefinition *sd = (UA_StructureDefinition*)typeDef->value.data;
+
+        UA_DataValue *typeName = &rr.results[i+typesSize];
+        if(typeName->hasStatus && typeName->status != UA_STATUSCODE_GOOD)
+            continue;
+        if(!typeName->hasValue || !UA_Variant_hasScalarType(&typeName->value,
+                                                            &UA_TYPES[UA_TYPES_LOCALIZEDTEXT]))
+            continue;
+        UA_LocalizedText *name = (UA_LocalizedText*)typeName->value.data;
+
+        res = UA_DataType_fromStructureDefinition(&dta->types[dta->typesSize], sd,
+                                                  req->nodesToRead[i].nodeId, name->text,
+                                                  client->config.customDataTypes);
+        if(res != UA_STATUSCODE_GOOD) {
+            UA_LOG_ERROR(client->config.logging, UA_LOGCATEGORY_CLIENT,
+                         "Could not add the DataType %S (%N) with status code %s",
+                         name->text, req->nodesToRead[i].nodeId, UA_StatusCode_name(res));
+            continue;
+        }
+
+        UA_LOG_INFO(client->config.logging, UA_LOGCATEGORY_CLIENT,
+                    "Added DataType %S (%N) to the DataTypeArray",
+                    name->text, req->nodesToRead[i].nodeId);
+        dta->typesSize++;
+    }
+
+    *outCustomTypes = dta;
+
+    UA_ReadRequest_clear(req);
+    if(dta->typesSize == 0) {
+        UA_free(dta->types);
+        dta->types = NULL;
+    }
+    return UA_STATUSCODE_GOOD;
+}
+
 static void *
 setNodesToRead(void *context, NodeIdTreeEntry *elm) {
     UA_ReadValueId **rvi = (UA_ReadValueId**)context;
     (*rvi)->nodeId = elm->nodeId;
     (*rvi)->attributeId = UA_ATTRIBUTEID_DATATYPEDEFINITION;
     (*rvi)++;
+    UA_free(elm);
     return NULL;
 }
 
-UA_StatusCode
+UA_StatusCode UA_EXPORT UA_THREADSAFE
 UA_Client_getRemoteDataTypes(UA_Client *client,
-                             UA_DataTypeArray *customTypes) {
-    if(customTypes->typesSize != 0 || customTypes->types != NULL)
-        return UA_STATUSCODE_BADINTERNALERROR;
-    
-    size_t treeSize = 0;
-    NodeIdTree tree;
-    ZIP_INIT(&tree);
-
-    /* Get all unknown DataType NodeIds */
-    UA_StatusCode res = browseDataTypesRecursive(client, &tree, &treeSize,
-                                                 UA_NS0ID(STRUCTURE));
-    if(res != UA_STATUSCODE_GOOD) {
-        ZIP_ITER(NodeIdTree, &tree, deleteNodeIdEntry, NULL);
-        return res;
-    }
-
-    if(treeSize == 0)
-        return UA_STATUSCODE_GOOD;
-
-    /* Allocate the array of custom types */
-    UA_DataType *types = (UA_DataType*)UA_calloc(treeSize, sizeof(UA_DataType));
-    if(!types) {
-        ZIP_ITER(NodeIdTree, &tree, deleteNodeIdEntry, NULL);
-        return UA_STATUSCODE_BADOUTOFMEMORY;
-    }
-
-    /* Read the descriptions of the unknown types */
-    UA_ReadValueId *nodesToRead = (UA_ReadValueId*)
-        UA_calloc(treeSize, sizeof(UA_ReadValueId));
-    if(!nodesToRead) {
-        UA_free(types);
-        ZIP_ITER(NodeIdTree, &tree, deleteNodeIdEntry, NULL);
-        return UA_STATUSCODE_BADOUTOFMEMORY;
-    }
-    UA_ReadValueId *tmp = nodesToRead;
-    ZIP_ITER(NodeIdTree, &tree, setNodesToRead, &tmp);
+                             size_t dataTypesNodesSize,
+                             const UA_NodeId *dataTypesNodes,
+                             UA_DataTypeArray **customTypes) {
+    if(!customTypes)
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
 
     UA_ReadRequest req;
     UA_ReadRequest_init(&req);
-    req.nodesToRead = nodesToRead;
-    req.nodesToReadSize = treeSize;
 
-    UA_ReadResponse rr = UA_Client_Service_read(client, req);
-    res = rr.responseHeader.serviceResult;
-    if(res == UA_STATUSCODE_GOOD && rr.resultsSize != treeSize)
-        res = UA_STATUSCODE_BADINTERNALERROR;
-    if(res != UA_STATUSCODE_GOOD) {
-        ZIP_ITER(NodeIdTree, &tree, deleteNodeIdEntry, NULL);
-        UA_free(types);
-        UA_ReadRequest_clear(&req);
-        return UA_STATUSCODE_BADOUTOFMEMORY;
-    }
+    /* req.nodesToRead has twice the normal length. Because we read the
+     * DataTypeDescription and the DisplayName in the same request. */
 
-    /* Create the new types */
-    size_t typesSize = 0;
-    for(size_t i = 0; i < treeSize; i++) {
-        UA_DataValue *dv = &rr.results[i];
-        if(dv->hasStatus && dv->status != UA_STATUSCODE_GOOD)
-            continue;
-        if(!dv->hasValue || dv->value.type != &UA_TYPES[UA_TYPES_STRUCTUREDEFINITION])
-            continue;
-        NodeIdTreeEntry *entry = ZIP_FIND(NodeIdTree, &tree, &nodesToRead[i].nodeId);
-        UA_StructureDefinition *sd = (UA_StructureDefinition*)dv->value.data;
-        res = UA_DataType_fromStructureDefinition(&types[typesSize], sd,
-                                                  nodesToRead[i].nodeId, entry->typeName,
-                                                  client->config.customDataTypes);
+    UA_StatusCode res = UA_STATUSCODE_GOOD;
+    if(dataTypesNodesSize == 0) {
+        /* Browse to find unknown datatypes in the information model */
+
+        /* Get all unknown DataType NodeIds */
+        NodeIdTree tree;
+        ZIP_INIT(&tree);
+        size_t treeSize = 0;
+        res = browseDataTypesRecursive(client, &tree, &treeSize, UA_NS0ID(STRUCTURE));
         if(res != UA_STATUSCODE_GOOD) {
-            UA_LOG_ERROR(client->config.logging, UA_LOGCATEGORY_CLIENT,
-                         "Could not add the DataType %S (%N) with status code %s",
-                         entry->typeName, nodesToRead[i].nodeId, UA_StatusCode_name(res));
-            continue;
+            ZIP_ITER(NodeIdTree, &tree, deleteNodeIdEntry, NULL);
+            return res;
         }
-        typesSize++;
-    }
 
-    /* Clean up */
-    ZIP_ITER(NodeIdTree, &tree, deleteNodeIdEntry, NULL);
-    UA_ReadRequest_clear(&req);
+        if(treeSize == 0)
+            return UA_STATUSCODE_GOOD;
 
-    /* Set the output types array */
-    if(typesSize > 0) {
-        customTypes->types = types;
-        customTypes->typesSize = typesSize;
-        customTypes->cleanup = true; /* Contains allocated data that needs to be freed */
+        /* Read the descriptions of the unknown types */
+        UA_ReadValueId *nodesToRead = (UA_ReadValueId*)
+            UA_calloc(treeSize * 2, sizeof(UA_ReadValueId));
+        if(!nodesToRead) {
+            ZIP_ITER(NodeIdTree, &tree, deleteNodeIdEntry, NULL);
+            return UA_STATUSCODE_BADOUTOFMEMORY;
+        }
+        UA_ReadValueId *tmp = nodesToRead;
+        ZIP_ITER(NodeIdTree, &tree, setNodesToRead, &tmp);
+        req.nodesToRead = nodesToRead;
+        req.nodesToReadSize = treeSize;
     } else {
-        UA_free(types);
+        /* Prepare the ReadRequest for the provided NodeIds */
+        req.nodesToRead = (UA_ReadValueId*)
+            UA_Array_new(dataTypesNodesSize * 2, &UA_TYPES[UA_TYPES_READVALUEID]);
+        if(!req.nodesToRead)
+            return UA_STATUSCODE_BADOUTOFMEMORY;
+        req.nodesToReadSize = dataTypesNodesSize;
+
+        for(size_t i = 0; i < dataTypesNodesSize; i++) {
+            req.nodesToRead[i].attributeId = UA_ATTRIBUTEID_DATATYPEDEFINITION;
+            res |= UA_NodeId_copy(&dataTypesNodes[i], &req.nodesToRead[i].nodeId);
+        }
+
+        if(res != UA_STATUSCODE_GOOD) {
+            UA_ReadRequest_clear(&req);
+            return res;
+        }
     }
-    return UA_STATUSCODE_GOOD;
+
+    /* Prepare reading the Displayname attributes additionally */
+    for(size_t i = 0; i < req.nodesToReadSize; i++) {
+        res |= UA_NodeId_copy(&req.nodesToRead[i].nodeId,
+                              &req.nodesToRead[i + req.nodesToReadSize].nodeId);
+        req.nodesToRead[i + req.nodesToReadSize].attributeId = UA_ATTRIBUTEID_DISPLAYNAME;
+    }
+    req.nodesToReadSize *= 2;
+    if(res != UA_STATUSCODE_GOOD) {
+        UA_ReadRequest_clear(&req);
+        return res;
+    }
+
+    /* Read and convert the DataTypeDefinitions */
+    res = getRemoteDataTypes(client, &req, customTypes);
+    UA_ReadRequest_clear(&req);
+    return res;
 }
