ddd4588a14575b88dac2d3944e91f919a19df2e1
Andreas Ebner <ebner.andreas@outlook.com>
2025-09-09 17:58:05 +0200

    feat(server): add sceleton for RBAC methods AddIdentity, RemoveIdentity, AddApplication, RemoveApplication, AddEndpoint, and RemoveEndpoint

--- src/server/ua_server_ns0_rbac.c
+++ src/server/ua_server_ns0_rbac.c
@@ -95,80 +95,139 @@ removeRoleMethodCallback(UA_Server *server,
     return UA_STATUSCODE_GOOD;
 }
 
+static UA_StatusCode
+addIdentityMethodCallback(UA_Server *server,
+                         const UA_NodeId *objectId, void *objectContext,
+                         const UA_NodeId *methodId, void *methodContext,
+                         const UA_NodeId *inputType, void *inputContext,
+                         size_t inputSize, const UA_Variant *input,
+                         size_t outputSize, UA_Variant *output) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                "AddIdentity method called");
+    
+    if(inputSize != 1) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                     "AddIdentity: Expected 1 input parameter, got %zu", inputSize);
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+    
+    return UA_STATUSCODE_GOOD;
+}
+
+static UA_StatusCode
+removeIdentityMethodCallback(UA_Server *server,
+                            const UA_NodeId *objectId, void *objectContext,
+                            const UA_NodeId *methodId, void *methodContext,
+                            const UA_NodeId *inputType, void *inputContext,
+                            size_t inputSize, const UA_Variant *input,
+                            size_t outputSize, UA_Variant *output) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                "RemoveIdentity method called");
+    
+    if(inputSize != 1) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                     "RemoveIdentity: Expected 1 input parameter, got %zu", inputSize);
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+    
+    return UA_STATUSCODE_GOOD;
+}
+
+static UA_StatusCode
+addApplicationMethodCallback(UA_Server *server,
+                            const UA_NodeId *objectId, void *objectContext,
+                            const UA_NodeId *methodId, void *methodContext,
+                            const UA_NodeId *inputType, void *inputContext,
+                            size_t inputSize, const UA_Variant *input,
+                            size_t outputSize, UA_Variant *output) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                "AddApplication method called");
+    
+    if(inputSize != 1) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                     "AddApplication: Expected 1 input parameter, got %zu", inputSize);
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+    
+    return UA_STATUSCODE_GOOD;
+}
+
+static UA_StatusCode
+removeApplicationMethodCallback(UA_Server *server,
+                               const UA_NodeId *objectId, void *objectContext,
+                               const UA_NodeId *methodId, void *methodContext,
+                               const UA_NodeId *inputType, void *inputContext,
+                               size_t inputSize, const UA_Variant *input,
+                               size_t outputSize, UA_Variant *output) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                "RemoveApplication method called");
+    
+    if(inputSize != 1) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                     "RemoveApplication: Expected 1 input parameter, got %zu", inputSize);
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+    
+    return UA_STATUSCODE_GOOD;
+}
+
+static UA_StatusCode
+addEndpointMethodCallback(UA_Server *server,
+                         const UA_NodeId *objectId, void *objectContext,
+                         const UA_NodeId *methodId, void *methodContext,
+                         const UA_NodeId *inputType, void *inputContext,
+                         size_t inputSize, const UA_Variant *input,
+                         size_t outputSize, UA_Variant *output) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                "AddEndpoint method called");
+    
+    if(inputSize != 1) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                     "AddEndpoint: Expected 1 input parameter, got %zu", inputSize);
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+    
+    return UA_STATUSCODE_GOOD;
+}
+
+static UA_StatusCode
+removeEndpointMethodCallback(UA_Server *server,
+                            const UA_NodeId *objectId, void *objectContext,
+                            const UA_NodeId *methodId, void *methodContext,
+                            const UA_NodeId *inputType, void *inputContext,
+                            size_t inputSize, const UA_Variant *input,
+                            size_t outputSize, UA_Variant *output) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                "RemoveEndpoint method called");
+    
+    if(inputSize != 1) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                     "RemoveEndpoint: Expected 1 input parameter, got %zu", inputSize);
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+    
+    return UA_STATUSCODE_GOOD;
+}
+
 UA_StatusCode
 initNS0RBAC(UA_Server *server) {
     UA_StatusCode retval = UA_STATUSCODE_GOOD;
     
-    /* Initialize RBAC information model nodes 
-     * According to OPC UA Part 18 specification, the RoleSet Object should be
-     * present under Server.ServerCapabilities.RoleSet (i=15606) with the following
-     * well-known roles:
-     * - Anonymous (i=15644)
-     * - AuthenticatedUser (i=15656) 
-     * - Observer (i=15668)
-     * - Operator (i=15680)
-     * - Engineer (i=16036)
-     * - Supervisor (i=15692)
-     * - ConfigureAdmin (i=15716)
-     * - SecurityAdmin (i=15704)
-     * - SecurityKeyServerAdmin (i=25565)
-     * - SecurityKeyServerAccess (i=25603)
-     * - SecurityKeyServerPush (i=25584)
-     */
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+                "RoleSet Object and well-known roles are available through namespace0_generated");
     
-    /* Add the RoleSet Object to Server.ServerCapabilities */
-    UA_ObjectAttributes roleSetAttr = UA_ObjectAttributes_default;
-    roleSetAttr.displayName = UA_LOCALIZEDTEXT("", "RoleSet");
-    roleSetAttr.description = UA_LOCALIZEDTEXT("", "Contains the roles supported by the server");
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_ROLESETTYPE_ADDROLE), addRoleMethodCallback);
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_ROLESETTYPE_REMOVEROLE), removeRoleMethodCallback);
 
-    retval = UA_Server_addObjectNode(server,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERCAPABILITIES_ROLESET),  // RoleSet Object ID
-        UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERCAPABILITIES),   // ServerCapabilities parent
-        UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),     // HasComponent reference
-        UA_QUALIFIEDNAME(0, "RoleSet"),
-        UA_NODEID_NUMERIC(0, UA_NS0ID_ROLESETTYPE),  // RoleSetType
-        roleSetAttr,
-        NULL, NULL);
-    
-    if(retval != UA_STATUSCODE_GOOD) {
-        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
-                     "Failed to add RoleSet Object: %s", UA_StatusCode_name(retval));
-        return retval;
-    }
-    
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
-                "RoleSet Object added successfully to Server.ServerCapabilities");
-    
-    retval = UA_Server_setMethodNode_callback(server,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_ROLESETTYPE_ADDROLE), 
-        addRoleMethodCallback);
-    
-    if(retval != UA_STATUSCODE_GOOD) {
-        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
-                     "Failed to set AddRole method callback: %s", UA_StatusCode_name(retval));
-        return retval;
-    }
-    
-    retval = UA_Server_setMethodNode_callback(server,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_ROLESETTYPE_REMOVEROLE),
-        removeRoleMethodCallback);
-    
-    if(retval != UA_STATUSCODE_GOOD) {
-        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
-                     "Failed to set RemoveRole method callback: %s", UA_StatusCode_name(retval));
-        return retval;
-    }
-    
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
-                "RBAC method callbacks (AddRole, RemoveRole) attached successfully");
-    
-    /* TODO: Implement other RBAC method callbacks */
-    /* - AddUser, ModifyUser, RemoveUser methods */
-    /* - ChangePassword method */
-    /* - AddIdentity, RemoveIdentity methods */
-    /* - AddApplication, RemoveApplication methods */
-    /* - AddEndpoint, RemoveEndpoint methods */
-    
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_ROLETYPE_ADDIDENTITY), addIdentityMethodCallback);
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_ROLETYPE_REMOVEIDENTITY), removeIdentityMethodCallback);
+
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_ROLETYPE_ADDAPPLICATION), addApplicationMethodCallback);
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_ROLETYPE_REMOVEAPPLICATION), removeApplicationMethodCallback);
+
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_ROLETYPE_ADDENDPOINT), addEndpointMethodCallback);
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_ROLETYPE_REMOVEENDPOINT), removeEndpointMethodCallback);
+
     return retval;
 }
 
