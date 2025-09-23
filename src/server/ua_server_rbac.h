fd742ae0cacb1320a78b1f373d99eaa4a8e339c3
Andreas Ebner <ebner.andreas@outlook.com>
2025-09-23 11:01:04 +0200

    feat(server): add custom role type, implement role management API and internal role representation

---
+++ src/server/ua_server_rbac.h
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *    Copyright 2025 (c) Andreas Ebner
+ */
+
+#ifndef UA_SERVER_RBAC_H_
+#define UA_SERVER_RBAC_H_
+
+#include "ua_server_internal.h"
+
+_UA_BEGIN_DECLS
+
+#ifdef UA_ENABLE_RBAC
+
+UA_StatusCode
+UA_Server_initializeRBAC(UA_Server *server);
+
+void
+UA_Server_cleanupRBAC(UA_Server *server);
+
+#endif /* UA_ENABLE_RBAC */
+
+_UA_END_DECLS
+
+#endif /* UA_SERVER_RBAC_H_ */
