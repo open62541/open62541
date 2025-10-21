20875a858836a5082c3fd94942a591377b53675e
Andreas Ebner <andreas.ebner@o6-automation.com>
2025-10-21 09:52:07 +0200

    feat(server): update RBAC implementation with role representation and data source callbacks

--- src/server/ua_server_ns0_rbac.c
+++ src/server/ua_server_ns0_rbac.c
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- *    Copyright 2025 (c) Fraunhofer IOSB (Author: Andreas Ebner)
+ *    Copyright 2025 (c) o6 Automation GmbH (Author: Andreas Ebner)
  */
 
 
@@ -12,23 +12,220 @@
 #include <open62541/plugin/accesscontrol.h>
 #include "open62541/namespace0_generated.h"
 #include "open62541/nodeids.h"
+#include "ua_server_internal.h"
 
 #ifdef UA_ENABLE_RBAC
+
+/* Use the same flag for information model */
+#define UA_ENABLE_RBAC_INFORMATIONMODEL
 
 /* OPC UA Part 18 Role-Based Access Control Implementation
  * 
  * Reference: OPC 10000-18: UA Part 18: Role-Based Security
  * https://reference.opcfoundation.org/Core/Part18/v105/docs/
  * 
- * This file implements the RBAC method callbacks for the OPC UA namespace 0:
- * - Role Management: AddRole, RemoveRole methods using UA_Server_addRole/removeRole API
- * - Identity Management: AddIdentity, RemoveIdentity methods using UA_Server_addRoleIdentity/removeRoleIdentity API
- * - Application Management: AddApplication, RemoveApplication methods using UA_Server_addRoleApplication/removeRoleApplication API
- * - Endpoint Management: AddEndpoint, RemoveEndpoint methods using UA_Server_addRoleEndpoint/removeRoleEndpoint API
+ * This file implements the RBAC NS0 integration:
+ * - Role object creation and deletion in namespace 0
+ * - DataSource callbacks for role properties (Identities, Applications, Endpoints)
+ * - Method callbacks for role management operations
  */
 
-/* Forward declarations */
 UA_StatusCode initNS0RBAC(UA_Server *server);
+
+#ifdef UA_ENABLE_RBAC_INFORMATIONMODEL
+
+static UA_StatusCode
+readRoleIdentities(UA_Server *server, const UA_NodeId *sessionId, void *sessionContext,
+                   const UA_NodeId *nodeId, void *nodeContext,
+                   UA_Boolean includeSourceTimeStamp, const UA_NumericRange *range,
+                   UA_DataValue *value) {
+    UA_LOCK_ASSERT(&server->serviceMutex);
+    
+    UA_NodeId roleId = *(UA_NodeId*)nodeContext;
+    const UA_Role *role = UA_Server_getRolesById(server, roleId);
+    if(!role)
+        return UA_STATUSCODE_BADNODEIDUNKNOWN;
+    
+    UA_Variant_setArrayCopy(&value->value, role->imrt, role->imrtSize,
+                           &UA_TYPES[UA_TYPES_IDENTITYMAPPINGRULETYPE]);
+    value->hasValue = true;
+    return UA_STATUSCODE_GOOD;
+}
+
+static UA_StatusCode
+readRoleApplications(UA_Server *server, const UA_NodeId *sessionId, void *sessionContext,
+                     const UA_NodeId *nodeId, void *nodeContext,
+                     UA_Boolean includeSourceTimeStamp, const UA_NumericRange *range,
+                     UA_DataValue *value) {
+    UA_LOCK_ASSERT(&server->serviceMutex);
+    
+    UA_NodeId roleId = *(UA_NodeId*)nodeContext;
+    const UA_Role *role = UA_Server_getRolesById(server, roleId);
+    if(!role)
+        return UA_STATUSCODE_BADNODEIDUNKNOWN;
+    
+    UA_Variant_setArrayCopy(&value->value, role->applications, role->applicationsSize,
+                           &UA_TYPES[UA_TYPES_STRING]);
+    value->hasValue = true;
+    return UA_STATUSCODE_GOOD;
+}
+
+static UA_StatusCode
+readRoleEndpoints(UA_Server *server, const UA_NodeId *sessionId, void *sessionContext,
+                  const UA_NodeId *nodeId, void *nodeContext,
+                  UA_Boolean includeSourceTimeStamp, const UA_NumericRange *range,
+                  UA_DataValue *value) {
+    UA_LOCK_ASSERT(&server->serviceMutex);
+    
+    UA_NodeId roleId = *(UA_NodeId*)nodeContext;
+    const UA_Role *role = UA_Server_getRolesById(server, roleId);
+    if(!role)
+        return UA_STATUSCODE_BADNODEIDUNKNOWN;
+    
+    UA_Variant_setArrayCopy(&value->value, role->endpoints, role->endpointsSize,
+                           &UA_TYPES[UA_TYPES_ENDPOINTTYPE]);
+    value->hasValue = true;
+    return UA_STATUSCODE_GOOD;
+}
+
+/* Add Role object to NS0 
+ * Note: role->roleId MUST already be set by caller (UA_Server_addRole)
+ * This function simply creates the NS0 information model representation */
+UA_StatusCode
+addRoleRepresentation(UA_Server *server, UA_Role *role) {
+    UA_LOCK_ASSERT(&server->serviceMutex);
+    
+    if(!server || !role)
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    
+    /* role->roleId MUST be valid at this point */
+    if(UA_NodeId_isNull(&role->roleId))
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    
+    UA_StatusCode res = UA_STATUSCODE_GOOD;
+    
+    /* Add Role object instance using the pre-assigned roleId */
+    UA_ObjectAttributes oAttr = UA_ObjectAttributes_default;
+    oAttr.displayName.locale = UA_STRING("en-US");
+    oAttr.displayName.text = role->roleName.name;
+    oAttr.description = UA_LOCALIZEDTEXT("en-US", "");
+    
+    res = UA_Server_addObjectNode(server, role->roleId,
+                                  UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERCAPABILITIES_ROLESET),
+                                  UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
+                                  role->roleName,
+                                  UA_NODEID_NUMERIC(0, UA_NS0ID_ROLETYPE),
+                                  oAttr, NULL, NULL);
+    if(res != UA_STATUSCODE_GOOD)
+        return res;
+    
+    /* Add Identities variable with DataSource callback */
+    UA_NodeId *roleIdContext = UA_NodeId_new();
+    if(!roleIdContext) {
+        UA_Server_deleteNode(server, role->roleId, true);
+        return UA_STATUSCODE_BADOUTOFMEMORY;
+    }
+    UA_NodeId_copy(&role->roleId, roleIdContext);
+    
+    UA_VariableAttributes vAttr = UA_VariableAttributes_default;
+    vAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Identities");
+    vAttr.dataType = UA_TYPES[UA_TYPES_IDENTITYMAPPINGRULETYPE].typeId;
+    vAttr.valueRank = UA_VALUERANK_ONE_OR_MORE_DIMENSIONS;
+    vAttr.accessLevel = UA_ACCESSLEVELMASK_READ;
+    
+    UA_DataSource identitiesDataSource;
+    identitiesDataSource.read = readRoleIdentities;
+    identitiesDataSource.write = NULL;
+    
+    UA_NodeId identitiesNodeId;
+    res = UA_Server_addDataSourceVariableNode(server, UA_NODEID_NULL,
+                                              role->roleId,
+                                              UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                                              UA_QUALIFIEDNAME(0, "Identities"),
+                                              UA_NODEID_NUMERIC(0, UA_NS0ID_PROPERTYTYPE),
+                                              vAttr, identitiesDataSource,
+                                              roleIdContext, &identitiesNodeId);
+    if(res != UA_STATUSCODE_GOOD) {
+        UA_NodeId_delete(roleIdContext);
+        UA_Server_deleteNode(server, role->roleId, true);
+        return res;
+    }
+    
+    /* Add Applications variable with DataSource callback */
+    UA_NodeId *roleIdContext2 = UA_NodeId_new();
+    if(!roleIdContext2) {
+        UA_Server_deleteNode(server, role->roleId, true);
+        return UA_STATUSCODE_BADOUTOFMEMORY;
+    }
+    UA_NodeId_copy(&role->roleId, roleIdContext2);
+    
+    vAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Applications");
+    vAttr.dataType = UA_TYPES[UA_TYPES_STRING].typeId;
+    vAttr.valueRank = UA_VALUERANK_ONE_OR_MORE_DIMENSIONS;
+    
+    UA_DataSource applicationsDataSource;
+    applicationsDataSource.read = readRoleApplications;
+    applicationsDataSource.write = NULL;
+    
+    UA_NodeId applicationsNodeId;
+    res = UA_Server_addDataSourceVariableNode(server, UA_NODEID_NULL,
+                                              role->roleId,
+                                              UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                                              UA_QUALIFIEDNAME(0, "Applications"),
+                                              UA_NODEID_NUMERIC(0, UA_NS0ID_PROPERTYTYPE),
+                                              vAttr, applicationsDataSource,
+                                              roleIdContext2, &applicationsNodeId);
+    if(res != UA_STATUSCODE_GOOD) {
+        UA_Server_deleteNode(server, role->roleId, true);
+        return res;
+    }
+    
+    /* Add Endpoints variable with DataSource callback */
+    UA_NodeId *roleIdContext3 = UA_NodeId_new();
+    if(!roleIdContext3) {
+        UA_Server_deleteNode(server, role->roleId, true);
+        return UA_STATUSCODE_BADOUTOFMEMORY;
+    }
+    UA_NodeId_copy(&role->roleId, roleIdContext3);
+    
+    vAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Endpoints");
+    vAttr.dataType = UA_TYPES[UA_TYPES_ENDPOINTTYPE].typeId;
+    vAttr.valueRank = UA_VALUERANK_ONE_OR_MORE_DIMENSIONS;
+    
+    UA_DataSource endpointsDataSource;
+    endpointsDataSource.read = readRoleEndpoints;
+    endpointsDataSource.write = NULL;
+    
+    UA_NodeId endpointsNodeId;
+    res = UA_Server_addDataSourceVariableNode(server, UA_NODEID_NULL,
+                                              role->roleId,
+                                              UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                                              UA_QUALIFIEDNAME(0, "Endpoints"),
+                                              UA_NODEID_NUMERIC(0, UA_NS0ID_PROPERTYTYPE),
+                                              vAttr, endpointsDataSource,
+                                              roleIdContext3, &endpointsNodeId);
+    if(res != UA_STATUSCODE_GOOD) {
+        UA_Server_deleteNode(server, role->roleId, true);
+        return res;
+    }
+    
+    return UA_STATUSCODE_GOOD;
+}
+
+/* Remove Role object from NS0 */
+UA_StatusCode
+removeRoleRepresentation(UA_Server *server, const UA_NodeId *roleId) {
+    UA_LOCK_ASSERT(&server->serviceMutex);
+    
+    if(!server || !roleId)
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    
+    return UA_Server_deleteNode(server, *roleId, true);
+}
+
+#endif /* UA_ENABLE_RBAC_INFORMATIONMODEL */
+
+/* Method callbacks */
 
 static UA_StatusCode
 addRoleMethodCallback(UA_Server *server,
