9f344a2c15e8a17257f6b5844810a74b76b28469
Moritz Bruder <moritz.bruder@extern.o6-automation.com>
2026-02-02 12:00:32 +0100

    feat(plugin): Change json config loading functions
    Update functions for server and client config that load the
    configuration from a file but clear the passed struct first to avoid
    inconsistent state.

--- plugins/include/open62541/client_config_file_based.h
+++ plugins/include/open62541/client_config_file_based.h
@@ -17,14 +17,14 @@ _UA_BEGIN_DECLS
 UA_EXPORT UA_Client *
 UA_Client_newFromFile(const UA_ByteString json_config);
 
-/* Update a client configuration from a file.  The client configuration is
- * loaded from a Json5 file into the server.
+/* Loads a client configuration from a file.  The passed client configuration is
+ * cleared.
  *
  * @param config The client configuration.
  * @param json The configuration in json5 format.
  */
 UA_EXPORT UA_StatusCode
-UA_ClientConfig_updateFromFile(UA_ClientConfig *config, const UA_ByteString json_config);
+UA_ClientConfig_loadFromFile(UA_ClientConfig *config, const UA_ByteString json_config);
 
 _UA_END_DECLS
 
