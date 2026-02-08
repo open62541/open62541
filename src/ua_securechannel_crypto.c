a0656c35218ee16e1ee36127e80f0cd657d63b57
Julius Pfrommer <julius.pfrommer@web.de>
2026-02-08 22:03:16 +0100

    refactor(core): Bump logging of security issues in SecureChannel from warning to error

--- src/ua_securechannel_crypto.c
+++ src/ua_securechannel_crypto.c
@@ -72,9 +72,9 @@ UA_SecureChannel_generateLocalKeys(const UA_SecureChannel *channel) {
 
  error:
     if(res != UA_STATUSCODE_GOOD) {
-        UA_LOG_WARNING_CHANNEL(sp->logger, channel,
-                               "Could not generate local keys (statuscode: %s)",
-                               UA_StatusCode_name(res));
+        UA_LOG_ERROR_CHANNEL(sp->logger, channel,
+                             "Could not generate local keys (%s)",
+                             UA_StatusCode_name(res));
     }
     UA_ByteString_clear(&buf);
     return res;
@@ -117,8 +117,8 @@ generateRemoteKeys(const UA_SecureChannel *channel) {
 
  error:
     if(res != UA_STATUSCODE_GOOD) {
-        UA_LOG_WARNING_CHANNEL(sp->logger, channel,
-                               "Could not generate remote keys (statuscode: %s)",
+        UA_LOG_ERROR_CHANNEL(sp->logger, channel,
+                               "Could not generate remote keys (%s)",
                                UA_StatusCode_name(res));
     }
     UA_ByteString_clear(&buf);
@@ -478,7 +478,7 @@ decryptAndVerifyChunk(const UA_SecureChannel *channel,
      * bytes long: 8 byte for the SequenceHeader and one byte for the actual
      * message */
     UA_CHECK(offset + padSize + sigsize + 9 < chunk->length,
-             UA_LOG_WARNING_CHANNEL(sp->logger, channel, "Impossible padding value");
+             UA_LOG_ERROR_CHANNEL(sp->logger, channel, "Impossible padding value");
              return UA_STATUSCODE_BADSECURITYCHECKSFAILED);
 
     /* Hide the signature and padding */
@@ -521,7 +521,7 @@ checkSymHeader(UA_SecureChannel *channel, const UA_UInt32 tokenId,
 
         /* Not the new token */
         UA_CHECK(tokenId == channel->altSecurityToken.tokenId,
-                 UA_LOG_WARNING_CHANNEL(sp->logger, channel, "Unknown SecurityToken");
+                 UA_LOG_ERROR_CHANNEL(sp->logger, channel, "Unknown SecurityToken");
                  return UA_STATUSCODE_BADSECURECHANNELTOKENUNKNOWN);
 
         /* Roll over to the new token, generate new local and remote keys */
@@ -542,7 +542,7 @@ checkSymHeader(UA_SecureChannel *channel, const UA_UInt32 tokenId,
 
         /* Not the new token */
         UA_CHECK(tokenId == channel->securityToken.tokenId,
-                 UA_LOG_WARNING_CHANNEL(sp->logger, channel, "Unknown SecurityToken");
+                 UA_LOG_ERROR_CHANNEL(sp->logger, channel, "Unknown SecurityToken");
                  return UA_STATUSCODE_BADSECURECHANNELTOKENUNKNOWN);
 
         /* The remote server uses the new token for the first time. Delete the
@@ -556,7 +556,7 @@ checkSymHeader(UA_SecureChannel *channel, const UA_UInt32 tokenId,
 
     UA_DateTime timeout = token->createdAt + (token->revisedLifetime * UA_DATETIME_MSEC);
     if(channel->state == UA_SECURECHANNELSTATE_OPEN && timeout < nowMonotonic) {
-        UA_LOG_WARNING_CHANNEL(sp->logger, channel, "SecurityToken timed out");
+        UA_LOG_ERROR_CHANNEL(sp->logger, channel, "SecurityToken timed out");
         UA_SecureChannel_shutdown(channel, UA_SHUTDOWNREASON_TIMEOUT);
         return UA_STATUSCODE_BADSECURECHANNELCLOSED;
     }
