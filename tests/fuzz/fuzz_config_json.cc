79d87196c7bac16ac054b8e34fb0ab11cb8dc750
David Korczynski <david@adalogics.com>
2026-04-03 03:54:53 -0700

    adjust to 1.5
    Signed-off-by: David Korczynski <david@adalogics.com>

--- tests/fuzz/fuzz_config_json.cc
+++ tests/fuzz/fuzz_config_json.cc
@@ -2,36 +2,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Fuzzer for JSON configuration parsing (UA_ServerConfig_loadFromFile
- * and UA_ClientConfig_loadFromFile).
+ * Fuzzer for JSON configuration parsing (UA_Server_newFromFile).
  */
 
 #include <open62541/server.h>
-#include <open62541/client.h>
 #include <open62541/server_config_default.h>
-#include <open62541/client_config_default.h>
+#include <open62541/server_config_file_based.h>
 
 extern "C" int
 LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    if(size < 1)
+        return 0;
+
     UA_ByteString buf;
     buf.data = (UA_Byte*)(uintptr_t)data;
     buf.length = size;
 
-    /* Use the first byte to decide which parser to exercise */
-    if(size < 1)
-        return 0;
-
-    if(data[0] % 2 == 0) {
-        /* Fuzz server config parsing */
-        UA_Server *server = UA_Server_newFromFile(buf);
-        if(server)
-            UA_Server_delete(server);
-    } else {
-        /* Fuzz client config parsing */
-        UA_Client *client = UA_Client_newFromFile(buf);
-        if(client)
-            UA_Client_delete(client);
-    }
+    /* Fuzz server config parsing from JSON */
+    UA_Server *server = UA_Server_newFromFile(buf);
+    if(server)
+        UA_Server_delete(server);
 
     return 0;
 }
