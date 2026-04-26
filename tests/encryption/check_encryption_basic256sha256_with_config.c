10bd8b5709f96cf0ea08438d3004dfeed461df00
Alexander Karzhenkov <alexander.karzhenkov@horiba.com>
2026-04-26 10:24:20 +0500

    test(crypto): add CK_NOFORK

--- tests/encryption/check_encryption_basic256sha256_with_config.c
+++ tests/encryption/check_encryption_basic256sha256_with_config.c
@@ -114,6 +114,7 @@ static Suite* testSuite_encryption(void) {
 int main(void) {
     Suite *s = testSuite_encryption();
     SRunner *sr = srunner_create(s);
+    srunner_set_fork_status(sr, CK_NOFORK);
     srunner_run_all(sr, CK_NORMAL);
     int number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
