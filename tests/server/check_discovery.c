0a7f106f67dbcfca3d051e33574f7442c6eec7e5
Hannes Bichler <hannes.bichler@engelglobal.com>
2026-05-08 11:30:50 +0200

    add missing header

--- tests/server/check_discovery.c
+++ tests/server/check_discovery.c
@@ -4,6 +4,7 @@
 
 #include <open62541/client.h>
 #include <open62541/client_config_default.h>
+#include <open62541/server.h>
 #include <open62541/server_config_default.h>
 #include <open62541/plugin/certificategroup_default.h>
 
