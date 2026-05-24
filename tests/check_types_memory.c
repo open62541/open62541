1ce79b4d7ce3074fd8d4291a6546b7231eae0f9d
Julius Pfrommer <julius.pfrommer@o6-automation.com>
2026-05-24 21:04:27 +0200

    fix(tests): Fix build of check_types_memory.c

--- tests/check_types_memory.c
+++ tests/check_types_memory.c
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#define _XOPEN_SOURCE 500
 #include <open62541/server.h>
 #include <open62541/types_generated.h>
 #include <open62541/util.h>
