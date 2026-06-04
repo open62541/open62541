391de16e301d1308f07317e6b29051629c3d2329
Julius Pfrommer <julius.pfrommer@o6-automation.com>
2026-06-04 17:54:41 +0200

    refactor(tests): Always use UA_Server_newForUnitTest and UA_Client_newForUnitTest to make tests deterministic

--- tests/fuzz/fuzz_client.cc
+++ tests/fuzz/fuzz_client.cc
@@ -19,7 +19,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         return 0;
     
     UA_ClientConfig *config = UA_Client_getConfig(client);
-    UA_ClientConfig_setDefault(config);
     if(config->logging)
         config->logging->log = NULL; // Disable logging
 
