1fdfa359a1269135d558ed41589226fa83f98445
Julius Pfrommer <julius.pfrommer@web.de>
2026-08-07 13:59:50 +0200

    fix(win32): restore filestore platform definitions

--- plugins/crypto/ua_filestore_common.h
+++ plugins/crypto/ua_filestore_common.h
@@ -14,7 +14,49 @@
 
 #if defined(UA_ARCHITECTURE_POSIX) || defined(UA_ARCHITECTURE_WIN32) || defined(__APPLE__)
 
+#ifdef UA_ARCHITECTURE_WIN32
+#include <direct.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include "tr_dirent.h"
+
+#define UA_STAT stat
+#define UA_DIR DIR
+#define UA_DIRENT dirent
+#define UA_FILE FILE
+#define UA_MODE uint16_t
+
+#define UA_stat stat
+#define UA_opendir opendir
+#define UA_readdir readdir
+#define UA_rewinddir rewinddir
+#define UA_closedir closedir
+#define UA_mkdir(path, mode) _mkdir(path)
+#define UA_fopen fopen
+#define UA_fread fread
+#define UA_fwrite fwrite
+#define UA_fseek fseek
+#define UA_ftell ftell
+#define UA_fclose fclose
+#define UA_remove remove
+
+#define UA_SEEK_END SEEK_END
+#define UA_SEEK_SET SEEK_SET
+#define UA_DT_REG DT_REG
+#define UA_DT_DIR DT_DIR
+#define UA_PATH_MAX MAX_PATH
+#define UA_FILENAME_MAX FILENAME_MAX
+
+_UA_BEGIN_DECLS
+char *
+_UA_dirname_minimal(char *path);
+_UA_END_DECLS
+#define UA_dirname _UA_dirname_minimal
+#else
 #include "../../arch/posix/eventloop_posix.h"
+#endif
 
 UA_StatusCode
 readFileToByteString(const char *const path,
