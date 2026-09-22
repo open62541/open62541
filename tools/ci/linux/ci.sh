623cddb2bae43e6d2fb8bfadb6280ec86abcdc97
Andreas Ebner <andreas.ebner@o6-automation.com>
2026-09-22 15:22:25 +0200

    ci: test PubSub security and SKS with OpenSSL
    The PubSub encryption and SKS jobs take the backend as argument and run
    with OpenSSL as well. The release amalgamation build drops the removed
    UA_ENABLE_PUBSUB_ENCRYPTION flag.

--- tools/ci/linux/ci.sh
+++ tools/ci/linux/ci.sh
@@ -113,7 +113,6 @@ function build_release_amalgamation {
           -DUA_ENABLE_DATATYPES_ALL=ON \
           -DUA_ENABLE_ENCRYPTION=MBEDTLS \
           -DUA_ENABLE_PUBSUB=ON \
-          -DUA_ENABLE_PUBSUB_ENCRYPTION=ON \
           -DUA_ENABLE_PUBSUB_INFORMATIONMODEL=ON \
           ..
     make open62541-amalgamation ${MAKEOPTS}
@@ -578,13 +577,13 @@ function unit_tests_encryption {
     make gcov
 }
 
-function unit_tests_encryption_mbedtls_pubsub {
+function unit_tests_encryption_pubsub {
     rm -rf build; mkdir -p build; cd build
     cmake -DCMAKE_BUILD_TYPE=Debug \
           -DUA_BUILD_EXAMPLES=ON \
           -DUA_BUILD_UNIT_TESTS=ON \
           -DUA_ENABLE_COVERAGE=ON \
-          -DUA_ENABLE_ENCRYPTION=MBEDTLS \
+          -DUA_ENABLE_ENCRYPTION=$1 \
           -DUA_ENABLE_PUBSUB=ON \
           -DUA_ENABLE_PUBSUB_INFORMATIONMODEL=ON \
           -DUA_FORCE_WERROR=ON \
@@ -602,7 +601,7 @@ function unit_tests_pubsub_sks {
           -DUA_BUILD_EXAMPLES=ON \
           -DUA_BUILD_UNIT_TESTS=ON \
           -DUA_ENABLE_COVERAGE=ON \
-          -DUA_ENABLE_ENCRYPTION=MBEDTLS \
+          -DUA_ENABLE_ENCRYPTION=$1 \
           -DUA_ENABLE_PUBSUB=ON \
           -DUA_ENABLE_PUBSUB_INFORMATIONMODEL=ON \
           -DUA_ENABLE_PUBSUB_SKS=ON \
